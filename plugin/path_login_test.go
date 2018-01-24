b71a1aa1de9af8017dca90e721f82566c3eb6a8d
Andreas Heider <a.heider@winton.com>
2018-01-24 16:58:36 +0000

    Add login tests

---
+++ plugin/path_login_test.go
@@ -0,0 +1,54 @@
+package kerberos
+
+import (
+	"strings"
+	"testing"
+
+	"github.com/hashicorp/vault/logical"
+)
+
+func setupTestBackend(t *testing.T) (logical.Backend, logical.Storage) {
+	b, storage := getTestBackend(t)
+
+	data := map[string]interface{}{
+		"keytab":          testValidKeytab,
+		"service_account": "testuser",
+	}
+
+	req := &logical.Request{
+		Operation: logical.CreateOperation,
+		Path:      configPath,
+		Storage:   storage,
+		Data:      data,
+	}
+
+	resp, err := b.HandleRequest(req)
+	if err != nil || (resp != nil && resp.IsError()) {
+		t.Fatalf("err: %s resp: %#v\n", err, resp)
+	}
+
+	return b, storage
+}
+
+func TestLogin(t *testing.T) {
+	b, storage := setupTestBackend(t)
+
+	data := map[string]interface{}{
+		"authorization": "",
+	}
+
+	req := &logical.Request{
+		Operation: logical.UpdateOperation,
+		Path:      "login",
+		Storage:   storage,
+		Data:      data,
+	}
+
+	resp, err := b.HandleRequest(req)
+	if err != nil || resp == nil {
+		t.Fatalf("err: %s resp: %#v\n", err, resp)
+	}
+	if !resp.IsError() && !strings.HasPrefix(resp.Error().Error(), "Missing or invalid authorization") {
+		t.Fatalf("err: %s resp: %#v\n", err, resp)
+	}
+}

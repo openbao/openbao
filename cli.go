65cf93bad3f210ca570cc899a068607bd661bacd
James Burns <james.burns@rtslabs.com>
2019-10-10 13:30:58 -0400

    Vault login for WSL - Hardened openURL function for WSL (#77)
    Fixes #76

--- cli.go
+++ cli.go
@@ -3,6 +3,7 @@ package jwtauth
 import (
 	"errors"
 	"fmt"
+	"io/ioutil"
 	"net"
 	"net/http"
 	"os"
@@ -135,18 +136,31 @@ func fetchAuthURL(c *api.Client, role, mount, port string, callbackHost string) 
 	return authURL, nil
 }
 
+// isWSL tests if the binary is being run in Windows Subsystem for Linux
+func isWSL() bool {
+	if runtime.GOOS == "darwin" || runtime.GOOS == "windows" {
+		return false
+	}
+	data, err := ioutil.ReadFile("/proc/version")
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to read /proc/version.\n")
+		return false
+	}
+	return strings.Contains(strings.ToLower(string(data)), "microsoft")
+}
+
 // openURL opens the specified URL in the default browser of the user.
 // Source: https://stackoverflow.com/a/39324149/453290
 func openURL(url string) error {
 	var cmd string
 	var args []string
 
-	switch runtime.GOOS {
-	case "windows":
-		cmd = "cmd"
+	switch {
+	case "windows" == runtime.GOOS || isWSL():
+		cmd = "cmd.exe"
 		args = []string{"/c", "start"}
 		url = strings.Replace(url, "&", "^&", -1)
-	case "darwin":
+	case "darwin" == runtime.GOOS:
 		cmd = "open"
 	default: // "linux", "freebsd", "openbsd", "netbsd"
 		cmd = "xdg-open"

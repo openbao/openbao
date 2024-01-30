85455fb252c65f3ed7367b2a4d4b392de47b7e4a
Alexander Scheel <alexander.m.scheel@gmail.com>
2024-01-29 19:19:01 -0500

    Remove Enos Test Scenarios
    Signed-off-by: Alexander Scheel <alexander.m.scheel@gmail.com>

--- enos/modules/get_local_metadata/scripts/build_date.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-# Copyright (c) HashiCorp, Inc.
-# SPDX-License-Identifier: MPL-2.0
-
-set -eu -o pipefail
-
-pushd "$(git rev-parse --show-toplevel)" > /dev/null
-make ci-get-date
-popd > /dev/null

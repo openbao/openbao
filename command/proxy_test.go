6efe0a18a0f13ad36a3a02ed701f76ab4b3474ad
Alexander Scheel <alexander.m.scheel@gmail.com>
2024-02-02 07:30:49 -0500

    Use builtin/credential/jwt over hashicorp/vault-plugin-auth-jwt
    This switches all references to the JWT plugin to use the new internal
    location for the plugin over the external, HashiCorp owned plugin.
    
    Cherry-pick of e55805ef96c9dc71a2dd2362275201118a2a3f21.
    
    Signed-off-by: Alexander Scheel <alexander.m.scheel@gmail.com>

--- command/proxy_test.go
+++ command/proxy_test.go
@@ -17,11 +17,11 @@ import (
 	"time"
 
 	"github.com/hashicorp/go-hclog"
-	vaultjwt "github.com/hashicorp/vault-plugin-auth-jwt"
 	logicalKv "github.com/hashicorp/vault-plugin-secrets-kv"
 	"github.com/mitchellh/cli"
 	"github.com/openbao/openbao/api"
 	credAppRole "github.com/openbao/openbao/builtin/credential/approle"
+	vaultjwt "github.com/openbao/openbao/builtin/credential/jwt"
 	"github.com/openbao/openbao/command/agent"
 	proxyConfig "github.com/openbao/openbao/command/proxy/config"
 	"github.com/openbao/openbao/helper/useragent"

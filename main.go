9e6b0073005bb55b8bc5b0d1681354faf2a271df
Alexander Scheel <alexander.m.scheel@gmail.com>
2024-02-03 18:11:40 -0500

    Remove patch for SHA-1 certificates
    Vault < 1.12 supported a patch to allow certificates with SHA-1 hashes
    to continue to function after Go removed support in Go 1.18 (or, rather,
    gated it behind a feature flag).
    
    As our version number remains unclear, we'll want to remove this patch
    in case we choose a lower version number for our releases.
    
    Signed-off-by: Alexander Scheel <alexander.m.scheel@gmail.com>

--- main.go
+++ main.go
@@ -1,19 +1,13 @@
 // Copyright (c) HashiCorp, Inc.
 // SPDX-License-Identifier: MPL-2.0
 
-package main // import "github.com/hashicorp/vault"
+package main // import "github.com/openbao/openbao"
 
 import (
 	"os"
 
 	"github.com/openbao/openbao/command"
-	"github.com/openbao/openbao/internal"
 )
-
-func init() {
-	// this is a good place to patch SHA-1 support back into x509
-	internal.PatchSha1()
-}
 
 func main() {
 	os.Exit(command.Run(os.Args[1:]))

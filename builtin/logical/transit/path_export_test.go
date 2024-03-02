4f1c2a21282cd5f438c6c6d47bb25a6e8fa1c610
Alexander Scheel <alexander.m.scheel@gmail.com>
2024-03-02 09:13:44 -0500

    Revert "Address a panic when exporting RSA public keys in transit (#24054) (#24116)"
    This partially reverts commit 402f3ef8e1ad05c99303186c9a3555638ec237d0,
    leaving the non-BUSL code but removing the inadvertent change to the
    test case that relicensed it to BUSL.
    
    Signed-off-by: Alexander Scheel <alexander.m.scheel@gmail.com>

--- builtin/logical/transit/path_export_test.go
+++ builtin/logical/transit/path_export_test.go
@@ -1,5 +1,5 @@
 // Copyright (c) HashiCorp, Inc.
-// SPDX-License-Identifier: BUSL-1.1
+// SPDX-License-Identifier: MPL-2.0
 
 package transit
 
@@ -8,51 +8,12 @@ import (
 	"fmt"
 	"reflect"
 	"strconv"
-	"strings"
 	"testing"
 
 	"github.com/openbao/openbao/sdk/logical"
 )
 
-func TestTransit_Export_Unknown_ExportType(t *testing.T) {
-	t.Parallel()
-
-	b, storage := createBackendWithSysView(t)
-	keyType := "ed25519"
-	req := &logical.Request{
-		Storage:   storage,
-		Operation: logical.UpdateOperation,
-		Path:      "keys/foo",
-		Data: map[string]interface{}{
-			"exportable": true,
-			"type":       keyType,
-		},
-	}
-	_, err := b.HandleRequest(context.Background(), req)
-	if err != nil {
-		t.Fatalf("failed creating key %s: %v", keyType, err)
-	}
-
-	req = &logical.Request{
-		Storage:   storage,
-		Operation: logical.ReadOperation,
-		Path:      "export/bad-export-type/foo",
-	}
-	rsp, err := b.HandleRequest(context.Background(), req)
-	if err == nil {
-		t.Fatalf("did not error on bad export type got: %v", rsp)
-	}
-	if rsp == nil || !rsp.IsError() {
-		t.Fatalf("response did not contain an error on bad export type got: %v", rsp)
-	}
-	if !strings.Contains(rsp.Error().Error(), "invalid export type") {
-		t.Fatalf("failed with unexpected error: %v", err)
-	}
-}
-
 func TestTransit_Export_KeyVersion_ExportsCorrectVersion(t *testing.T) {
-	t.Parallel()
-
 	verifyExportsCorrectVersion(t, "encryption-key", "aes128-gcm96")
 	verifyExportsCorrectVersion(t, "encryption-key", "aes256-gcm96")
 	verifyExportsCorrectVersion(t, "encryption-key", "chacha20-poly1305")
@@ -64,9 +25,6 @@ func TestTransit_Export_KeyVersion_ExportsCorrectVersion(t *testing.T) {
 	verifyExportsCorrectVersion(t, "signing-key", "ecdsa-p384")
 	verifyExportsCorrectVersion(t, "signing-key", "ecdsa-p521")
 	verifyExportsCorrectVersion(t, "signing-key", "ed25519")
-	verifyExportsCorrectVersion(t, "signing-key", "rsa-2048")
-	verifyExportsCorrectVersion(t, "signing-key", "rsa-3072")
-	verifyExportsCorrectVersion(t, "signing-key", "rsa-4096")
 	verifyExportsCorrectVersion(t, "hmac-key", "aes128-gcm96")
 	verifyExportsCorrectVersion(t, "hmac-key", "aes256-gcm96")
 	verifyExportsCorrectVersion(t, "hmac-key", "chacha20-poly1305")
@@ -76,13 +34,6 @@ func TestTransit_Export_KeyVersion_ExportsCorrectVersion(t *testing.T) {
 	verifyExportsCorrectVersion(t, "hmac-key", "ecdsa-p521")
 	verifyExportsCorrectVersion(t, "hmac-key", "ed25519")
 	verifyExportsCorrectVersion(t, "hmac-key", "hmac")
-	verifyExportsCorrectVersion(t, "public-key", "rsa-2048")
-	verifyExportsCorrectVersion(t, "public-key", "rsa-3072")
-	verifyExportsCorrectVersion(t, "public-key", "rsa-4096")
-	verifyExportsCorrectVersion(t, "public-key", "ecdsa-p256")
-	verifyExportsCorrectVersion(t, "public-key", "ecdsa-p384")
-	verifyExportsCorrectVersion(t, "public-key", "ecdsa-p521")
-	verifyExportsCorrectVersion(t, "public-key", "ed25519")
 }
 
 func verifyExportsCorrectVersion(t *testing.T, exportType, keyType string) {
@@ -179,8 +130,6 @@ func verifyExportsCorrectVersion(t *testing.T, exportType, keyType string) {
 }
 
 func TestTransit_Export_ValidVersionsOnly(t *testing.T) {
-	t.Parallel()
-
 	b, storage := createBackendWithSysView(t)
 
 	// First create a key, v1
@@ -281,8 +230,6 @@ func TestTransit_Export_ValidVersionsOnly(t *testing.T) {
 }
 
 func TestTransit_Export_KeysNotMarkedExportable_ReturnsError(t *testing.T) {
-	t.Parallel()
-
 	b, storage := createBackendWithSysView(t)
 
 	req := &logical.Request{
@@ -313,8 +260,6 @@ func TestTransit_Export_KeysNotMarkedExportable_ReturnsError(t *testing.T) {
 }
 
 func TestTransit_Export_SigningDoesNotSupportSigning_ReturnsError(t *testing.T) {
-	t.Parallel()
-
 	b, storage := createBackendWithSysView(t)
 
 	req := &logical.Request{
@@ -343,8 +288,6 @@ func TestTransit_Export_SigningDoesNotSupportSigning_ReturnsError(t *testing.T) 
 }
 
 func TestTransit_Export_EncryptionDoesNotSupportEncryption_ReturnsError(t *testing.T) {
-	t.Parallel()
-
 	testTransit_Export_EncryptionDoesNotSupportEncryption_ReturnsError(t, "ecdsa-p256")
 	testTransit_Export_EncryptionDoesNotSupportEncryption_ReturnsError(t, "ecdsa-p384")
 	testTransit_Export_EncryptionDoesNotSupportEncryption_ReturnsError(t, "ecdsa-p521")
@@ -379,52 +322,7 @@ func testTransit_Export_EncryptionDoesNotSupportEncryption_ReturnsError(t *testi
 	}
 }
 
-func TestTransit_Export_PublicKeyDoesNotSupportEncryption_ReturnsError(t *testing.T) {
-	t.Parallel()
-
-	testTransit_Export_PublicKeyNotSupported_ReturnsError(t, "chacha20-poly1305")
-	testTransit_Export_PublicKeyNotSupported_ReturnsError(t, "xchacha20-poly1305")
-	testTransit_Export_PublicKeyNotSupported_ReturnsError(t, "aes128-gcm96")
-	testTransit_Export_PublicKeyNotSupported_ReturnsError(t, "aes256-gcm96")
-	testTransit_Export_PublicKeyNotSupported_ReturnsError(t, "hmac")
-}
-
-func testTransit_Export_PublicKeyNotSupported_ReturnsError(t *testing.T, keyType string) {
-	b, storage := createBackendWithSysView(t)
-
-	req := &logical.Request{
-		Storage:   storage,
-		Operation: logical.UpdateOperation,
-		Path:      "keys/foo",
-		Data: map[string]interface{}{
-			"type": keyType,
-		},
-	}
-	if keyType == "hmac" {
-		req.Data["key_size"] = 32
-	}
-	_, err := b.HandleRequest(context.Background(), req)
-	if err != nil {
-		t.Fatalf("failed creating key %s: %v", keyType, err)
-	}
-
-	req = &logical.Request{
-		Storage:   storage,
-		Operation: logical.ReadOperation,
-		Path:      "export/public-key/foo",
-	}
-	_, err = b.HandleRequest(context.Background(), req)
-	if err == nil {
-		t.Fatalf("Key %s does not support public key exporting but was exported without error.", keyType)
-	}
-	if !strings.Contains(err.Error(), fmt.Sprintf("unknown key type %s for export type public-key", keyType)) {
-		t.Fatalf("unexpected error value for key type: %s: %v", keyType, err)
-	}
-}
-
 func TestTransit_Export_KeysDoesNotExist_ReturnsNotFound(t *testing.T) {
-	t.Parallel()
-
 	b, storage := createBackendWithSysView(t)
 
 	req := &logical.Request{
@@ -440,8 +338,6 @@ func TestTransit_Export_KeysDoesNotExist_ReturnsNotFound(t *testing.T) {
 }
 
 func TestTransit_Export_EncryptionKey_DoesNotExportHMACKey(t *testing.T) {
-	t.Parallel()
-
 	b, storage := createBackendWithSysView(t)
 
 	req := &logical.Request{

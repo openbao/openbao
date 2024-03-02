57be42cd4acdc9e22b653bb9e149d793628cabf9
Alexander Scheel <alexander.m.scheel@gmail.com>
2024-03-02 09:13:38 -0500

    Revert "manual backport (#24035)"
    This reverts commit ed85e15d841c494122e9a3d39d920d636d2f7899 to remove
    BUSL code.
    
    Signed-off-by: Alexander Scheel <alexander.m.scheel@gmail.com>

--- ui/lib/open-api-explorer/addon/routes/index.js
+++ ui/lib/open-api-explorer/addon/routes/index.js
@@ -1,14 +1,16 @@
 /**
  * Copyright (c) HashiCorp, Inc.
- * SPDX-License-Identifier: BUSL-1.1
+ * SPDX-License-Identifier: MPL-2.0
  */
 
 import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
 
-export default class OpenApiExplorerIndex extends Route {
-  @service flashMessages;
-
+export default Route.extend({
+  flashMessages: service(),
+  // without an empty model hook here, ember likes to use the parent model, and then things get weird with
+  // query params, so here we're no-op'ing the model hook
+  model() {},
   afterModel() {
     const warning = `The "Try it out" functionality in this API explorer will make requests to this OpenBao server on your behalf.
 
@@ -19,5 +21,5 @@ Your token will also be shown on the screen in the example curl command output.`
       sticky: true,
       preformatted: true,
     });
-  }
-}
+  },
+});

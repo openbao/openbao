c075f0ef2c7e19667f6b2aea217d0298ba06a6c3
Jan Martens <jan@martens.eu.org>
2024-03-21 21:05:32 +0100

    Add landingpage to website
    This adds a simple landingpage to the existing website showing use-cases
    for OpenBao, a FAQ section, how to contribute and who is supporting the
    project.
    
    Signed-off-by: Jan Martens <jan@martens.eu.org>

--- website/src/components/HomepageFeatures/index.tsx
+++ website/src/components/HomepageFeatures/index.tsx
@@ -1,55 +1,85 @@
-import clsx from 'clsx';
-import Heading from '@theme/Heading';
-import styles from './styles.module.css';
+import clsx from "clsx";
+import Heading from "@theme/Heading";
 
 type FeatureItem = {
   title: string;
-  Svg: React.ComponentType<React.ComponentProps<'svg'>>;
   description: JSX.Element;
 };
 
 const FeatureList: FeatureItem[] = [
   {
-    title: 'Easy to Use',
-    Svg: require('@site/public/img/undraw_docusaurus_mountain.svg').default,
+    title: "Secure Secret Storage",
     description: (
       <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
+        Arbitrary key/value secrets can be stored in OpenBao. OpenBao encrypts
+        these secrets prior to writing them to persistent storage, so gaining
+        access to the raw storage is not enough to access your secrets.
       </>
     ),
   },
   {
-    title: 'Focus on What Matters',
-    Svg: require('@site/public/img/undraw_docusaurus_tree.svg').default,
+    title: "Dynamic Secrets",
     description: (
       <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
+        OpenBao can generate secrets on-demand for some systems, such as
+        Kubernetes or SQL databases. After creating these dynamic secrets,
+        OpenBao will also automatically revoke them after the lease is up.
       </>
     ),
   },
   {
-    title: 'Powered by React',
-    Svg: require('@site/public/img/undraw_docusaurus_react.svg').default,
+    title: "Data Encryption",
     description: (
       <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
+        OpenBao provides encryption as a service with centralized key management
+        to simplify encrypting data in transit and stored across clouds and
+        datacenters.
+      </>
+    ),
+  },
+  {
+    title: "Identity based access",
+    description: (
+      <>
+        Organizations need a way to manage identity sprawl with the use of
+        different clouds, services, and systems. OpenBao solves this challenge
+        by using a unified ACL system to broker access to systems and secrets
+        and merges identities across providers.
+      </>
+    ),
+  },
+  {
+    title: "Leasing and Renewal",
+    description: (
+      <>
+        All secrets in OpenBao have a lease associated with them. At the end of
+        the lease, OpenBao will automatically revoke that secret. Clients are
+        able to renew leases via built-in renew APIs.
+      </>
+    ),
+  },
+  {
+    title: "Revocation",
+    description: (
+      <>
+        OpenBao has built-in support for secret revocation. OpenBao can revoke
+        not only single secrets, but a tree of secrets, for example all secrets
+        read by a specific user, or all secrets of a particular type.
       </>
     ),
   },
 ];
 
-function Feature({title, Svg, description}: FeatureItem) {
+function Feature({ title, description }: FeatureItem) {
   return (
-    <div className={clsx('col col--4')}>
-      <div className="text--center">
-        <Svg className={styles.featureSvg} role="img" />
-      </div>
-      <div className="text--center padding-horiz--md">
-        <Heading as="h3">{title}</Heading>
-        <p>{description}</p>
+    <div className={clsx("col col--4 padding-bottom--lg")}>
+      <div className="card card--full-height">
+        <div className="card__header">
+          <h3>{title}</h3>
+        </div>
+        <div className="card__body">
+          <p>{description}</p>
+        </div>
       </div>
     </div>
   );
@@ -57,8 +87,11 @@ function Feature({title, Svg, description}: FeatureItem) {
 
 export default function HomepageFeatures(): JSX.Element {
   return (
-    <section className={styles.features}>
+    <section className="padding-vert--md margin-vert--lg">
       <div className="container">
+        <Heading as="h2" className="hero__title text--center">
+          Use cases
+        </Heading>
         <div className="row">
           {FeatureList.map((props, idx) => (
             <Feature key={idx} {...props} />

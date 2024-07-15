70abaca1171439e0c4710bf4271dd6c0294915d8
Erick Bajao <fcbajao@gmail.com>
2024-07-16 01:36:39 +0800

    Add system as OperationSuffix to initialize
    Signed-off-by: Erick Bajao <fcbajao@gmail.com>

--- vault/logical_system_paths.go
+++ vault/logical_system_paths.go
@@ -583,7 +583,8 @@ func (b *SystemBackend) configPaths() []*framework.Path {
 				},
 				logical.UpdateOperation: &framework.PathOperation{
 					DisplayAttrs: &framework.DisplayAttributes{
-						OperationVerb: "initialize",
+						OperationVerb:   "initialize",
+						OperationSuffix: "system",
 					},
 					Summary:     "Initialize a new OpenBao instance.",
 					Description: "The OpenBao instance must not have been previously initialized. The recovery options, as well as the stored shares option, are only available when using OpenBao HSM.",

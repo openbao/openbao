eaeb84a45ee65671272a6fece612beab9a7be770
SADIK KUZU <sadikkuzu@hotmail.com>
2024-08-08 03:28:21 +0300

    Fix future requests part in login success message
    Co-authored-by: Alexander Scheel <alexander.m.scheel@gmail.com>
    Signed-off-by: SADIK KUZU <sadikkuzu@hotmail.com>

--- command/login.go
+++ command/login.go
@@ -343,7 +343,7 @@ func (c *LoginCommand) Run(args []string) int {
 		c.UI.Output(wrapAtLength(
 			"Success! You are now authenticated. The token information displayed "+
 				"below is already stored in the token helper. You do NOT need to run "+
-				"\"bao login\" again. Future Vault requests will automatically use "+
+				"\"bao login\" again. Future OpenBao requests will automatically use "+
 				"this token.") + "\n")
 	}
 

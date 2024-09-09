hashicorp/vault

Cleanup Okta LoginMFA tests

vault/external_tests/identity/login_mfa_okta_test.go
@@ -12,7 +12,9 @@ import (
 
 	"github.com/hashicorp/go-secure-stdlib/password"
 	"github.com/hashicorp/vault/api"
+	"github.com/hashicorp/vault/helper/constants"
 	"github.com/hashicorp/vault/helper/testhelpers"
+	logicaltest "github.com/hashicorp/vault/helper/testhelpers/logical"
 	"github.com/hashicorp/vault/helper/testhelpers/minimal"
 )
 
@@ -24,9 +26,9 @@ import (
 // OKTA_PASSWORD=<find in 1password>
 //
 // You will need to install the Okta client app on your mobile device and
-// setup MFA in order to use the Okta web UI.  This test does not exercise
+// setup MFA in order to use the Okta web UI. This test does not exercise
 // MFA however (which is an enterprise feature), and therefore the test
-// user in OKTA_USERNAME should not be configured with it.  Currently
+// user in OKTA_USERNAME should not be configured with it. Currently,
 // test3@example.com is not a member of testgroup, which is the group with
 // the profile that requires MFA. If you need to use a different group name
 // for the test group, you can set:
@@ -36,7 +38,7 @@ import (
 // OKTA_PROMPT_FOR_TOTP=1
 
 func TestOktaEngineMFA(t *testing.T) {
-	if os.Getenv("VAULT_ACC") == "" {
+	if os.Getenv(logicaltest.TestEnvVar) == "" {
 		t.Skip("This test requires manual intervention and OKTA verify on cellphone is needed")
 	}
 
@@ -85,13 +87,16 @@ func TestOktaEngineMFA(t *testing.T) {
 		"password": os.Getenv("OKTA_PASSWORD"),
 	})
 	if err != nil {
-		t.Fatalf("error configuring okta group, %v", err)
+		t.Fatalf("error logging in, %v", err)
 	}
 }
 
 func TestInteg_PolicyMFAOkta(t *testing.T) {
-	if os.Getenv("VAULT_ACC") == "" {
+	if os.Getenv(logicaltest.TestEnvVar) == "" {
 		t.Skip("This test requires manual intervention and OKTA verify on cellphone is needed")
+	}
+	if !constants.IsEnterprise {
+		t.Skip("PolicyMFA is an enterprise-only feature")
 	}
 
 	// Ensure each cred is populated.
@@ -192,7 +197,7 @@ path "secret/foo" {
 }
 
 func TestInteg_LoginMFAOkta(t *testing.T) {
-	if os.Getenv("VAULT_ACC") == "" {
+	if os.Getenv(logicaltest.TestEnvVar) == "" {
 		t.Skip("This test requires manual intervention and OKTA verify on cellphone is needed")
 	}
 

openbao/openbao

Signed-off-by: Jacob Floyd <cognifloyd@gmail.com>

vault/external_tests/identity/login_mfa_okta_test.go
@@ -12,8 +12,10 @@ import (
 
 	"github.com/hashicorp/go-secure-stdlib/password"
 	"github.com/openbao/openbao/api"
+	"github.com/openbao/openbao/helper/constants"
 	"github.com/openbao/openbao/builtin/credential/userpass"
 	"github.com/openbao/openbao/helper/testhelpers"
+	logicaltest "github.com/openbao/openbao/helper/testhelpers/logical"
 	vaulthttp "github.com/openbao/openbao/http"
 	"github.com/openbao/openbao/sdk/logical"
 	"github.com/openbao/openbao/vault"
@@ -26,9 +28,9 @@ import (
 // OKTA_USERNAME=test3@example.com
 //
 // You will need to install the Okta client app on your mobile device and
-// setup MFA in order to use the Okta web UI.  This test does not exercise
+// setup MFA in order to use the Okta web UI. This test does not exercise
 // MFA however (which is an enterprise feature), and therefore the test
-// user in OKTA_USERNAME should not be configured with it.  Currently
+// user in OKTA_USERNAME should not be configured with it. Currently,
 // test3@example.com is not a member of testgroup, which is the group with
 // the profile that requires MFA.
 //
@@ -42,8 +44,11 @@ var identityOktaMFACoreConfig = &vault.CoreConfig{
 }
 
 func TestInteg_PolicyMFAOkta(t *testing.T) {
-	if os.Getenv("VAULT_ACC") == "" {
+	if os.Getenv(logicaltest.TestEnvVar) == "" {
 		t.Skip("This test requires manual intervention and OKTA verify on cellphone is needed")
+	}
+	if !constants.IsEnterprise {
+		t.Skip("PolicyMFA is an enterprise-only feature")
 	}
 
 	// Ensure each cred is populated.
@@ -149,7 +154,7 @@ path "secret/foo" {
 }
 
 func TestInteg_LoginMFAOkta(t *testing.T) {
-	if os.Getenv("VAULT_ACC") == "" {
+	if os.Getenv(logicaltest.TestEnvVar) == "" {
 		t.Skip("This test requires manual intervention and OKTA verify on cellphone is needed")
 	}
 
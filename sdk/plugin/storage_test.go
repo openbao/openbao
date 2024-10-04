f4816b21e9c291b11c8b0a3eb65eeb9b2be3cd02
Alexander Scheel <ascheel@gitlab.com>
2024-10-04 10:29:32 -0400

    Add transactions to plugin GRPC layer
    This adds support for transactions to the plugin GRPC layer,
    opportunistically upgrading the client storage implementation to a
    transactional aware one if the underlying storage implementation on the
    server supports it. This retains bidirectional interoperability with
    upstream, allowing plugins compiled with OpenBao's SDK to continue to
    work with upstream server implementations and visa-versa.
    
    Signed-off-by: Alexander Scheel <ascheel@gitlab.com>

--- sdk/plugin/storage_test.go
+++ sdk/plugin/storage_test.go
@@ -9,8 +9,11 @@ import (
 
 	"google.golang.org/grpc"
 
+	log "github.com/hashicorp/go-hclog"
 	"github.com/hashicorp/go-plugin"
+	"github.com/openbao/openbao/sdk/v2/helper/logging"
 	"github.com/openbao/openbao/sdk/v2/logical"
+	"github.com/openbao/openbao/sdk/v2/physical/inmem"
 	"github.com/openbao/openbao/sdk/v2/plugin/pb"
 )
 
@@ -35,6 +38,37 @@ func TestStorage_GRPC(t *testing.T) {
 	defer client.Close()
 
 	testStorage := &GRPCStorageClient{client: pb.NewStorageClient(client)}
+
+	logical.TestStorage(t, testStorage)
+}
+
+func TestStorage_GRPCTransaction(t *testing.T) {
+	physical, err := inmem.NewInmem(nil, logging.NewVaultLogger(log.Trace))
+	if err != nil {
+		t.Fatalf("failed to create backend: %v", err)
+	}
+
+	storage := logical.NewLogicalStorage(physical)
+
+	client, _ := plugin.TestGRPCConn(t, func(s *grpc.Server) {
+		pb.RegisterStorageServer(s, &GRPCStorageServer{
+			impl: storage,
+		})
+	})
+	defer client.Close()
+
+	testStorage, err := newGRPCStorageClient(context.TODO(), client)
+	if err != nil {
+		t.Fatalf("failed to create client: %v", err)
+	}
+
+	if _, ok := storage.(logical.Transactional); !ok {
+		t.Fatalf("expected base storage to be transactional but wasn't")
+	}
+
+	if _, ok := testStorage.(logical.Transactional); !ok {
+		t.Fatalf("expected client to be transactional but wasn't")
+	}
 
 	logical.TestStorage(t, testStorage)
 }

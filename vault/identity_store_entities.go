34ee84911435ee6be559e9f9ecee8d39e18a8813
Alexander Scheel <ascheel@gitlab.com>
2024-11-12 15:42:19 -0500

    Fix root namespace permission elevation (#695)
    * Clone entity on modifications
    
    When modifying an entity, ensure we clone the MemDB copy so that we do
    not accidentally modify the value stored in the underlying MemDB. This
    ensures we can use it as a staging area and only persist changes on
    explicit upsert.
    
    Signed-off-by: Alexander Scheel <ascheel@gitlab.com>
    
    * Clone entity on invalidations
    
    While the invalidation should only be triggered when storage is
    writable, if we have an alias invalidation on an entity, we should
    only modify the in-memory entity on upsert, not when the alias is
    added to the entity. This ensures failures to upsert do not cause a
    modified entity to be present in memory.
    
    Signed-off-by: Alexander Scheel <ascheel@gitlab.com>
    
    * Add changelog entry
    
    Signed-off-by: Alexander Scheel <ascheel@gitlab.com>
    
    ---------
    
    Signed-off-by: Alexander Scheel <ascheel@gitlab.com>

--- vault/identity_store_entities.go
+++ vault/identity_store_entities.go
@@ -329,7 +329,7 @@ func (i *IdentityStore) handleEntityUpdateCommon() framework.OperationFunc {
 		// Get the name
 		entityName := d.Get("name").(string)
 		if entityName != "" {
-			entityByName, err := i.MemDBEntityByName(ctx, entityName, false)
+			entityByName, err := i.MemDBEntityByName(ctx, entityName, true)
 			if err != nil {
 				return nil, err
 			}

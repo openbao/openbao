c88134da96a550e25fd3e73ce1d8ae83e716c793
Alexander Scheel <ascheel@gitlab.com>
2025-02-12 21:47:03 -0600

    Fix additional uses of formatters with constants
    Signed-off-by: Alexander Scheel <ascheel@gitlab.com>

--- vault/diagnose/output.go
+++ vault/diagnose/output.go
@@ -119,7 +119,7 @@ func (t *TelemetryCollector) OnStart(_ context.Context, s sdktrace.ReadWriteSpan
 	defer t.mu.Unlock()
 	t.spans[s.SpanContext().SpanID()] = s
 	if isMainSection(s) {
-		fmt.Fprintf(t.ui, status_unknown+s.Name())
+		fmt.Fprint(t.ui, status_unknown+s.Name())
 	}
 }
 

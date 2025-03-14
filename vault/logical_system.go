a56afdaf9dde4e9fe6558f7fda013cb7769fb85f
Jonas Köhnen <mail@satoqz.net>
2025-03-14 15:24:17 +0100

    Polish namespaces API (#1041)
    * namespaces api: fix list namespaces response body
    
    I'm not sure why the namespaces endpoints were marked to inject data
    into the toplevel. Up to discussion, from the API docs it seems
    they shouldn't be?
    
    Resolves #1037.
    
    Signed-off-by: Jonas Köhnen <jonas.koehnen@sap.com>
    
    * namespaces api: return namespace from create/patch
    
    Resolves part 1 of #1022.
    
    Signed-off-by: Jonas Köhnen <jonas.koehnen@sap.com>
    
    * namespaces api: improve patch error message
    
    Previously when attempting to PATCH a non-existent namespace we would
    hit the "failed validating namespace: path is missing; cannot validate root namespace"
    response, which wasn't all too user-friendly.
    
    Signed-off-by: Jonas Köhnen <jonas.koehnen@sap.com>
    
    * namespaces api: polish openapi schema
    
    Signed-off-by: Jonas Köhnen <jonas.koehnen@sap.com>
    
    * namespaces api: doc comment consistency
    
    Signed-off-by: Jonas Köhnen <jonas.koehnen@sap.com>
    
    * namespaces api: remove GET for list endpoint
    
    Signed-off-by: Jonas Köhnen <jonas.koehnen@sap.com>
    
    * namespaces api: stub handler for future scan support
    
    Signed-off-by: Jonas Köhnen <jonas.koehnen@sap.com>
    
    * namespaces api: add tests
    
    Signed-off-by: Jonas Köhnen <jonas.koehnen@sap.com>
    
    ---------
    
    Signed-off-by: Jonas Köhnen <jonas.koehnen@sap.com>

--- vault/logical_system.go
+++ vault/logical_system.go
@@ -5788,4 +5788,35 @@ This path responds to the following HTTP methods.
         Returns a list historical version changes sorted by installation time in ascending order.
 		`,
 	},
+
+	"list-namespaces": {
+		"List namespaces.",
+		`
+This path responds to the following HTTP methods.
+
+	LIST /
+		List namespaces.
+
+	SCAN /
+		Scan (recursively list) namespaces.
+		`,
+	},
+	"namespaces": {
+		"Create, read, update and delete namespaces.",
+		`
+This path responds to the following HTTP methods.
+
+	GET /<path>
+		Retrieve a namespace.
+
+	PUT /<path>
+		Create or update a namespace.
+
+	PATCH /<path>
+		Update a namespace's custom metadata.
+
+	DELETE /<path>
+		Delete a namespace.
+		`,
+	},
 }

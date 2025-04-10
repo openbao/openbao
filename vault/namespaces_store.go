7126610ab7a53175fc1e2afbb8e1c3287cefbfd1
Tom Gehrke <tom.gehrke@sap.com>
2025-04-10 16:54:03 +0200

    extract namespace from path instead of context
    This let's us treat these the same when applying quotas:
    
    - bao kv get -ns ns1 -mount kv testkey
    - bao kv get -mount ns1/kv testkey
    
    Before, the second command was not affected by a quota on ns1
    
    Signed-off-by: Tom Gehrke <tom.gehrke@sap.com>

--- vault/namespaces_store.go
+++ vault/namespaces_store.go
@@ -445,6 +445,24 @@ func (ns *NamespaceStore) GetNamespaceByAccessor(ctx context.Context, id string)
 	return item.Clone(), nil
 }
 
+func (ns *NamespaceStore) GetNamespaceByLongestPrefix(ctx context.Context, path string) (*namespace.Namespace, string) {
+	ctxNs, err := namespace.FromContext(ctx)
+	if err != nil {
+		ctxNs = namespace.RootNamespace
+	}
+
+	// e.g. needed for /sys/unseal, where the namespace store is not yet initialized
+	if ns == nil {
+		return ctxNs, path
+	}
+
+	combinedPath := ctxNs.Path + path
+	ns.lock.RLock()
+	prefix, entry, _ := ns.namespacesByPath.LongestPrefix(combinedPath)
+	ns.lock.RUnlock()
+	return entry, strings.TrimPrefix(combinedPath, prefix)
+}
+
 // GetNamespaceByPath is used to fetch the namespace with the given full path.
 func (ns *NamespaceStore) GetNamespaceByPath(ctx context.Context, path string) (*namespace.Namespace, error) {
 	defer metrics.MeasureSince([]string{"namespace", "get_namespace_by_path"}, time.Now())

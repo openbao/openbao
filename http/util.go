211e4c4e892621bae29dca976e0017c58cbb7ce4
Jonas Köhnen <jonas.koehnen@sap.com>
2025-04-28 13:42:28 +0200

    resolve namespace in core
    ...and also in http, but just once to apply quotas on a best-effort
    basis. discard namespace afterwards.
    
    Signed-off-by: Jonas Köhnen <jonas.koehnen@sap.com>

--- http/util.go
+++ http/util.go
@@ -91,6 +91,7 @@ func rateLimitQuotaWrapping(handler http.Handler, core *vault.Core) http.Handler
 			Type:          quotas.TypeRateLimit,
 			Path:          path,
 			MountPath:     mountPath,
+			NamespacePath: ns.Path,
 			ClientAddress: parseRemoteIPAddress(r),
 		}
 

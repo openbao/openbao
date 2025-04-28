f33f9a51b42659bc89d0693e640adc6bf2288711
Lucas Gehlen <lucas.gehlen@sap.com>
2025-04-28 09:29:45 +0200

    Added a "--pr" flag to only run test coverage for files that were changed Added a "--json" flag to prints the coverage output as a JSON string Added a filter to only run specific tests matching a given string
    Signed-off-by: Lucas Gehlen <lucas.gehlen@sap.com>

--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -7,10 +7,12 @@
 # Works around the fact that `go test -coverprofile` currently does not work
 # with multiple packages, see https://code.google.com/p/go/issues/detail?id=6909
 #
-# Usage: script/coverage [--html|--coveralls]
+# Usage: script/coverage [--html|--coveralls|--json] [--pr|<package>]
 #
 #     --html      Additionally create HTML report and open it in browser
 #     --coveralls Push coverage statistics to coveralls.io
+#     --json      Output coverage data in JSON format
+#     --pr        Only include packages changed in the current PR
 #
 
 set -e
@@ -25,7 +27,8 @@ generate_cover_data() {
 
     for pkg in "$@"; do
         f="$workdir/$(echo $pkg | tr / -).cover"
-        go test -covermode="$mode" -coverprofile="$f" "$pkg"
+        # gotestsum --format=short-verbose -- -covermode="$mode" -coverprofile="$f" "$pkg"
+        go test -v -covermode="$mode" -coverprofile="$f" "$pkg"
     done
 
     echo "mode: $mode" >"$profile"
@@ -41,15 +44,32 @@ push_to_coveralls() {
     goveralls -coverprofile="$profile"
 }
 
-generate_cover_data $(go list ./... | grep -v /vendor/)
+# Get packages to test
+if [ "$2" = "--pr" ]; then
+    pkgs=$(git diff --name-only origin/main...HEAD | grep '\.go$' | xargs -n1 dirname | sort -u | uniq | xargs -I{} go list ./{} 2>/dev/null)
+elif [ -n "$2" ]; then
+    pkgs=$(go list ./... | grep -v /vendor/ | grep "$2")
+else
+    pkgs=$(go list ./... | grep -v /vendor/)
+fi
+
+generate_cover_data $pkgs
+
 show_cover_report func
+
 case "$1" in
-"")
-    ;;
---html)
-    show_cover_report html ;;
---coveralls)
-    push_to_coveralls ;;
-*)
-    echo >&2 "error: invalid option: $1"; exit 1 ;;
+    "")
+        ;;
+    --html)
+        show_cover_report html
+        ;;
+    --json)
+        cat "$profile" | jq -R -s -c 'split("\n") | map(select(length > 0))'
+        ;;
+    --coveralls)
+        push_to_coveralls
+        ;;
+    *)
+        echo >&2 "error: invalid option: $2"; exit 1
+        ;;
 esac

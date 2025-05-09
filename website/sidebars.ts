068b2893fbd5c8f0f728a6ff2b28d46a811c7bb9
Alexander Scheel <ascheel@gitlab.com>
2025-05-09 07:56:38 -0500

    Add RFC for inline authentication
    Signed-off-by: Alexander Scheel <ascheel@gitlab.com>

--- website/sidebars.ts
+++ website/sidebars.ts
@@ -504,6 +504,7 @@ const sidebars: SidebarsConfig = {
                 "rfcs/acl-paginated-lists",
                 "rfcs/ssh-ca-multi-issuer",
                 "rfcs/cel-best-practices",
+                "rfcs/inline-auth",
             ],
             FAQ: ["faq/index", "deprecation/faq", "auth/login-mfa/faq"],
         },

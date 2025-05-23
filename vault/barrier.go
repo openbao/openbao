cea695bcacb07e38564dc5ce1031c0381f23fb61
Lucas Gehlen <lucas.gehlen@sap.com>
2025-05-23 11:22:11 +0200

    Support CLI namespace seal operations
    - SealConfigs can now be passed as a JSON file to the bao namespace create command
    - Added custom Unmarshalling to seal.go because values of SealConfigs that where sent over the API where converted to strings
    - Namespace sealing api implementation
    
    Signed-off-by: Lucas Gehlen <lucas.gehlen@sap.com>
    Signed-off-by: Tom Gehrke <tom.gehrke@sap.com>
    Signed-off-by: Wojciech Slabosz <wojciech.slabosz@sap.com>

--- vault/barrier.go
+++ vault/barrier.go
@@ -89,7 +89,7 @@ type SecurityBarrierCore interface {
 
 	// Sealed checks if the barrier has been unlocked yet. The Barrier
 	// is not expected to be able to perform any CRUD until it is unsealed.
-	Sealed() (bool, error)
+	Sealed() bool
 
 	// Unseal is used to provide the unseal key which permits the barrier
 	// to be unsealed. If the key is not correct, the barrier remains sealed.
--- vault/logical_system.go
+++ vault/logical_system.go
@@ -5886,6 +5886,12 @@ This path responds to the following HTTP methods.
 
 	DELETE /<path>
 		Delete a namespace.
+
+	POST /<name>/seal
+		Seal a namespace.
+
+	POST /<name/unseal
+		Unseal a namespace.
 		`,
 	},
 	"namespaces-lock": {

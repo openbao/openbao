7b47d4a77113dea6c304e702bafce114d1c87a94
Geoff Wilson <geoff@gr-oss.io>
2025-06-16 12:12:56 -0400

    Undo timing change to integ_test
    Signed-off-by: Geoff Wilson <geoff@gr-oss.io>

--- vault/expiration_integ_test.go
+++ vault/expiration_integ_test.go
@@ -42,8 +42,8 @@ func TestExpiration_RenewToken_TestCluster(t *testing.T) {
 
 	// Tune the mount
 	err = client.Sys().TuneMount("auth/approle", api.MountConfigInput{
-		DefaultLeaseTTL: "7s",
-		MaxLeaseTTL:     "7s",
+		DefaultLeaseTTL: "5s",
+		MaxLeaseTTL:     "5s",
 	})
 	if err != nil {
 		t.Fatal(err)
@@ -51,7 +51,7 @@ func TestExpiration_RenewToken_TestCluster(t *testing.T) {
 
 	// Create role
 	resp, err := client.Logical().Write("auth/approle/role/role-period", map[string]interface{}{
-		"period": "7s",
+		"period": "5s",
 	})
 	if err != nil {
 		t.Fatal(err)
@@ -164,7 +164,7 @@ func TestExpiration_RenewToken_TestCluster(t *testing.T) {
 		t.Fatalf("unable to convert ttl to int: %s", err)
 	}
 	ttl = time.Duration(ttlInt) * time.Second
-	if ttl < 6*time.Second {
-		t.Fatal("expected ttl value to be around 7s")
+	if ttl < 4*time.Second {
+		t.Fatal("expected ttl value to be around 5s")
 	}
 }

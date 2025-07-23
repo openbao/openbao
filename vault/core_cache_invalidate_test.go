1207eb4d3b0dc36abd7008fc65c5e46290db7d56
Philipp Stehle <anderschwiedu@gmail.com>
2025-07-23 13:39:23 +0200

    Implement cache invalidation for namespaces and policies (#1551)
    * Implement cache invalidation for namespaces and policies
    
    Signed-off-by: Philipp Stehle <philipp.stehle@secretz.io>
    
    * Use constant
    
    Co-authored-by: Jonas Köhnen <mail@satoqz.net>
    Signed-off-by: Philipp Stehle <philipp.stehle@secretz.io>
    
    * also invalidate policies when invalidating namespace
    
    Signed-off-by: Philipp Stehle <philipp.stehle@secretz.io>
    
    * add timeout to cache invalidation
    
    Signed-off-by: Philipp Stehle <philipp.stehle@secretz.io>
    
    * Check for context cancellation during policy invalidation
    
    Signed-off-by: Philipp Stehle <philipp.stehle@secretz.io>
    
    ---------
    
    Signed-off-by: Philipp Stehle <philipp.stehle@secretz.io>
    Co-authored-by: Jonas Köhnen <mail@satoqz.net>

---
+++ vault/core_cache_invalidate_test.go
@@ -0,0 +1,138 @@
+// Copyright (c) 2025 OpenBao a Series of LF Projects, LLC
+// SPDX-License-Identifier: MPL-2.0
+
+package vault
+
+import (
+	"context"
+	"fmt"
+	"testing"
+	"time"
+
+	"github.com/go-test/deep"
+	"github.com/openbao/openbao/helper/namespace"
+	"github.com/openbao/openbao/sdk/v2/logical"
+)
+
+func TestCore_Invalidate_Namespaces(t *testing.T) {
+	c, _, root := TestCoreUnsealed(t)
+
+	// 1. Create some namespace to populate cache
+	ns := &namespace.Namespace{
+		ID:   "ns",
+		Path: "ns",
+		CustomMetadata: map[string]string{
+			"testkey": "initial value",
+		},
+	}
+
+	TestCoreCreateNamespaces(t, c, ns)
+
+	// 2. Manipulate Storage
+	clone := *ns
+	clone.CustomMetadata["testkey"] = "updated value"
+
+	storagePath := "core/namespaces/" + ns.UUID
+	newEntry, err := logical.StorageEntryJSON(storagePath, clone)
+	if err != nil {
+		t.Fatalf("err: %v", err)
+	}
+
+	err = c.barrier.Put(context.TODO(), newEntry)
+	if err != nil {
+		t.Fatalf("err: %v", err)
+	}
+
+	// 3. Invalidate Path
+	c.Invalidate(storagePath)
+
+	// 4. Check cache was properly invalidated
+	req := logical.TestRequest(t, logical.ReadOperation, "sys/namespaces/ns")
+	req.ClientToken = root
+	resp, err := c.HandleRequest(namespace.RootContext(nil), req)
+	if err != nil {
+		t.Fatalf("err: %v", err)
+	}
+
+	if diff := deep.Equal(resp.Data["custom_metadata"], map[string]string{
+		"testkey": "updated value",
+	}); diff != nil {
+		t.Error(diff)
+	}
+}
+
+func TestCore_Invalidate_Policy(t *testing.T) {
+	testCases := map[string]func(t *testing.T, c *Core) (storagePath string, ctx context.Context){
+		"global": func(t *testing.T, c *Core) (storagePath string, ctx context.Context) {
+			return "sys/policy/test-policy", namespace.RootContext(t.Context())
+		},
+
+		"local": func(t *testing.T, c *Core) (storagePath string, ctx context.Context) {
+			ns := &namespace.Namespace{
+				ID:   "ns",
+				Path: "ns",
+			}
+			TestCoreCreateNamespaces(t, c, ns)
+
+			return fmt.Sprintf("namespaces/%s/sys/policy/test-policy", ns.UUID), namespace.ContextWithNamespace(t.Context(), ns)
+		},
+	}
+
+	for name, init := range testCases {
+		t.Run(name, func(t *testing.T) {
+			c, _, root := TestCoreUnsealed(t)
+			storagePath, ctx := init(t, c)
+
+			// 1. Create some policy to populate cache
+			req := logical.TestRequest(t, logical.CreateOperation, "sys/policy/test-policy")
+			req.ClientToken = root
+			req.Data = map[string]interface{}{
+				"policy": `
+					path "test/path/*" {
+						capabilities = ["read"]
+					}
+			`,
+			}
+			resp, err := c.HandleRequest(ctx, req)
+			if err != nil {
+				t.Fatalf("err: %v", err)
+			}
+
+			if resp.IsError() {
+				t.Fatalf("err: %v", resp.Error())
+			}
+
+			// 2. Manipulate Storage
+			policy, err := c.policyStore.GetPolicy(ctx, "test-policy", PolicyTypeACL)
+			if err != nil {
+				t.Fatalf("err: %v", err)
+			}
+
+			clone := policy.ShallowClone()
+			clone.Expiration = time.Date(2099, 1, 1, 12, 0, 0, 0, time.UTC)
+
+			newEntry, err := logical.StorageEntryJSON(storagePath, clone)
+			if err != nil {
+				t.Fatalf("err: %v", err)
+			}
+
+			err = c.barrier.Put(ctx, newEntry)
+			if err != nil {
+				t.Fatalf("err: %v", err)
+			}
+
+			// 3. Invalidate Path
+			c.Invalidate(storagePath)
+
+			// 4. Check cache was properly invalidated
+			updatedPolicy, err := c.policyStore.GetPolicy(ctx, "test-policy", PolicyTypeACL)
+			if err != nil {
+				t.Fatalf("err: %v", err)
+			}
+
+			if updatedPolicy.Expiration != clone.Expiration {
+				t.Errorf("invalidation did not work: expected to see updated expiry time, got: %v", updatedPolicy.Expiration)
+			}
+		})
+	}
+}

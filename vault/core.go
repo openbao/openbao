8d176376af81e85af417623e96401457acd881b9
Philipp Stehle <anderschwiedu@gmail.com>
2025-07-28 14:04:56 +0200

    Fix error comment
    Co-authored-by: Jonas Köhnen <mail@satoqz.net>
    Signed-off-by: Philipp Stehle <anderschwiedu@gmail.com>

--- vault/core.go
+++ vault/core.go
@@ -142,7 +142,7 @@ var (
 	// enabled in the configuration file
 	ErrIntrospectionNotEnabled = errors.New("The Vault configuration must set \"introspection_endpoint\" to true to enable this endpoint")
 
-	// ErrNoMatchingMount is returned if the mount is not found
+	// errNoMatchingMount is returned if the mount is not found
 	errNoMatchingMount = errors.New("no matching mount")
 
 	// manualStepDownSleepPeriod is how long to sleep after a user-initiated

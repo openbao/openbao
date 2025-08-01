dcbb7a776997ba2524a371c853e88006f477a02e
Philipp Stehle <philipp.stehle@secretz.io>
2025-08-01 10:57:05 +0200

    test all the files
    Signed-off-by: Philipp Stehle <philipp.stehle@secretz.io>

--- command/operator_validate_config_test.go
+++ command/operator_validate_config_test.go
@@ -7,10 +7,13 @@ package command
 
 import (
 	"io"
+	"os"
+	"path"
 	"testing"
 
 	"github.com/hashicorp/cli"
 	"github.com/openbao/openbao/vault/diagnose"
+	"github.com/stretchr/testify/require"
 )
 
 func testOperatorValidateConfigCommand(tb testing.TB) *OperatorValidateConfigCommand {
@@ -27,57 +30,70 @@ func testOperatorValidateConfigCommand(tb testing.TB) *OperatorValidateConfigCom
 
 func TestOperatorValidateConfigCommand_Run(t *testing.T) {
 	t.Parallel()
-	cases := []struct {
-		name     string
-		args     []string
-		expected diagnose.Result
-	}{
-		{
-			"file_not_found",
-			[]string{
-				"-config", "./file/not.found",
-			},
-			diagnose.Result{
-				Name:    "Validate Config",
-				Status:  diagnose.ErrorStatus,
-				Message: "no such file or directory.",
-			},
-		}, {
-			"invalid_syntax",
-			[]string{
-				"-config", "./server/test-fixtures/diagnose_bad_syntax.hcl",
-			},
-			diagnose.Result{
-				Name:    "Validate Config",
-				Status:  diagnose.ErrorStatus,
-				Message: "expected: IDENT | STRING got: LBRACE",
-			},
-		}, {
-			"unknown_property",
-			[]string{
-				"-config", "./server/test-fixtures/diagnose_unknown_property.hcl",
-			},
-			diagnose.Result{
-				Name:     "Validate Config",
-				Status:   diagnose.WarningStatus,
-				Message:  "",
-				Warnings: []string{"Unknown or unsupported field unknown found in configuration"},
-			},
+
+	warn := func(warnings ...string) diagnose.Result {
+		return diagnose.Result{
+			Name:     "Validate Config",
+			Status:   diagnose.WarningStatus,
+			Message:  "",
+			Warnings: warnings,
+		}
+	}
+
+	expectedErrors := map[string]diagnose.Result{
+		"config2.hcl.json":                        warn("Unknown or unsupported field sentinel found in configuration"),
+		"config2.hcl":                             warn("Unknown or unsupported field sentinel found in configuration"),
+		"config3.hcl":                             warn("Unknown or unsupported field sentinel found in configuration"),
+		"config.hcl":                              warn("Unknown or unsupported field sentinel found in configuration"),
+		"config_raft.hcl":                         warn("Unknown or unsupported field sentinel found in configuration"),
+		"tls_config_ok.hcl":                       warn("Unknown or unsupported field sentinel found in configuration"),
+		"diagnose_unknown_property.hcl":           warn("Unknown or unsupported field unknown found in configuration"),
+		"hcp_link_config.hcl":                     warn("Unknown or unsupported field cloud found in configuration"),
+		"config_bad_https_storage.hcl":            warn("Unknown or unsupported field sentinel found in configuration"),
+		"diagnose_bad_https_consul_sr.hcl":        warn("Unknown or unsupported field sentinel found in configuration"),
+		"config5.hcl":                             warn("Unknown or unsupported field sentinel found in configuration"),
+		"config.hcl.json":                         warn("Unknown or unsupported field sentinel found in configuration"),
+		"config_diagnose_hastorage_bad_https.hcl": warn("Unknown or unsupported field sentinel found in configuration"),
+		"diagnose_bad_syntax.hcl": {
+			Name:    "Validate Config",
+			Status:  diagnose.ErrorStatus,
+			Message: "expected: IDENT | STRING got: LBRACE",
 		},
 	}
 
-	for _, tc := range cases {
-		tc := tc
-		t.Run(tc.name, func(t *testing.T) {
+	files, err := os.ReadDir("./server/test-fixtures/")
+	require.NoError(t, err)
+
+	test := func(filename string, expected diagnose.Result) {
+		t.Run(filename, func(t *testing.T) {
 			t.Parallel()
 			cmd := testOperatorValidateConfigCommand(t)
 
-			cmd.Run(tc.args)
+			cmd.Run([]string{"-config", path.Join("./server/test-fixtures/", filename)})
 			result := cmd.diagnose.Finalize(t.Context())
 
-			if err := compareResult(&tc.expected, result); err != nil {
+			if err := compareResult(&expected, result); err != nil {
 				t.Fatalf("Did not find expected test results: %v", err)
 			}
 		})
 	}
+
+	for _, file := range files {
+		expected, ok := expectedErrors[file.Name()]
+		if !ok {
+			expected = diagnose.Result{
+				Name:    "Validate Config",
+				Status:  diagnose.OkStatus,
+				Message: "",
+			}
+		}
+
+		test(file.Name(), expected)
+	}
+
+	test("./file/not.found", diagnose.Result{
+		Name:    "Validate Config",
+		Status:  diagnose.ErrorStatus,
+		Message: "no such file or directory",
+	})
 }

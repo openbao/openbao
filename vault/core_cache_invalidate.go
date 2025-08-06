12f86337743aea25395f9573c11e65df795c8226
Philipp Stehle <philipp.stehle@secretz.io>
2025-08-06 07:08:47 +0200

    rework cache invalidation error handling
    Signed-off-by: Philipp Stehle <philipp.stehle@secretz.io>

--- vault/core_cache_invalidate.go
+++ vault/core_cache_invalidate.go
@@ -12,18 +12,14 @@ import (
 	"github.com/openbao/openbao/vault/quotas"
 )
 
-func (c *Core) processInvalidationFailure(key string, err error) {
-	c.logger.Error("cache invalidation failed", "key", key, "error", err.Error())
-	// TODO(phil9909): It's not save to continue, restart the core
-}
-
 func (c *Core) Invalidate(key string) {
 	ctx, cancel := context.WithTimeout(c.activeContext, 2*time.Second)
 	defer cancel()
 
 	err := c.invalidateInternal(ctx, key)
 	if err != nil {
-		c.processInvalidationFailure(key, err)
+		c.logger.Error("cache invalidation failed, restarting core", "key", key, "error", err.Error())
+		c.restart()
 	}
 }
 
@@ -39,11 +35,14 @@ func (c *Core) invalidateInternal(ctx context.Context, key string) error {
 		var err error
 		ns, err = c.namespaceStore.GetNamespace(ctx, namespaceUUID)
 		if err != nil {
-			c.logger.Debug("error while invalidating cache: could not find namespace", "key", key, "error", err.Error())
+			return err
+		}
+		if ns == nil {
+			c.logger.Debug("error while invalidating cache: could not find namespace", "key", key)
 			// We can't find the namespace, this can happen for two reasons:
 			// 1. The namespace was deleted already
 			// 2. The namespace has just been created (and the core/namespaces/<uuid> key was not yet invalidated)
-			// We will also recive a invalidation request for the core/namespaces/<uuid> key in both cases, so we are fine
+			// We will also receive a invalidation request for the core/namespaces/<uuid> key in both cases, so we are fine
 			return nil
 		}
 	}
@@ -52,22 +51,12 @@ func (c *Core) invalidateInternal(ctx context.Context, key string) error {
 
 	switch {
 	case strings.HasPrefix(namespacedKey, namespaceStoreSubPath):
-		err := c.namespaceStore.invalidate(ctx, "")
-		if err != nil {
-			return err
-		}
-
-		err = c.policyStore.invalidateNamespace(ctx, strings.TrimPrefix(namespacedKey, namespaceStoreSubPath))
-		if err != nil {
-			return err
-		}
+		c.namespaceStore.invalidate(ctx, "")
+		c.policyStore.invalidateNamespace(ctx, strings.TrimPrefix(namespacedKey, namespaceStoreSubPath))
 
 	case strings.HasPrefix(namespacedKey, systemBarrierPrefix+policyACLSubPath):
 		policyType := PolicyTypeACL // for now it is safe to assume type is ACL
-		err := c.policyStore.invalidate(ctx, strings.TrimPrefix(namespacedKey, systemBarrierPrefix+policyACLSubPath), policyType)
-		if err != nil {
-			return err
-		}
+		c.policyStore.invalidate(ctx, strings.TrimPrefix(namespacedKey, systemBarrierPrefix+policyACLSubPath), policyType)
 
 	case strings.HasPrefix(namespacedKey, systemBarrierPrefix+quotas.StoragePrefix):
 		c.quotaManager.Invalidate(strings.TrimPrefix(key, systemBarrierPrefix+quotas.StoragePrefix))
@@ -76,20 +65,11 @@ func (c *Core) invalidateInternal(ctx context.Context, key string) error {
 	// if router.Invalidate returns true, a matching plugin was found and the invalidation is therefore dispatched
 
 	case key == coreAuditConfigPath || key == coreLocalAuditConfigPath:
-		go func() {
-			err := c.reconcileAudits(reconcileAuditsRequests{
-				ctx:       c.activeContext,
-				readonly:  true,
-				isInitial: false,
-			})
-			if err != nil {
-				c.processInvalidationFailure(key, err)
-			}
-		}()
+		c.invalidateAudits()
 
 	default:
 		c.logger.Warn("no idea how to invalidate cache. Maybe it's not cached and this is fine, maybe not", "key", key)
 	}
 
-	return ctx.Err()
+	return nil
 }

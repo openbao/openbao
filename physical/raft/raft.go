5f853bebbae5388975cc1daa8a75b2e068589941
fp17 <bint.dr.hanif@icloud.com>
2025-08-11 09:35:26 +0200

    handle read req locally
    Signed-off-by: fp17 <bint.dr.hanif@icloud.com>

--- physical/raft/raft.go
+++ physical/raft/raft.go
@@ -1628,6 +1628,10 @@ func (b *RaftBackend) Delete(ctx context.Context, path string) error {
 	b.l.RLock()
 	err := b.applyLog(ctx, command)
 	b.l.RUnlock()
+
+	if err == raft.ErrNotLeader && b.raft.State() != raft.Leader {
+		return errors.New(logical.ErrReadOnly.Error())
+	}
 	return err
 }
 
@@ -1690,6 +1694,10 @@ func (b *RaftBackend) Put(ctx context.Context, entry *physical.Entry) error {
 	b.l.RLock()
 	err := b.applyLog(ctx, command)
 	b.l.RUnlock()
+
+	if err == raft.ErrNotLeader && b.raft.State() != raft.Leader {
+		return errors.New(logical.ErrReadOnly.Error())
+	}
 	return err
 }
 

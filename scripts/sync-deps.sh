dd2398d264c5155ddd9b16c3d36a06e95b0e0394
Jonas Köhnen <jonas.koehnen@sap.com>
2025-09-22 08:50:44 +0200

    Cache find command output in sync-deps
    This was taking _forever_ (10 minutes or so), now it's fast again.
    
    Signed-off-by: Jonas Köhnen <jonas.koehnen@sap.com>

--- scripts/sync-deps.sh
+++ scripts/sync-deps.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
+modules=$(find . -name go.mod)
+
 grep '^	' go.mod | while read line; do
 	pkg="$(awk '{print $1}' <<< "$line")"
 	version="$(awk '{print $2}' <<< "$line")"
 
-	find . -name go.mod | while read go_mod; do
+	while read go_mod; do
 		dir="$(dirname "$go_mod")"
 		if grep -q "^	$pkg" "$go_mod" && grep "$pkg" "$go_mod" | grep -qv "$pkg $version"; then
 			( cd "$dir" && go get "$pkg"@"$version" )
 		fi
-	done
+	done <<< "$modules"
 done
 
 make tidy-all

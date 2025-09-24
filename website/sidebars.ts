050072cc0f822bc0743035aa0c7bce851a3848c8
Andrii Fedorchuk <andrii.fedorchuk@secretz.io>
2025-09-24 15:29:48 +0200

    add RFC for enabling Standby Nodes handle READ Requests (#1553)
    * add RFC for enabling Standby Nodes handle READ Requests
    
    Signed-off-by: driif <andrii.fedorchuk@secretz.io>
    
    * add mermaid diagram for stanby / active nodes lifecycle
    
    Signed-off-by: driif <andrii.fedorchuk@secretz.io>
    
    * Update website/content/docs/rfcs/standby-nodes-handle-read-requests.mdx
    
    Co-authored-by: Alexander Scheel <alexander.m.scheel@gmail.com>
    Signed-off-by: Andrii Fedorchuk <andrii.fedorchuk@secretz.io>
    
    * refine text for forwarding logic, delete unnecessary code snipet for request forwarding base on HTTP Method
    
    Signed-off-by: driif <andrii.fedorchuk@secretz.io>
    
    * add PoC and related PRs
    
    Signed-off-by: driif <andrii.fedorchuk@secretz.io>
    
    ---------
    
    Signed-off-by: driif <andrii.fedorchuk@secretz.io>
    Signed-off-by: Andrii Fedorchuk <andrii.fedorchuk@secretz.io>
    Co-authored-by: Alexander Scheel <alexander.m.scheel@gmail.com>
    Signed-off-by: Geoff Wilson <geoff@gr-oss.io>

--- website/sidebars.ts
+++ website/sidebars.ts
@@ -538,6 +538,11 @@ const sidebars: SidebarsConfig = {
                 {
                   "UI/UX": ["rfcs/web-ui-modernization"],
                 },
+                {
+                    "Horizontal Scalability": [
+                        "rfcs/standby-nodes-handle-read-requests",
+                    ],
+                },
             ],
             FAQ: ["faq/index", "deprecation/faq", "auth/login-mfa/faq"],
         },

8d21a4e3c9ffe631d7e20b2e9d199b12a2a66dd0
Alexander Scheel <ascheel@gitlab.com>
2025-10-22 11:56:13 -0500

    Refactor audit log formatting due to copy (#2002) (#2004)
    Refactor audit log formatting due to copy
    
    When cloning the to be logged structure, the audit subsystem attempted
    to walk the original copy, modifying values in the cloned. This resulted
    in a few cases where the subsystem would reject response auditing due to
    an inability to correctly walk both in parallel.
    
    Additionally, the HMAC'd HTTPRawBody would not be correctly HMAC'd as
    the original copy is usually of type []byte, a non-HMAC'd type.
    
    
    
    Add changelog entry
    
    
    
    test: added few additional test cases
    
    Signed-off-by: Wojciech Slabosz <wojciech.slabosz@sap.com>

--- audit/format.go
+++ audit/format.go
@@ -572,24 +572,25 @@ func NewTemporaryFormatter(format, prefix string) *AuditFormatter {
 	return ret
 }
 
-// doElideListResponseData performs the actual elision of list operation response data, once surrounding code has
-// determined it should apply to a particular request. The data map that is passed in must be a copy that is safe to
-// modify in place, but need not be a full recursive deep copy, as only top-level keys are changed.
+// doElideListResponseData performs the actual elision of list operation
+// response data, once surrounding code has determined it should apply to
+// a particular request. The data map that is passed in must be a copy that
+// is safe to modify in place, but need not be a full recursive deep copy,
+// as only top-level keys are changed.
 //
 // See the documentation of the controlling option in FormatterConfig for more information on the purpose.
 func doElideListResponseData(data map[string]interface{}) {
-	doElideListResponseDataWithCopy(data, data)
-}
-
-func doElideListResponseDataWithCopy(inputData map[string]interface{}, outputData map[string]interface{}) {
-	for k, v := range inputData {
-		if k == "keys" {
-			if vSlice, ok := v.([]string); ok {
-				outputData[k] = len(vSlice)
+	for k, v := range data {
+		switch k {
+		case "keys":
+			if vSlice, ok := v.([]interface{}); ok {
+				data[k] = len(vSlice)
+			} else if vSlice, ok := v.([]string); ok {
+				data[k] = len(vSlice)
 			}
-		} else if k == "key_info" {
+		case "key_info":
 			if vMap, ok := v.(map[string]interface{}); ok {
-				outputData[k] = len(vMap)
+				data[k] = len(vMap)
 			}
 		}
 	}

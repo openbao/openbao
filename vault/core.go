a86eefd3c38fe840d440a909c6a2e6a8be70c378
Philipp Stehle <philipp.stehle@secretz.io>
2025-10-23 09:56:50 +0200

    grab state lock before closing standbyStop channel
    in `sealInternalWithOptions` this was removed here:
    https://github.com/openbao/openbao/pull/1847/commits/cef50884967e184e2e9a6ca6322dbba29c79f8db#diff-a0a40b411f03b3c927d6a2eafaf8a17693cc05a44e81bfe1362045a9372b9cfeL2217-L2220
    by @cipherboy
    
    Was this an oversight? I think it would probably fix the same issue as adding
    the lock grab timeout in runStandby (but causes a race in
    `TestSysMonitorStreamingLogs`). So probably we don't need both changes.
    
    Signed-off-by: Philipp Stehle <philipp.stehle@secretz.io>

--- vault/core.go
+++ vault/core.go
@@ -2228,6 +2228,10 @@ func (c *Core) sealInternalWithOptions(grabStateLock, keepHALock, performCleanup
 		if keepHALock {
 			atomic.StoreUint32(c.keepHALockOnStepDown, 1)
 		}
+		if grabStateLock {
+			cancelCtxAndLock()
+			defer c.stateLock.Unlock()
+		}
 
 		// If we are trying to acquire the lock, force it to return with nil so
 		// runStandby will exit

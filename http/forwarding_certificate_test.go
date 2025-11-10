e0b4dca96d11805a008614f479ccbfd2da5e78af
Chloe Surett <chloe@surett.me>
2025-11-10 15:32:29 -0500

    Add tests for `WrapClientCertificateHandler`
    Signed-off-by: Chloe Surett <chloe@surett.me>

---
+++ http/forwarding_certificate_test.go
@@ -0,0 +1,177 @@
+// Copyright (c) 2025 OpenBao a Series of LF Projects, LLC
+// SPDX-License-Identifier: MPL-2.0
+
+package http
+
+import (
+	"bytes"
+	"encoding/base64"
+	"encoding/pem"
+	"net/http"
+	"net/url"
+	"os"
+	"strings"
+	"testing"
+
+	"github.com/openbao/openbao/internalshared/configutil"
+	"github.com/openbao/openbao/vault"
+)
+
+// Reuse the test fixtures from the cert credential handler
+const testCertPath1 = "../builtin/credential/cert/test-fixtures/testissuedcert4.pem"
+
+func getTestHandler(decoders []string) func(props *vault.HandlerProperties) http.Handler {
+	return func(props *vault.HandlerProperties) http.Handler {
+		origHandler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			w.WriteHeader(http.StatusOK)
+		})
+		listenerConfig := getListenerConfigForClientCerts(decoders)
+		return WrapClientCertificateHandler(origHandler, listenerConfig)
+	}
+}
+
+func getListenerConfigForClientCerts(decoders []string) *configutil.Listener {
+	return &configutil.Listener{
+		XForwardedForClientCertHeader:   "X-Forwarded-For-Client-Cert",
+		XForwardedForClientCertDecoders: decoders,
+	}
+}
+
+func TestHandler_XForwardedForClientCert(t *testing.T) {
+	// Custom error name to prevent shadowing.
+	clientCertFile, certErr := os.ReadFile(testCertPath1)
+	if certErr != nil {
+		t.Fatal(certErr)
+	}
+	clientCertBlock, _ := pem.Decode(clientCertFile)
+	if clientCertBlock == nil || clientCertBlock.Type != "CERTIFICATE" {
+		t.Fatalf("failed to decode test client certificate")
+	}
+	clientCertPemText := string(clientCertFile)
+
+	t.Run("invalid_base64", func(t *testing.T) {
+		t.Parallel()
+		testHandler := getTestHandler([]string{})
+		cluster := vault.NewTestCluster(t, nil, &vault.TestClusterOptions{
+			HandlerFunc: HandlerFunc(testHandler),
+		})
+		cluster.Start()
+		defer cluster.Cleanup()
+
+		client := cluster.Cores[0].Client
+		req := client.NewRequest("GET", "/")
+		req.Headers = make(http.Header)
+		req.Headers.Add("X-Forwarded-For-Client-Cert", "invalid")
+		resp, err := client.RawRequest(req)
+		if err == nil {
+			t.Fatal("expected error")
+		}
+		defer resp.Body.Close()
+		buf := bytes.NewBuffer(nil)
+		_, err = buf.ReadFrom(resp.Body)
+		if err != nil {
+			t.Fatal("failed to read response body")
+		}
+		if !strings.Contains(buf.String(), "error decoding client certificate header as base64") {
+			t.Fatalf("bad body: %s", buf.String())
+		}
+	})
+	t.Run("valid_base64", func(t *testing.T) {
+		t.Parallel()
+		testHandler := getTestHandler([]string{})
+		cluster := vault.NewTestCluster(t, nil, &vault.TestClusterOptions{
+			HandlerFunc: HandlerFunc(testHandler),
+		})
+		cluster.Start()
+		defer cluster.Cleanup()
+		client := cluster.Cores[0].Client
+		req := client.NewRequest("GET", "/")
+		req.Headers = make(http.Header)
+		req.Headers.Add("X-Forwarded-For-Client-Cert", base64.StdEncoding.EncodeToString(clientCertBlock.Bytes))
+		resp, err := client.RawRequest(req)
+		if err != nil {
+			t.Fatal(err)
+		}
+		if resp.StatusCode != http.StatusOK {
+			t.Fatalf("bad status: %d", resp.StatusCode)
+		}
+	})
+	// This will not work for some reason. Go refuses the header even when encoded with /r/n and with leading tabs.
+	t.Run("nginx_ssl_client_cert_fails", func(t *testing.T) {
+		t.Parallel()
+		testHandler := getTestHandler([]string{"PEM"})
+		cluster := vault.NewTestCluster(t, nil, &vault.TestClusterOptions{
+			HandlerFunc: HandlerFunc(testHandler),
+		})
+		cluster.Start()
+		defer cluster.Cleanup()
+		client := cluster.Cores[0].Client
+		req := client.NewRequest("GET", "/")
+		req.Headers = make(http.Header)
+		// NGINX Prepends a tab before every line except the first
+		// https://nginx.org/en/docs/http/ngx_http_ssl_module.html#var_ssl_client_cert
+		clientCertPemTextHeader := strings.ReplaceAll(clientCertPemText, "\n", "\r\n\t")
+		req.Headers.Add("X-Forwarded-For-Client-Cert", clientCertPemTextHeader)
+		_, err := client.RawRequest(req)
+		if err == nil {
+			t.Fatal("expected error")
+		}
+		if !strings.Contains(err.Error(), "net/http: invalid header field value for \"X-Forwarded-For-Client-Cert\"") {
+			t.Fatalf("bad error: %s", err)
+		}
+	})
+
+	// https://nginx.org/en/docs/http/ngx_http_ssl_module.html#var_ssl_client_escaped_cert
+	t.Run("nginx_ssl_client_escaped_cert", func(t *testing.T) {
+		t.Parallel()
+		testHandler := getTestHandler([]string{"URL", "PEM"})
+		cluster := vault.NewTestCluster(t, nil, &vault.TestClusterOptions{
+			HandlerFunc: HandlerFunc(testHandler),
+		})
+		cluster.Start()
+		defer cluster.Cleanup()
+		client := cluster.Cores[0].Client
+		req := client.NewRequest("GET", "/")
+		req.Headers = make(http.Header)
+
+		// NGINX urlencodes the certificate
+		req.Headers.Add("X-Forwarded-For-Client-Cert", url.QueryEscape(clientCertPemText))
+		resp, err := client.RawRequest(req)
+		if err != nil {
+			t.Fatal(err)
+		}
+		if resp.StatusCode != http.StatusOK {
+			t.Fatalf("bad status: %d", resp.StatusCode)
+		}
+	})
+
+	t.Run("invalid_escaped_cert", func(t *testing.T) {
+		t.Parallel()
+		testHandler := getTestHandler([]string{"URL", "PEM"})
+		cluster := vault.NewTestCluster(t, nil, &vault.TestClusterOptions{
+			HandlerFunc: HandlerFunc(testHandler),
+		})
+		cluster.Start()
+		defer cluster.Cleanup()
+		client := cluster.Cores[0].Client
+		req := client.NewRequest("GET", "/")
+		req.Headers = make(http.Header)
+		urlencodedCert := url.QueryEscape(clientCertPemText)
+		// Encode with invalid hex
+		urlencodedCert += "%GG"
+		req.Headers.Add("X-Forwarded-For-Client-Cert", urlencodedCert)
+		resp, err := client.RawRequest(req)
+		if err == nil {
+			t.Fatal("expected error")
+		}
+		defer resp.Body.Close()
+		buf := bytes.NewBuffer(nil)
+		_, err = buf.ReadFrom(resp.Body)
+		if err != nil {
+			t.Fatal("failed to read response body")
+		}
+		if !strings.Contains(buf.String(), "error decoding client certificate header") {
+			t.Fatalf("bad body: %s", buf.String())
+		}
+	})
+}

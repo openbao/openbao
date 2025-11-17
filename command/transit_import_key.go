b451fda3c68b09cc4d87d316c28f10bae1cd25e0
Quoc Huy Dinh <quoc.huy.dinh@sap.com>
2025-11-17 10:44:33 +0100

    add nolint explanation
    Signed-off-by: Quoc Huy Dinh <quoc.huy.dinh@sap.com>

--- command/transit_import_key.go
+++ command/transit_import_key.go
@@ -77,7 +77,8 @@ func (c *TransitImportCommand) Run(args []string) int {
 func transitImportKeyPath(s string, operation string) (path string, apiPath string, err error) {
 	parts := keyPath.FindStringSubmatch(s)
 	if len(parts) != 3 {
-		return "", "", errors.New("expected transit path and key name in the form :path:/keys/:name:") //nolint:staticcheck
+		//nolint:staticcheck // colon is used for path placeholders
+		return "", "", errors.New("expected transit path and key name in the form :path:/keys/:name:")
 	}
 	path = parts[1]
 	keyName := parts[2]

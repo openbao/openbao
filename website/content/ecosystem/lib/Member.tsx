aa6b3b4c10d01f8e251541d41cac59eb592f87c8
José Gonçalves <5504279+Tosta-Mista@users.noreply.github.com>
2026-01-09 10:53:47 +0100

    feat(ecosystem): Add helvethink to vendors (#2279)
    * improve the way that the svg is loaded by docusaurus (webpack)
    
    Signed-off-by: José Gonçalves <jose.goncalves@helvethink.ch>
    
    * Add helvethink as vendor
    
    Signed-off-by: José Gonçalves <jose.goncalves@helvethink.ch>
    
    * Add Randomize to vendors block
    
    Signed-off-by: José Gonçalves <jose.goncalves@helvethink.ch>
    
    ---------
    
    Signed-off-by: José Gonçalves <jose.goncalves@helvethink.ch>

--- website/content/ecosystem/lib/Member.tsx
+++ website/content/ecosystem/lib/Member.tsx
@@ -1,19 +1,22 @@
 import clsx from "clsx";
 import styles from "./styles.module.css";
 
+const logos = require.context(
+    "openbao-ecosystem-logos", false, /\.svg$/
+);
+
 export default function Member({ title, children }) {
-  let logo = require(
-    `../../../node_modules/openbao-ecosystem-logos/${title.toLowerCase()}.svg`,
-  ).default;
-  return (
-    <div className="col col--6 padding-bottom--lg">
-      <div className="card card--full-height">
-        <div className={clsx("card__header", styles.cardHeader)}>
-          <h2>{title}</h2>
-          <img className={styles.logo} src={logo} />
+    const key = title.trim().toLowerCase();
+    const logo = logos(`./${key}.svg`).default ?? logos(`./${key}.svg`);
+    return (
+        <div className="col col--6 padding-bottom--lg">
+            <div className="card card--full-height">
+                <div className={clsx("card__header", styles.cardHeader)}>
+                    <h2>{title}</h2>
+                    <img className={styles.logo} src={logo} alt={`${title} logo`} />
+                </div>
+                <div className="card__body">{children}</div>
+            </div>
         </div>
-        <div className="card__body">{children}</div>
-      </div>
-    </div>
-  );
+    );
 }

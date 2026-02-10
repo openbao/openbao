3c6cd7920e09060670b5380be054dc0aa35284f1
Jonas Köhnen <jonas.koehnen@sap.com>
2026-02-10 12:06:49 +0100

    Remove unused top-level files (#2448)
    Signed-off-by: Jonas Köhnen <jonas.koehnen@sap.com>

--- main_test.go
+++
@@ -1,7 +0,0 @@
-// Copyright (c) HashiCorp, Inc.
-// SPDX-License-Identifier: MPL-2.0
-
-package main // import "github.com/hashicorp/vault"
-
-// This file is intentionally empty to force early versions of Go
-// to test compilation for tests.

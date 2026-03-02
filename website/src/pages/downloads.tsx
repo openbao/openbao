b22a0cb021477d76e3a654464e57df32d7f764fb
Gian Klug <gian.klug@secretz.io>
2026-03-02 15:42:02 +0100

    fix: include download icon svg directly
    Signed-off-by: Gian Klug <gian.klug@secretz.io>

--- website/src/pages/downloads.tsx
+++ website/src/pages/downloads.tsx
@@ -12,7 +12,6 @@ import {
   ArchPackageMapApply
 } from "@site/src/components/Releases";
 import CodeBlockWrap from "@site/src/components/CodeBlockWrap";
-import { Download as DownloadIcon } from "lucide-react";
 
 // Create a context
 const OptionsContext = createContext(null);
@@ -207,7 +206,7 @@ const Asset = ({ urls }) => {
           <div className="download-card__button-wrapper">
             <a className="button button--primary" href={asset}>
               <span className="download-card__button-icon">
-                <DownloadIcon size={18} />
+                <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M12 15V3"></path><path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"></path><path d="m7 10 5 5 5-5"></path></svg>
                 {buttonText}
               </span>
             </a>

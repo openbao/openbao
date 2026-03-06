e3f6c3ce110e57eef4bbca88cecc9f001b014a67
Andrew Hay <andrew.hay@benchmarkanalytics.com>
2026-03-05 20:46:55 -0500

    test: add BenchmarkTokenStore_HandleCreateCommon
    Adds a benchmark for end-to-end token creation to track the
    performance of handleCreateCommon and ensure future changes
    do not introduce regressions.
    
    Signed-off-by: Andrew Hay <andrew.hay@benchmarkanalytics.com>

---
+++ vault/token_store_create_bench_test.go
@@ -0,0 +1,36 @@
+package vault
+
+import (
+	"testing"
+
+	"github.com/openbao/openbao/helper/benchhelpers"
+	"github.com/openbao/openbao/helper/namespace"
+	"github.com/openbao/openbao/sdk/v2/logical"
+)
+
+func BenchmarkTokenStore_HandleCreateCommon(b *testing.B) {
+	c, _, root := TestCoreUnsealed(benchhelpers.TBtoT(b))
+	ts := c.tokenStore
+
+	ctx := namespace.RootContext(nil)
+
+	b.ResetTimer()
+	b.ReportAllocs()
+
+	for i := 0; i < b.N; i++ {
+		req := logical.TestRequest(benchhelpers.TBtoT(b), logical.UpdateOperation, "create")
+		req.ClientToken = root
+		req.Data = map[string]interface{}{
+			"policies": []string{"default"},
+			"ttl":      "1h",
+		}
+
+		resp, err := ts.HandleRequest(ctx, req)
+		if err != nil {
+			b.Fatalf("err: %v", err)
+		}
+		if resp != nil && resp.IsError() {
+			b.Fatalf("resp err: %v", resp.Error())
+		}
+	}
+}

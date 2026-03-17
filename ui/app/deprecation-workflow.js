c8395fc19b7ae52fde474a2c69f68e4267f4e463
Jan Martens <jan@martens.eu.org>
2026-03-17 22:57:07 +0100

    fix ember-deprecration-workflow
    it broke because of the recent emberJS upgrade
    
    Signed-off-by: Jan Martens <jan@martens.eu.org>

--- ui/app/deprecation-workflow.js
+++ ui/app/deprecation-workflow.js
@@ -3,12 +3,10 @@
  * SPDX-License-Identifier: MPL-2.0
  */
 
-self.deprecationWorkflow = self.deprecationWorkflow || {};
-//self.deprecationWorkflow.config = {
-//throwOnUnhandled: true
-//}
-self.deprecationWorkflow.config = {
+import setupDeprecationWorkflow from 'ember-cli-deprecation-workflow';
+
+setupDeprecationWorkflow({
   // current output from deprecationWorkflow.flushDeprecations();
   // deprecations that will not be removed until 5.0.0 are filtered by deprecation-filter initializer rather than silencing below
   workflow: [],
-};
+});

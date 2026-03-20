openbao/openbao

commit 7ded341e7d50de563a99e45999bf4f2694a7790f

Add Auto Unseal plugins (#2586)
Signed-off-by: Jonas Köhnen <jonas.koehnen@sap.com>

vault/core.go
@@ -1076,12 +1076,7 @@ func NewCore(conf *CoreConfig) (*Core, error) {
 		return c, nil
 	}
 
-	if conf.PluginDirectory != "" {
-		c.pluginDirectory, err = filepath.Abs(conf.PluginDirectory)
-		if err != nil {
-			return nil, fmt.Errorf("core setup failed, could not verify plugin directory: %w", err)
-		}
-	}
+	c.pluginDirectory = conf.PluginDirectory
 
 	if conf.PluginFileUid != 0 {
 		c.pluginFileUid = conf.PluginFileUid
@@ -2330,7 +2325,7 @@ func (readonlyUnsealStrategy) unsealShared(ctx context.Context, c *Core, standby
 	if err := c.setupPluginCatalog(ctx); err != nil {
 		return err
 	}
-	if err := c.reconcileOCIPlugins(ctx, standby); err != nil {
+	if err := c.registerDeclarativePlugins(ctx, standby); err != nil {
 		return err
 	}
 	if err := c.setupNamespaceStore(ctx); err != nil {
@@ -3671,7 +3666,7 @@ func (c *Core) CheckPluginPerms(pluginName string) (err error) {
 		var err error
 		enableFilePermissionsCheck, err = strconv.ParseBool(enableFilePermissionsCheckEnv)
 		if err != nil {
-			return errors.New("Error parsing the environment variable VAULT_ENABLE_FILE_PERMISSIONS_CHECK")
+			return fmt.Errorf("failed to parse environment variable %s", consts.VaultEnableFilePermissionsCheckEnv)
 		}
 	}
 

vault/logical_system.go
@@ -349,7 +349,7 @@ func (b *SystemBackend) handlePluginCatalogTypedList(ctx context.Context, req *l
 func (b *SystemBackend) handlePluginCatalogUntypedList(ctx context.Context, _ *logical.Request, _ *framework.FieldData) (*logical.Response, error) {
 	data := make(map[string]interface{})
 	var versionedPlugins []pluginutil.VersionedPlugin
-	for _, pluginType := range consts.PluginTypes {
+	for _, pluginType := range pluginTypes {
 		plugins, err := b.Core.pluginCatalog.List(ctx, pluginType)
 		if err != nil {
 			return nil, err

vault/plugin_catalog.go
@@ -37,12 +37,22 @@ import (
 	"google.golang.org/grpc/metadata"
 )
 
+const pluginCatalogPath = "core/plugin-catalog/"
+
 var (
-	pluginCatalogPath           = "core/plugin-catalog/"
 	ErrDirectoryNotConfigured   = errors.New("could not set plugin, plugin directory is not configured")
 	ErrPluginNotFound           = errors.New("plugin not found in the catalog")
 	ErrPluginConnectionNotFound = errors.New("plugin connection not found for client")
 	ErrPluginBadType            = errors.New("unable to determine plugin type")
+
+	// pluginTypes is the subset of consts.PluginTypes that is handled by the
+	// plugin catalog.
+	pluginTypes = []consts.PluginType{
+		consts.PluginTypeUnknown,
+		consts.PluginTypeCredential,
+		consts.PluginTypeDatabase,
+		consts.PluginTypeSecrets,
+	}
 )
 
 // PluginCatalog keeps a record of plugins known to vault. External plugins need
@@ -205,65 +215,34 @@ func (c *Core) ReloadPlugins() {
 		return
 	}
 
-	c.pluginCatalog.lock.Lock()
-	defer c.pluginCatalog.lock.Unlock()
-
-	if err := c.reconcileOCIPlugins(c.activeContext, c.Standby()); err != nil {
+	if err := c.registerDeclarativePlugins(c.activeContext, c.Standby()); err != nil {
 		c.logger.Error("failed to set up plugins on reload", "error", err)
 	}
 }
 
-// reconcileOCIPlugins handles downloading and validating OCI-based plugins configured in the server
-func (c *Core) reconcileOCIPlugins(ctx context.Context, standby bool) error {
-	logger := c.logger.Named("oci-plugins")
-
-	// Load the current configuration
-	conf := c.rawConfig.Load()
-	if conf == nil {
-		logger.Error("nil config")
+func (c *Core) registerDeclarativePlugins(ctx context.Context, standby bool) error {
+	config := c.rawConfig.Load()
+	if config == nil {
 		return nil
 	}
 
-	if err := c.downloadOCIPlugins(ctx, logger, conf); err != nil {
-		return fmt.Errorf("failed to download OCI plugins: %w", err)
-	}
-
-	if err := c.registerOCIPlugins(ctx, logger, conf, standby); err != nil {
-		return fmt.Errorf("failed to register OCI plugins: %w", err)
-	}
-
-	return nil
-}
-
-func (c *Core) downloadOCIPlugins(ctx context.Context, logger log.Logger, config *server.Config) error {
-	if !config.PluginAutoDownload {
-		logger.Info("skipping OCI plugin downloading")
-		return nil
-	}
-
-	// Create OCI plugin downloader
-	logger.Info("starting OCI plugin downloading")
-	downloader := oci.NewPluginDownloader(c.pluginDirectory, config, logger)
-	err := downloader.ReconcilePlugins(ctx)
-	if err != nil {
-		return err
-	}
-
-	logger.Info("OCI plugin downloading completed")
-	return nil
-}
-
-func (c *Core) registerOCIPlugins(ctx context.Context, logger log.Logger, config *server.Config, standby bool) error {
 	if !config.PluginAutoRegister {
-		logger.Info("skipping OCI plugin registration")
 		return nil
 	}
 
-	// Register declarative plugins
-	logger.Info("starting OCI plugin registration")
-	defer logger.Info("OCI plugin registration completed")
+	logger := c.logger.Named("plugins")
 
-	return c.pluginCatalog.registerOCIPlugins(ctx, config.Plugins, standby)
+	logger.Info("starting declarative plugin registration")
+	defer logger.Info("declarative plugin registration completed")
+
+	c.pluginCatalog.lock.Lock()
+	defer c.pluginCatalog.lock.Unlock()
+
+	if err := c.pluginCatalog.registerDeclarativePlugins(ctx, config.Plugins, standby); err != nil {
+		return fmt.Errorf("failed to set up declarative plugins: %w", err)
+	}
+
+	return nil
 }
 
 type pluginClientConn struct {
@@ -325,13 +304,19 @@ func (c *PluginCatalog) reloadExternalPlugin(key externalPluginsKey, id, path st
 	return nil
 }
 
-func (c *PluginCatalog) registerOCIPlugins(ctx context.Context, plugins []*server.PluginConfig, standby bool) error {
+func (c *PluginCatalog) registerDeclarativePlugins(ctx context.Context, plugins []*server.PluginConfig, standby bool) error {
 	// Register any missing plugins and update existing ones.
 	for index, plugin := range plugins {
 		if err := func() error {
 			pluginType, err := consts.ParsePluginType(plugin.Type)
 			if err != nil {
 				return err
+			}
+
+			if !slices.Contains(pluginTypes, pluginType) {
+				// Skip registering plugins that are not handled by the plugin
+				// catalog, such as KMS plugins.
+				return nil
 			}
 
 			sha256, err := hex.DecodeString(plugin.SHA256Sum)
@@ -384,7 +369,7 @@ func (c *PluginCatalog) registerOCIPlugins(ctx context.Context, plugins []*serve
 	}
 
 	// Check if we need to remove any plugins.
-	for _, pluginType := range consts.PluginTypes {
+	for _, pluginType := range pluginTypes {
 		if err := func() error {
 			storedPlugins, err := c.listInternal(ctx, pluginType, true /* versioned */)
 			if err != nil {
@@ -1135,6 +1120,8 @@ func (c *PluginCatalog) setInternal(ctx context.Context, name string, pluginType
 		runningVersion, versionErr = c.getBackendRunningVersion(ctx, entryTmp)
 	case consts.PluginTypeDatabase:
 		runningVersion, versionErr = c.getDatabaseRunningVersion(ctx, entryTmp)
+	case consts.PluginTypeKMS:
+		return nil, fmt.Errorf("KMS plugins cannot be registered via the plugin catalog")
 	default:
 		return nil, fmt.Errorf("unknown plugin type: %v", pluginType)
 	}

api/plugin_types.go
@@ -14,6 +14,7 @@ var PluginTypes = []PluginType{
 	PluginTypeCredential,
 	PluginTypeDatabase,
 	PluginTypeSecrets,
+	PluginTypeKMS,
 }
 
 type PluginType uint32
@@ -33,6 +34,7 @@ const (
 	PluginTypeCredential
 	PluginTypeDatabase
 	PluginTypeSecrets
+	PluginTypeKMS
 )
 
 func (p PluginType) String() string {
@@ -45,6 +47,8 @@ func (p PluginType) String() string {
 		return "database"
 	case PluginTypeSecrets:
 		return "secret"
+	case PluginTypeKMS:
+		return "kms"
 	default:
 		return "unsupported"
 	}
@@ -60,6 +64,8 @@ func ParsePluginType(pluginType string) (PluginType, error) {
 		return PluginTypeDatabase, nil
 	case "secret":
 		return PluginTypeSecrets, nil
+	case "kms":
+		return PluginTypeKMS, nil
 	default:
 		return PluginTypeUnknown, fmt.Errorf("%q is not a supported plugin type", pluginType)
 	}

command/operator_diagnose.go
@@ -5,6 +5,7 @@ package command
 
 import (
 	"context"
+	"crypto/rand"
 	"encoding/json"
 	"errors"
 	"fmt"
@@ -14,16 +15,15 @@ import (
 	"sync"
 	"time"
 
-	wrapping "github.com/openbao/go-kms-wrapping/v2"
-
 	"github.com/hashicorp/cli"
 	log "github.com/hashicorp/go-hclog"
 	"github.com/hashicorp/go-secure-stdlib/reloadutil"
 	uuid "github.com/hashicorp/go-uuid"
-
+	wrapping "github.com/openbao/go-kms-wrapping/v2"
 	bApi "github.com/openbao/openbao/api/v2"
 	cserver "github.com/openbao/openbao/command/server"
 	"github.com/openbao/openbao/helper/configutil"
+	"github.com/openbao/openbao/helper/kmsplugin"
 	"github.com/openbao/openbao/helper/listenerutil"
 	"github.com/openbao/openbao/helper/metricsutil"
 	"github.com/openbao/openbao/physical/raft"
@@ -276,6 +276,12 @@ func (c *OperatorDiagnoseCommand) offlineDiagnostics(ctx context.Context) error 
 		return nil
 	})
 
+	var kms *kmsplugin.Catalog
+	_ = diagnose.Test(ctx, "Check KMS Plugin Catalog", func(context.Context) (err error) {
+		kms, err = kmsplugin.NewCatalog(server.logger, config)
+		return err
+	})
+
 	var metricSink *metricsutil.ClusterMetricSink
 	var metricsHelper *metricsutil.MetricsHelper
 
@@ -381,7 +387,7 @@ func (c *OperatorDiagnoseCommand) offlineDiagnostics(ctx context.Context) error 
 	var sealConfigError error
 
 	infoKeys := make([]string, 0)
-	barrierSeal, barrierWrapper, unwrapSeal, seals, sealConfigError, err := setSeal(server, config, &infoKeys, make(map[string]string))
+	barrierSeal, barrierWrapper, unwrapSeal, seals, sealConfigError, err := setSeal(server, config, kms, &infoKeys, make(map[string]string))
 	// Check error here
 	if err != nil {
 		diagnose.Advise(ctx, "For assistance with the seal stanza, see the Vault configuration documentation.")
@@ -474,20 +480,7 @@ SEALFAIL:
 		return nil
 	})
 
-	var coreConfig vault.CoreConfig
-	_ = diagnose.Test(ctx, "Create Core Configuration", func(ctx context.Context) error {
-		var secureRandomReader io.Reader
-		// prepare a secure random reader for core
-		randReaderTestName := "Initialize Randomness for Core"
-		secureRandomReader, err = configutil.CreateSecureRandomReaderFunc(config.SharedConfig, barrierWrapper)
-		if err != nil {
-			//nolint:staticcheck // user-facing error
-			return diagnose.SpotError(ctx, randReaderTestName, fmt.Errorf("Could not initialize randomness for core: %w.", err))
-		}
-		diagnose.SpotOk(ctx, randReaderTestName, "")
-		coreConfig = createCoreConfig(server, config, *backend, configSR, barrierSeal, unwrapSeal, metricsHelper, metricSink, secureRandomReader)
-		return nil
-	})
+	coreConfig := createCoreConfig(server, config, *backend, configSR, barrierSeal, unwrapSeal, metricsHelper, metricSink, rand.Reader)
 
 	var disableClustering bool
 	_ = diagnose.Test(ctx, "HA Storage", func(ctx context.Context) error {

commit 47888d5e5b9f4b277c06547d0ad6aadfaf4934c2

Return error if no matching plugin is found when reloading plugin (#2398)
* return error when no matching plugin is found

Signed-off-by: Quoc Huy Dinh <quoc.huy.dinh@sap.com>

* handle singleton plugins and non-reloadable types

Signed-off-by: Quoc Huy Dinh <quoc.huy.dinh@sap.com>

* skip database plugin reload test

Signed-off-by: Quoc Huy Dinh <quoc.huy.dinh@sap.com>

* err inside reloadBackendCommon for singletonmounts

Signed-off-by: Quoc Huy Dinh <quoc.huy.dinh@sap.com>

* Fixup plugin directory

This is a regression from #2586 that had stale code removals I should've
added back; I'm hijacking this PR to do so.

Signed-off-by: Jonas Köhnen <jonas.koehnen@sap.com>

* Wrap plugin reload in handleError to avoid 500s

Signed-off-by: Jonas Köhnen <jonas.koehnen@sap.com>

---------

Signed-off-by: Quoc Huy Dinh <quoc.huy.dinh@sap.com>
Signed-off-by: Jonas Köhnen <jonas.koehnen@sap.com>
Co-authored-by: Jonas Köhnen <jonas.koehnen@sap.com>

vault/core.go
@@ -1076,7 +1076,12 @@ func NewCore(conf *CoreConfig) (*Core, error) {
 		return c, nil
 	}
 
-	c.pluginDirectory = conf.PluginDirectory
+	if conf.PluginDirectory != "" {
+		c.pluginDirectory, err = filepath.Abs(conf.PluginDirectory)
+		if err != nil {
+			return nil, fmt.Errorf("core setup failed, could not verify plugin directory: %w", err)
+		}
+	}
 
 	if conf.PluginFileUid != 0 {
 		c.pluginFileUid = conf.PluginFileUid

vault/logical_system.go
@@ -621,12 +621,12 @@ func (b *SystemBackend) handlePluginReloadUpdate(ctx context.Context, req *logic
 	if pluginName != "" {
 		err := b.Core.reloadMatchingPlugin(ctx, pluginName)
 		if err != nil {
-			return nil, err
+			return handleError(err)
 		}
 	} else if len(pluginMounts) > 0 {
 		err := b.Core.reloadMatchingPluginMounts(ctx, pluginMounts)
 		if err != nil {
-			return nil, err
+			return handleError(err)
 		}
 	}
 

vault/plugin_catalog.go
@@ -40,10 +40,11 @@ import (
 const pluginCatalogPath = "core/plugin-catalog/"
 
 var (
-	ErrDirectoryNotConfigured   = errors.New("could not set plugin, plugin directory is not configured")
-	ErrPluginNotFound           = errors.New("plugin not found in the catalog")
-	ErrPluginConnectionNotFound = errors.New("plugin connection not found for client")
-	ErrPluginBadType            = errors.New("unable to determine plugin type")
+	ErrDirectoryNotConfigured       = errors.New("could not set plugin, plugin directory is not configured")
+	ErrPluginNotFound               = errors.New("plugin not found in the catalog")
+	ErrPluginConnectionNotFound     = errors.New("plugin connection not found for client")
+	ErrPluginBadType                = errors.New("unable to determine plugin type")
+	ErrSingletonPluginNotReloadable = errors.New("singleton plugin cannot be reloaded")
 
 	// pluginTypes is the subset of consts.PluginTypes that is handled by the
 	// plugin catalog.
@@ -1035,6 +1036,94 @@ func (c *PluginCatalog) get(ctx context.Context, name string, pluginType consts.
 	}
 
 	return nil, nil
+}
+
+// Get the plugin types associated with a plugin name
+func (c *PluginCatalog) TypesFromName(ctx context.Context, name string) ([]consts.PluginType, error) {
+	c.lock.RLock()
+	defer c.lock.RUnlock()
+	return c.typesFromNameLocked(ctx, name)
+}
+
+// Get the plugin types associated with a plugin name
+// Caller must have a read lock on c.lock
+func (c *PluginCatalog) typesFromNameLocked(ctx context.Context, name string) ([]consts.PluginType, error) {
+	typeSet := make(map[consts.PluginType]struct{})
+
+	if c.directory != "" {
+		// Check for matching external untyped unversioned plugin (registered before plugin types existed)
+		// we check this first because these storage entries are not under a plugin type directory
+		out, err := c.catalogView.Get(ctx, name)
+		if err != nil {
+			return nil, err
+		}
+		if out != nil {
+			// Parse the entry to figure out the type
+			entry := new(pluginutil.PluginRunner)
+			if err := jsonutil.DecodeJSON(out.Value, entry); err != nil {
+				return nil, fmt.Errorf("failed to decode plugin entry: %w", err)
+			}
+			if entry.Type != consts.PluginTypeUnknown {
+				typeSet[entry.Type] = struct{}{}
+			}
+		}
+	}
+
+	for _, pluginType := range consts.PluginTypes {
+		// Skip type unknown
+		if pluginType == consts.PluginTypeUnknown {
+			continue
+		}
+
+		// Check for plugin existence across plugin types.
+		hasPlugin, err := c.hasPluginWithTypeLocked(ctx, name, pluginType)
+		if err != nil {
+			return nil, err
+		}
+		if hasPlugin {
+			typeSet[pluginType] = struct{}{}
+		}
+	}
+
+	var pluginTypes []consts.PluginType
+	for k := range typeSet {
+		pluginTypes = append(pluginTypes, k)
+	}
+
+	return pluginTypes, nil
+}
+
+// Check if a plugin of a specific type and name is available in the plugin catalog.
+// Caller must have a read lock on c.lock
+func (c *PluginCatalog) hasPluginWithTypeLocked(ctx context.Context, name string, pluginType consts.PluginType) (bool, error) {
+	// If the directory isn't set only look for builtin plugins.
+	if c.directory != "" {
+		storagePath := path.Join(pluginType.String(), name)
+
+		// Check for matching unversioned plugin
+		out, err := c.catalogView.Get(ctx, storagePath)
+		if err != nil {
+			return false, err
+		}
+		if out != nil {
+			return true, nil
+		}
+
+		// Check if there is at least one matching versioned plugin
+		entries, err := c.catalogView.List(ctx, storagePath+"/")
+		if err != nil {
+			return false, err
+		}
+		if len(entries) > 0 {
+			return true, nil
+		}
+	}
+
+	// Finally, check for matching builtin plugins
+	if _, ok := c.builtinRegistry.Get(name, pluginType); ok {
+		return true, nil
+	}
+	return false, nil
 }
 
 // Set registers a new external plugin with the catalog, or updates an existing

vault/plugin_reload.go
@@ -85,6 +85,24 @@ func (c *Core) reloadMatchingPlugin(ctx context.Context, pluginName string) erro
 		return err
 	}
 
+	// Fail early if the plugin is a singleton plugin
+	if slices.Contains(singletonMounts, pluginName) {
+		return fmt.Errorf("%w: %q", ErrSingletonPluginNotReloadable, pluginName)
+	}
+
+	// Check if the plugin is in the plugin catalog and is of a reloadable type
+	types, err := c.pluginCatalog.TypesFromName(ctx, pluginName)
+	if err != nil {
+		return err
+	}
+	if len(types) == 0 {
+		return fmt.Errorf("%w: %q", ErrPluginNotFound, pluginName)
+	}
+	// Only plugin of types auth and secret can be reloaded
+	if !slices.Contains(types, consts.PluginTypeCredential) && !slices.Contains(types, consts.PluginTypeSecrets) {
+		return fmt.Errorf("plugin %q cannot be reloaded, only plugins of type auth and secret can be reloaded", pluginName)
+	}
+
 	// Filter mount entries that only matches the plugin name
 	for _, entry := range c.mounts.Entries {
 		// We dont reload mounts that are not in the same namespace
@@ -129,8 +147,7 @@ func (c *Core) reloadBackendCommon(ctx context.Context, entry *routing.MountEntr
 	// We don't want to reload the singleton mounts. They often have specific
 	// inmemory elements and we don't want to touch them here.
 	if slices.Contains(singletonMounts, entry.Type) {
-		c.logger.Debug("skipping reload of singleton mount", "type", entry.Type)
-		return nil
+		return fmt.Errorf("%w: %q", ErrSingletonPluginNotReloadable, entry.Type)
 	}
 
 	path := entry.Path

vault/external_tests/plugin/external_plugin_test.go
@@ -749,6 +749,9 @@ func TestExternalPlugin_Database(t *testing.T) {
 // TestExternalPlugin_DatabaseReload tests that we can use an external database
 // secrets engine after reload
 func TestExternalPlugin_DatabaseReload(t *testing.T) {
+	// TODO: revisit this when database plugins reloading is supported
+	t.Skip("reloading database plugins is not yet supported")
+
 	cluster := getCluster(t, consts.PluginTypeDatabase, 1)
 	defer cluster.Cleanup()
 
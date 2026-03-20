openbao/openbao

commit 7ded341e7d50de563a99e45999bf4f2694a7790f

Add Auto Unseal plugins (#2586)
Signed-off-by: Jonas Köhnen <jonas.koehnen@sap.com>

vault/core.go
@@ -1076,12 +1076,7 @@ func NewCore(conf *CoreConfig) (*Core, error) {
 		return c, nil
 	}
 
-	if conf.PluginDirectory != "" {
-		c.pluginDirectory, err = filepath.Abs(conf.PluginDirectory)
-		if err != nil {
-			return nil, fmt.Errorf("core setup failed, could not verify plugin directory: %w", err)
-		}
-	}
+	c.pluginDirectory = conf.PluginDirectory
 
 	if conf.PluginFileUid != 0 {
 		c.pluginFileUid = conf.PluginFileUid
@@ -2330,7 +2325,7 @@ func (readonlyUnsealStrategy) unsealShared(ctx context.Context, c *Core, standby
 	if err := c.setupPluginCatalog(ctx); err != nil {
 		return err
 	}
-	if err := c.reconcileOCIPlugins(ctx, standby); err != nil {
+	if err := c.registerDeclarativePlugins(ctx, standby); err != nil {
 		return err
 	}
 	if err := c.setupNamespaceStore(ctx); err != nil {
@@ -3671,7 +3666,7 @@ func (c *Core) CheckPluginPerms(pluginName string) (err error) {
 		var err error
 		enableFilePermissionsCheck, err = strconv.ParseBool(enableFilePermissionsCheckEnv)
 		if err != nil {
-			return errors.New("Error parsing the environment variable VAULT_ENABLE_FILE_PERMISSIONS_CHECK")
+			return fmt.Errorf("failed to parse environment variable %s", consts.VaultEnableFilePermissionsCheckEnv)
 		}
 	}
 

api/plugin_types.go
@@ -14,6 +14,7 @@ var PluginTypes = []PluginType{
 	PluginTypeCredential,
 	PluginTypeDatabase,
 	PluginTypeSecrets,
+	PluginTypeKMS,
 }
 
 type PluginType uint32
@@ -33,6 +34,7 @@ const (
 	PluginTypeCredential
 	PluginTypeDatabase
 	PluginTypeSecrets
+	PluginTypeKMS
 )
 
 func (p PluginType) String() string {
@@ -45,6 +47,8 @@ func (p PluginType) String() string {
 		return "database"
 	case PluginTypeSecrets:
 		return "secret"
+	case PluginTypeKMS:
+		return "kms"
 	default:
 		return "unsupported"
 	}
@@ -60,6 +64,8 @@ func ParsePluginType(pluginType string) (PluginType, error) {
 		return PluginTypeDatabase, nil
 	case "secret":
 		return PluginTypeSecrets, nil
+	case "kms":
+		return PluginTypeKMS, nil
 	default:
 		return PluginTypeUnknown, fmt.Errorf("%q is not a supported plugin type", pluginType)
 	}

command/operator_diagnose.go
@@ -5,6 +5,7 @@ package command
 
 import (
 	"context"
+	"crypto/rand"
 	"encoding/json"
 	"errors"
 	"fmt"
@@ -14,16 +15,15 @@ import (
 	"sync"
 	"time"
 
-	wrapping "github.com/openbao/go-kms-wrapping/v2"
-
 	"github.com/hashicorp/cli"
 	log "github.com/hashicorp/go-hclog"
 	"github.com/hashicorp/go-secure-stdlib/reloadutil"
 	uuid "github.com/hashicorp/go-uuid"
-
+	wrapping "github.com/openbao/go-kms-wrapping/v2"
 	bApi "github.com/openbao/openbao/api/v2"
 	cserver "github.com/openbao/openbao/command/server"
 	"github.com/openbao/openbao/helper/configutil"
+	"github.com/openbao/openbao/helper/kmsplugin"
 	"github.com/openbao/openbao/helper/listenerutil"
 	"github.com/openbao/openbao/helper/metricsutil"
 	"github.com/openbao/openbao/physical/raft"
@@ -276,6 +276,12 @@ func (c *OperatorDiagnoseCommand) offlineDiagnostics(ctx context.Context) error 
 		return nil
 	})
 
+	var kms *kmsplugin.Catalog
+	_ = diagnose.Test(ctx, "Check KMS Plugin Catalog", func(context.Context) (err error) {
+		kms, err = kmsplugin.NewCatalog(server.logger, config)
+		return err
+	})
+
 	var metricSink *metricsutil.ClusterMetricSink
 	var metricsHelper *metricsutil.MetricsHelper
 
@@ -381,7 +387,7 @@ func (c *OperatorDiagnoseCommand) offlineDiagnostics(ctx context.Context) error 
 	var sealConfigError error
 
 	infoKeys := make([]string, 0)
-	barrierSeal, barrierWrapper, unwrapSeal, seals, sealConfigError, err := setSeal(server, config, &infoKeys, make(map[string]string))
+	barrierSeal, barrierWrapper, unwrapSeal, seals, sealConfigError, err := setSeal(server, config, kms, &infoKeys, make(map[string]string))
 	// Check error here
 	if err != nil {
 		diagnose.Advise(ctx, "For assistance with the seal stanza, see the Vault configuration documentation.")
@@ -474,20 +480,7 @@ SEALFAIL:
 		return nil
 	})
 
-	var coreConfig vault.CoreConfig
-	_ = diagnose.Test(ctx, "Create Core Configuration", func(ctx context.Context) error {
-		var secureRandomReader io.Reader
-		// prepare a secure random reader for core
-		randReaderTestName := "Initialize Randomness for Core"
-		secureRandomReader, err = configutil.CreateSecureRandomReaderFunc(config.SharedConfig, barrierWrapper)
-		if err != nil {
-			//nolint:staticcheck // user-facing error
-			return diagnose.SpotError(ctx, randReaderTestName, fmt.Errorf("Could not initialize randomness for core: %w.", err))
-		}
-		diagnose.SpotOk(ctx, randReaderTestName, "")
-		coreConfig = createCoreConfig(server, config, *backend, configSR, barrierSeal, unwrapSeal, metricsHelper, metricSink, secureRandomReader)
-		return nil
-	})
+	coreConfig := createCoreConfig(server, config, *backend, configSR, barrierSeal, unwrapSeal, metricsHelper, metricSink, rand.Reader)
 
 	var disableClustering bool
 	_ = diagnose.Test(ctx, "HA Storage", func(ctx context.Context) error {

command/server.go
@@ -5,6 +5,7 @@ package command
 
 import (
 	"context"
+	"crypto/rand"
 	"encoding/base64"
 	"errors"
 	"fmt"
@@ -39,11 +40,13 @@ import (
 	"github.com/openbao/openbao/command/server"
 	"github.com/openbao/openbao/helper/builtinplugins"
 	"github.com/openbao/openbao/helper/configutil"
+	"github.com/openbao/openbao/helper/kmsplugin"
 	"github.com/openbao/openbao/helper/listenerutil"
 	loghelper "github.com/openbao/openbao/helper/logging"
 	"github.com/openbao/openbao/helper/metricsutil"
 	"github.com/openbao/openbao/helper/namespace"
 	"github.com/openbao/openbao/helper/osutil"
+	"github.com/openbao/openbao/helper/pluginutil/oci"
 	"github.com/openbao/openbao/helper/profiles"
 	"github.com/openbao/openbao/helper/testhelpers/teststorage"
 	"github.com/openbao/openbao/helper/useragent"
@@ -411,6 +414,17 @@ func (c *ServerCommand) runRecoveryMode() int {
 
 	logProxyEnvironmentVariables(c.logger)
 
+	if err := c.downloadOCIPlugins(context.Background(), config); err != nil {
+		c.UI.Error(fmt.Sprintf("Error downloading plugins: %s", err))
+		return 1
+	}
+
+	kms, err := kmsplugin.NewCatalog(c.logger, config)
+	if err != nil {
+		c.UI.Error(fmt.Sprintf("Error creating KMS plugin catalog: %s", err))
+		return 1
+	}
+
 	// Initialize the storage backend
 	factory, exists := c.PhysicalBackends[config.Storage.Type]
 	if !exists {
@@ -440,10 +454,6 @@ func (c *ServerCommand) runRecoveryMode() int {
 	info["log level"] = config.LogLevel
 	infoKeys = append(infoKeys, "log level")
 
-	var barrierSeal vault.Seal
-	var sealConfigError error
-	var wrapper wrapping.Wrapper
-
 	if len(config.Seals) == 0 {
 		config.Seals = append(config.Seals, &configutil.KMS{Type: wrapping.WrapperTypeShamir.String()})
 	}
@@ -460,29 +470,27 @@ func (c *ServerCommand) runRecoveryMode() int {
 		configSeal.Type = sealType
 	}
 
-	infoKeys = append(infoKeys, "Seal Type")
-	info["Seal Type"] = sealType
-
 	var seal vault.Seal
-	defaultSeal := vault.NewDefaultSeal(vaultseal.NewAccess(vaultseal.NewShamirWrapper()))
-	sealLogger := c.logger.ResetNamed(fmt.Sprintf("seal.%s", sealType))
-	wrapper, sealConfigError = configutil.ConfigureWrapper(configSeal, &infoKeys, &info, sealLogger)
-	if sealConfigError != nil {
-		if !errwrap.ContainsType(sealConfigError, new(logical.KeyNotFoundError)) {
-			c.UI.Error(fmt.Sprintf(
-				"Error parsing Seal configuration: %s", sealConfigError))
+	switch configSeal.Type {
+	case string(wrapping.WrapperTypeShamir):
+		seal = vault.NewDefaultSeal(vaultseal.NewAccess(vaultseal.NewShamirWrapper()))
+	default:
+		wrapper, config, err := kms.ConfigureWrapper(
+			context.Background(), configSeal.Type, wrapping.WithConfigMap(configSeal.Config))
+		if err != nil {
+			c.UI.Error(fmt.Sprintf("Error configuring seal %q: %s", configSeal.Type, err))
 			return 1
 		}
-	}
-	if wrapper == nil {
-		seal = defaultSeal
-	} else {
+
 		seal, err = vault.NewAutoSeal(vaultseal.NewAccess(wrapper))
 		if err != nil {
-			c.UI.Error(fmt.Sprintf("error creating auto seal: %v", err))
+			c.UI.Error(fmt.Sprintf("Error creating auto seal: %s", err))
+			return 1
 		}
+
+		info["auto seal"] = formatProps(configSeal.Type, config.Metadata)
+		infoKeys = append(infoKeys, "auto seal")
 	}
-	barrierSeal = seal
 
 	// Ensure that the seal finalizer is called, even if using verify-only
 	defer func() {
@@ -495,7 +503,7 @@ func (c *ServerCommand) runRecoveryMode() int {
 	coreConfig := &vault.CoreConfig{
 		Physical:     backend,
 		StorageType:  config.Storage.Type,
-		Seal:         barrierSeal,
+		Seal:         seal,
 		LogLevel:     config.LogLevel,
 		Logger:       c.logger,
 		RecoveryMode: c.flagRecovery,
@@ -613,18 +621,6 @@ func (c *ServerCommand) runRecoveryMode() int {
 				c.UI.Error(fmt.Sprintf("HTTP server (listening on %s) exited with error: %v", ln.Addr().String(), err))
 			}
 		}(ln.Listener)
-	}
-
-	if sealConfigError != nil {
-		init, err := core.InitializedLocally(context.Background())
-		if err != nil {
-			c.UI.Error(fmt.Sprintf("Error checking if core is initialized: %v", err))
-			return 1
-		}
-		if init {
-			c.UI.Error("Vault is initialized but no Seal key could be loaded")
-			return 1
-		}
 	}
 
 	if newCoreError != nil {
@@ -829,15 +825,8 @@ func (c *ServerCommand) InitListeners(logger hclog.Logger, config *server.Config
 
 		// Store the listener props for output later
 		key := fmt.Sprintf("listener %d", i+1)
-		propsList := make([]string, 0, len(props))
-		for k, v := range props {
-			propsList = append(propsList, fmt.Sprintf(
-				"%s: %q", k, v))
-		}
-		sort.Strings(propsList)
 		*infoKeys = append(*infoKeys, key)
-		(*info)[key] = fmt.Sprintf(
-			"%s (%s)", lnConfig.Type, strings.Join(propsList, ", "))
+		(*info)[key] = formatProps(lnConfig.Type, props)
 
 	}
 	if !disableClustering {
@@ -1026,6 +1015,17 @@ func (c *ServerCommand) Run(args []string) int {
 
 	logProxyEnvironmentVariables(c.logger)
 
+	if err := c.downloadOCIPlugins(context.Background(), config); err != nil {
+		c.UI.Error(fmt.Sprintf("Error downloading plugins: %s", err))
+		return 1
+	}
+
+	kms, err := kmsplugin.NewCatalog(c.logger, config)
+	if err != nil {
+		c.UI.Error(fmt.Sprintf("Error creating KMS plugin catalog: %s", err))
+		return 1
+	}
+
 	inmemMetrics, metricSink, prometheusEnabled, err := configutil.SetupTelemetry(&configutil.SetupTelemetryOpts{
 		Config:      config.Telemetry,
 		Ui:          c.UI,
@@ -1084,7 +1084,7 @@ func (c *ServerCommand) Run(args []string) int {
 	info[key] = strings.Join(envVarKeys, ", ")
 	infoKeys = append(infoKeys, key)
 
-	barrierSeal, barrierWrapper, unwrapSeal, seals, sealConfigError, err := setSeal(c, config, &infoKeys, info)
+	barrierSeal, _, unwrapSeal, seals, sealConfigError, err := setSeal(c, config, kms, &infoKeys, info)
 	// Check error here
 	if err != nil {
 		c.UI.Error(err.Error())
@@ -1112,14 +1112,7 @@ func (c *ServerCommand) Run(args []string) int {
 		return 1
 	}
 
-	// prepare a secure random reader for core
-	secureRandomReader, err := configutil.CreateSecureRandomReaderFunc(config.SharedConfig, barrierWrapper)
-	if err != nil {
-		c.UI.Error(err.Error())
-		return 1
-	}
-
-	coreConfig := createCoreConfig(c, config, backend, configSR, barrierSeal, unwrapSeal, metricsHelper, metricSink, secureRandomReader)
+	coreConfig := createCoreConfig(c, config, backend, configSR, barrierSeal, unwrapSeal, metricsHelper, metricSink, rand.Reader)
 	if c.flagDevThreeNode {
 		return c.enableThreeNodeDevCluster(&coreConfig, info, infoKeys, c.flagDevListenAddr, api.ReadBaoVariable("BAO_DEV_TEMP_DIR"))
 	}
@@ -1492,8 +1485,12 @@ func (c *ServerCommand) Run(args []string) int {
 			// c.Reload as it needs the reloadFuncsLock.
 			core.ReloadAuditLogs()
 
-			// Update plugins as necessary.
-			core.ReloadPlugins()
+			if err := c.downloadOCIPlugins(context.Background(), config); err != nil {
+				c.logger.Error("failed to re-download plugins", "error", err.Error())
+			} else {
+				// Update plugins as necessary.
+				core.ReloadPlugins()
+			}
 
 			// Reload log level for loggers
 			if config.LogLevel != "" {
@@ -2346,6 +2343,18 @@ func (c *ServerCommand) removePidFile(pidPath string) error {
 	return os.Remove(pidPath)
 }
 
+func (c *ServerCommand) downloadOCIPlugins(ctx context.Context, config *server.Config) error {
+	if !config.PluginAutoDownload || len(config.Plugins) == 0 || config.PluginDirectory == "" {
+		return nil
+	}
+
+	logger := c.logger.Named("plugins")
+	logger.Info("starting OCI plugin downloading")
+	defer logger.Info("OCI plugin downloading completed")
+
+	return oci.NewPluginDownloader(config.PluginDirectory, config, logger).ReconcilePlugins(ctx)
+}
+
 // storageMigrationActive checks and warns against in-progress storage migrations.
 // This function will block until storage is available.
 func (c *ServerCommand) storageMigrationActive(backend physical.Backend) bool {
@@ -2403,13 +2412,11 @@ func CheckStorageMigration(b physical.Backend) (*StorageMigrationStatus, error) 
 
 // setSeal return barrierSeal, barrierWrapper, unwrapSeal, and all the created seals from the configs so we can close them in Run
 // The two errors are the sealConfigError and the regular error
-func setSeal(c *ServerCommand, config *server.Config, infoKeys *[]string, info map[string]string) (vault.Seal, wrapping.Wrapper, vault.Seal, []vault.Seal, error, error) {
+func setSeal(c *ServerCommand, config *server.Config, kms *kmsplugin.Catalog, infoKeys *[]string, info map[string]string) (vault.Seal, wrapping.Wrapper, vault.Seal, []vault.Seal, error, error) {
 	var barrierSeal vault.Seal
+	var barrierWrapper wrapping.Wrapper
 	var unwrapSeal vault.Seal
 
-	var sealConfigError error
-	var wrapper wrapping.Wrapper
-	var barrierWrapper wrapping.Wrapper
 	if c.flagDevAutoSeal {
 		var err error
 		access, _ := vaultseal.NewTestSeal(nil)
@@ -2440,42 +2447,41 @@ func setSeal(c *ServerCommand, config *server.Config, infoKeys *[]string, info m
 		}
 
 		var seal vault.Seal
-		sealLogger := c.logger.ResetNamed(fmt.Sprintf("seal.%s", sealType))
-		c.allLoggers = append(c.allLoggers, sealLogger)
-		defaultSeal := vault.NewDefaultSeal(vaultseal.NewAccess(vaultseal.NewShamirWrapper()))
-		var sealInfoKeys []string
-		sealInfoMap := map[string]string{}
-		wrapper, sealConfigError = configutil.ConfigureWrapper(configSeal, &sealInfoKeys, &sealInfoMap, sealLogger)
-		if sealConfigError != nil {
-			if !errwrap.ContainsType(sealConfigError, new(logical.KeyNotFoundError)) {
-				return barrierSeal, barrierWrapper, unwrapSeal, createdSeals, sealConfigError, fmt.Errorf(
-					"Error parsing Seal configuration: %s", sealConfigError)
+		switch configSeal.Type {
+		case string(wrapping.WrapperTypeShamir):
+			seal = vault.NewDefaultSeal(vaultseal.NewAccess(vaultseal.NewShamirWrapper()))
+		default:
+			wrapper, config, err := kms.ConfigureWrapper(
+				context.Background(), configSeal.Type, wrapping.WithConfigMap(configSeal.Config))
+			if err != nil {
+				//nolint:staticcheck // User-facing error.
+				return nil, nil, nil, nil, nil, fmt.Errorf("Error configuring seal %q: %w", configSeal.Type, err)
 			}
-		}
-		if wrapper == nil {
-			seal = defaultSeal
-		} else {
-			var err error
+
 			seal, err = vault.NewAutoSeal(vaultseal.NewAccess(wrapper))
 			if err != nil {
-				return nil, nil, nil, nil, nil, err
+				//nolint:staticcheck // User-facing error.
+				return nil, nil, nil, nil, nil, fmt.Errorf("Error creating auto seal: %w", err)
 			}
+
+			infoKey := "auto seal"
+			if configSeal.Disabled {
+				infoKey = "old auto seal"
+			}
+
+			info[infoKey] = formatProps(configSeal.Type, config.Metadata)
+			*infoKeys = append(*infoKeys, infoKey)
 		}
-		infoPrefix := ""
+
 		if configSeal.Disabled {
 			unwrapSeal = seal
-			infoPrefix = "Old "
 		} else {
 			barrierSeal = seal
-			barrierWrapper = wrapper
 		}
-		for _, k := range sealInfoKeys {
-			*infoKeys = append(*infoKeys, infoPrefix+k)
-			info[infoPrefix+k] = sealInfoMap[k]
-		}
+
 		createdSeals = append(createdSeals, seal)
 	}
-	return barrierSeal, barrierWrapper, unwrapSeal, createdSeals, sealConfigError, nil
+	return barrierSeal, barrierWrapper, unwrapSeal, createdSeals, nil, nil
 }
 
 func initHaBackend(c *ServerCommand, config *server.Config, coreConfig *vault.CoreConfig, backend physical.Backend) (bool, error) {
@@ -2946,6 +2952,31 @@ func startHttpServers(c *ServerCommand, core *vault.Core, config *server.Config,
 		}(ln.Listener)
 	}
 	return nil
+}
+
+// formatPropts returns a formatted info key/value such as:
+// Listener 1: tcp (addr: "127.0.0.1:8200", cluster address: "127.0.0.1:8201", ...)
+func formatProps(title string, m map[string]string) string {
+	if len(m) == 0 {
+		return title
+	}
+
+	props := make([]string, 0, len(m))
+	for k, v := range m {
+		if k == "" || v == "" {
+			continue
+		}
+		// Poor man's pretty-printer.
+		switch v {
+		case "true", "false":
+		default:
+			v = fmt.Sprintf("%q", v)
+		}
+		props = append(props, fmt.Sprintf("%s: %s", k, v))
+	}
+
+	sort.Strings(props)
+	return fmt.Sprintf("%s (%s)", title, strings.Join(props, ", "))
 }
 
 func SetStorageMigration(b physical.Backend, active bool) error {

command/server/config.go
@@ -40,10 +40,6 @@ const (
 	PluginDownloadContinue = "continue"
 )
 
-var entConfigValidate = func(_ *Config, _ string) []configutil.ConfigError {
-	return nil
-}
-
 // Config is the configuration for the vault server.
 type Config struct {
 	UnusedKeys configutil.UnusedKeyMap `hcl:",unusedKeyPositions"`
@@ -173,12 +169,7 @@ func (c *Config) Validate(sourceFilePath string) []configutil.ConfigError {
 		}
 	}
 
-	results = append(results, c.validateEnt(sourceFilePath)...)
 	return results
-}
-
-func (c *Config) validateEnt(sourceFilePath string) []configutil.ConfigError {
-	return entConfigValidate(c, sourceFilePath)
 }
 
 // DevConfig is a Config that is used for dev mode of OpenBao.
@@ -354,7 +345,7 @@ func (p *PluginConfig) Validate(sourceFilePath string) []configutil.ConfigError 
 	}
 
 	// Validate Type is valid
-	_, err := consts.ParsePluginType(p.Type)
+	typ, err := consts.ParsePluginType(p.Type)
 	if err != nil {
 		results = append(results, configutil.ConfigError{
 			Problem: fmt.Sprintf("plugin %q: %s", p.Name, err.Error()),
@@ -372,18 +363,13 @@ func (p *PluginConfig) Validate(sourceFilePath string) []configutil.ConfigError 
 		})
 	}
 
-	// Validate Version is not empty
-	if p.Version == "" {
-		results = append(results, configutil.ConfigError{
-			Problem: fmt.Sprintf("plugin %q: version cannot be empty", p.Slug()),
-		})
-	}
-
-	// Ensure Image:Version is a valid image reference
-	if _, err := name.ParseReference(p.URL()); err != nil {
-		results = append(results, configutil.ConfigError{
-			Problem: fmt.Sprintf("plugin %q: image and version do not form a valid image reference. %v", p.Slug(), err),
-		})
+	if p.Image != "" {
+		// Ensure Image:Version is a valid image reference
+		if _, err := name.ParseReference(p.URL()); err != nil {
+			results = append(results, configutil.ConfigError{
+				Problem: fmt.Sprintf("plugin %q: image and version do not form a valid image reference. %v", p.Slug(), err),
+			})
+		}
 	}
 
 	// Validate binary_name is not empty
@@ -391,6 +377,17 @@ func (p *PluginConfig) Validate(sourceFilePath string) []configutil.ConfigError 
 		results = append(results, configutil.ConfigError{
 			Problem: fmt.Sprintf("plugin %q: binary_name cannot be empty when image specified", p.Slug()),
 		})
+	}
+
+	if typ != consts.PluginTypeKMS || p.Image != "" {
+		// Validate version is not empty. KMS plugins do not require or enforce
+		// that a version is set. OCI-based plugins however require a version be
+		// set at all times.
+		if p.Version == "" {
+			results = append(results, configutil.ConfigError{
+				Problem: fmt.Sprintf("plugin %q: version cannot be empty", p.Slug()),
+			})
+		}
 	}
 
 	// Validate sha256sum is exactly 64 hex characters
@@ -708,7 +705,7 @@ func LoadConfig(path string, allPaths []string) (cfg *Config, err error) {
 			var err error
 			enableFilePermissionsCheck, err = strconv.ParseBool(enableFilePermissionsCheckEnv)
 			if err != nil {
-				return nil, errors.New("error parsing the environment variable BAO_ENABLE_FILE_PERMISSIONS_CHECK")
+				return nil, fmt.Errorf("failed to parse environment variable %s", consts.VaultEnableFilePermissionsCheckEnv)
 			}
 		}
 		f, err := os.Open(path)
@@ -783,7 +780,7 @@ func LoadConfigFile(path string, allPaths []string) (cfg *Config, err error) {
 		var err error
 		enableFilePermissionsCheck, err = strconv.ParseBool(enableFilePermissionsCheckEnv)
 		if err != nil {
-			return nil, errors.New("error parsing the environment variable BAO_ENABLE_FILE_PERMISSIONS_CHECK")
+			return nil, fmt.Errorf("failed to parse environment variable %s", consts.VaultEnableFilePermissionsCheckEnv)
 		}
 	}
 
@@ -795,7 +792,6 @@ func LoadConfigFile(path string, allPaths []string) (cfg *Config, err error) {
 		}
 		// check permissions of the plugin directory
 		if conf.PluginDirectory != "" {
-
 			err = osutil.OwnerPermissionsMatch(conf.PluginDirectory, conf.PluginFileUid, conf.PluginFilePermissions)
 			if err != nil {
 				return nil, err

command/server/config_oss_test.go
@@ -1,16 +1,0 @@
-// Copyright (c) HashiCorp, Inc.
-// SPDX-License-Identifier: MPL-2.0
-
-package server
-
-import (
-	"testing"
-)
-
-func TestLoadConfigFile_topLevel(t *testing.T) {
-	testLoadConfigFile_topLevel(t, nil)
-}
-
-func TestLoadConfigFile_json2(t *testing.T) {
-	testLoadConfigFile_json2(t, nil)
-}

command/server/config_test.go
@@ -155,3 +155,11 @@ func TestLoadConfigFile_IgnoreDuplicates(t *testing.T) {
 		}
 	}
 }
+
+func TestLoadConfigFile_topLevel(t *testing.T) {
+	testLoadConfigFile_topLevel(t)
+}
+
+func TestLoadConfigFile_json2(t *testing.T) {
+	testLoadConfigFile_json2(t)
+}

command/server/config_test_helpers.go
@@ -67,7 +67,7 @@ func testConfigRaftRetryJoin(t *testing.T) {
 	}
 }
 
-func testLoadConfigFile_topLevel(t *testing.T, entropy *configutil.Entropy) {
+func testLoadConfigFile_topLevel(t *testing.T) {
 	config, err := LoadConfigFile("./test-fixtures/config2.hcl", nil)
 	if err != nil {
 		t.Fatalf("err: %s", err)
@@ -177,7 +177,7 @@ func testLoadConfigFile_topLevel(t *testing.T, entropy *configutil.Entropy) {
 	}
 }
 
-func testLoadConfigFile_json2(t *testing.T, entropy *configutil.Entropy) {
+func testLoadConfigFile_json2(t *testing.T) {
 	config, err := LoadConfigFile("./test-fixtures/config2.hcl.json", nil)
 	if err != nil {
 		t.Fatalf("err: %s", err)

commit 22208041db3afd0a909d82942949443623f25a32

Fixup plugin directory
This is a regression from #2586 that had stale code removals I should've
added back; I'm hijacking this PR to do so.

Signed-off-by: Jonas Köhnen <jonas.koehnen@sap.com>

vault/core.go
@@ -1076,7 +1076,12 @@ func NewCore(conf *CoreConfig) (*Core, error) {
 		return c, nil
 	}
 
-	c.pluginDirectory = conf.PluginDirectory
+	if conf.PluginDirectory != "" {
+		c.pluginDirectory, err = filepath.Abs(conf.PluginDirectory)
+		if err != nil {
+			return nil, fmt.Errorf("core setup failed, could not verify plugin directory: %w", err)
+		}
+	}
 
 	if conf.PluginFileUid != 0 {
 		c.pluginFileUid = conf.PluginFileUid
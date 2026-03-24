7688989375b877395d74365178799a814243b287
Geoff Wilson <geoff@gr-oss.io>
2026-03-24 15:18:27 -0400

    missed vault/control_group.go and _test from diff
    Signed-off-by: Geoff Wilson <geoff@gr-oss.io>

---
+++ vault/control_group.go
@@ -0,0 +1,185 @@
+package vault
+
+import (
+	"context"
+	"fmt"
+	"slices"
+	"time"
+
+	"github.com/openbao/openbao/sdk/v2/helper/jsonutil"
+	"github.com/openbao/openbao/sdk/v2/helper/locksutil"
+	"github.com/openbao/openbao/sdk/v2/logical"
+)
+
+// makeLogicalControlGroup copies a vault.ControlGroup to a logical.ControlGroup
+func makeLogicalControlGroup(authResultsControlGroup *ControlGroup) *logical.ControlGroup {
+	if authResultsControlGroup == nil {
+		return nil
+	}
+
+	cg := &logical.ControlGroup{
+		TTL: authResultsControlGroup.TTL,
+	}
+	cg.Factors = make([]logical.ControlGroupFactor, len(authResultsControlGroup.Factors))
+	for i, factor := range authResultsControlGroup.Factors {
+		cg.Factors[i] = logical.ControlGroupFactor{
+			Name:                   factor.Name,
+			ControlledCapabilities: factor.ControlledCapabilities,
+			Identity: logical.ControlGroupIdentity{
+				GroupNames: factor.Identity.GroupNames,
+				Approvals:  factor.Identity.Approvals,
+			},
+		}
+	}
+	return cg
+}
+
+// getControlGroup fetches control group from a token entry (where present) given a token
+func (c *Core) getControlGroup(ctx context.Context, token string) (*logical.ControlGroup, error) {
+	tokenEntry, err := c.tokenStore.Lookup(ctx, token)
+	if err != nil {
+		return nil, err
+	}
+	return c.getControlGroupFromTokenEntry(ctx, tokenEntry)
+}
+
+// getControlGroupFromTokenEntry fetches control group from a token entry where present
+func (c *Core) getControlGroupFromTokenEntry(ctx context.Context, tokenEntry *logical.TokenEntry) (*logical.ControlGroup, error) {
+	controlGroup, ok := tokenEntry.Meta["control_group"]
+	if !ok {
+		// if there's no control group, nothing to return but it's not an error
+		// nolint: nilnil
+		return nil, nil
+	}
+
+	cg := logical.ControlGroup{}
+	if err := jsonutil.DecodeJSON([]byte(controlGroup), &cg); err != nil {
+		return nil, err
+	}
+
+	return &cg, nil
+}
+
+// setControlGroupInTokenEntry replaces the control group meta data on a given token entry
+func (c *Core) setControlGroupInTokenEntry(ctx context.Context, tokenEntry *logical.TokenEntry, cg *logical.ControlGroup) error {
+	cgJson, err := jsonutil.EncodeJSON(cg)
+	if err != nil {
+		return err
+	}
+	if tokenEntry.Meta == nil {
+		tokenEntry.Meta = map[string]string{}
+	}
+	tokenEntry.Meta["control_group"] = string(cgJson)
+	return c.tokenStore.store(ctx, tokenEntry)
+}
+
+// setControlGroup sets/replaces control group metadata on a token entry
+func (c *Core) setControlGroup(ctx context.Context, token string, cg *logical.ControlGroup) error {
+	tokenEntry, err := c.tokenStore.Lookup(ctx, token)
+	if err != nil {
+		return err
+	}
+	if tokenEntry == nil {
+		return nil
+	}
+	return c.setControlGroupInTokenEntry(ctx, tokenEntry, cg)
+}
+
+// validateControlGroup checks for a passing control group factor; passes if there is no control group config
+func (c *Core) validateControlGroup(ctx context.Context, token string, requestCapability logical.Operation) (bool, error) {
+	cg, err := c.getControlGroup(ctx, token)
+	if err != nil {
+		return false, err
+	}
+	// when no control group policy found, we pass this check
+	if cg == nil {
+		return true, nil
+	}
+
+	applicableControlGroups := false
+	for _, factor := range cg.Factors {
+
+		// does factor apply?
+		if slices.Contains(factor.ControlledCapabilities, requestCapability) {
+			applicableControlGroups = true
+		}
+		
+		// count authorizations which have not expired
+		approvalCount := 0
+		for _, auth := range factor.Authorizations {
+			if auth.Timestamp.Add(cg.TTL).After(time.Now()) {
+				approvalCount++
+			}
+		}
+
+		// any factor having approvalCount >= required approvals
+		// will validate the token
+		if approvalCount >= factor.Identity.Approvals {
+			return true, nil
+		}
+	}
+	if !applicableControlGroups {
+		return true, nil
+	}
+	
+	return false, nil
+}
+
+// addAuthorization updates the control group metadata on the token with the given approval if applicable.
+// rw lock is obtained and released during the operation.
+func (c *Core) addAuthorization(ctx context.Context, token string, approver *logical.Auth) error {
+	// obtain rw lock for the token
+	lock := locksutil.LockForKey(c.tokenStore.tokenLocks, token)
+	lock.Lock()
+	defer lock.Unlock()
+
+	tokenEntry, err := c.tokenStore.lookupInternal(ctx, token, false, false)
+	if err != nil {
+		return err
+	}
+
+	cg, err := c.getControlGroupFromTokenEntry(ctx, tokenEntry)
+	if err != nil {
+		return err
+	}
+
+	// if there's no control group, no action taken but not an error
+	if cg == nil {
+		return nil
+	}
+
+	addingAuthorization := false
+	for i, factor := range cg.Factors {
+		identityGroups := factor.Identity.GroupNames
+		for _, group := range approver.GroupAliases {
+			if slices.Contains(identityGroups, group.Name) {
+				// make sure token doesn't have same identity as approver
+				if tokenEntry.DisplayName == approver.DisplayName {
+					return fmt.Errorf("token owner cannot be approver")
+				}
+
+				// make sure approver hasn't already approved
+				for _, auth := range factor.Authorizations {
+					if auth.Approver == approver.DisplayName {
+						return fmt.Errorf("approver has already authorized")
+					}
+				}
+
+				addingAuthorization = true
+				cg.Factors[i].Authorizations = append(factor.Authorizations, logical.ControlGroupAuthorization{
+					Timestamp: time.Now(),
+					Approver:  approver.DisplayName,
+				})
+			}
+		}
+	}
+
+	if addingAuthorization {
+		err = c.setControlGroupInTokenEntry(ctx, tokenEntry, cg)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
---
+++ vault/control_group_test.go
@@ -0,0 +1,307 @@
+package vault
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/openbao/openbao/helper/namespace"
+	"github.com/openbao/openbao/sdk/v2/helper/jsonutil"
+	"github.com/openbao/openbao/sdk/v2/logical"
+	"github.com/stretchr/testify/require"
+)
+
+func TestControlGroup_makeLogicalControlGroup(t *testing.T) {
+	input := &ControlGroup{
+		TTL: 14440,
+		Factors: []ControlGroupFactor{
+			{
+				Name:                   "tester",
+				ControlledCapabilities: []logical.Operation{logical.CreateOperation},
+				Identity: ControlGroupIdentity{
+					GroupNames: []string{"admin"},
+					Approvals:  2,
+				},
+			},
+		},
+	}
+	output := makeLogicalControlGroup(input)
+	require.IsType(t, logical.ControlGroup{}, *output)
+	require.Equal(t, time.Duration(14440), output.TTL)
+	require.Equal(t, "tester", output.Factors[0].Name)
+	require.Equal(t, []string{"create"}, output.Factors[0].ControlledCapabilities)
+	require.Equal(t, []string{"admin"}, output.Factors[0].Identity.GroupNames)
+
+	output = makeLogicalControlGroup(nil)
+	require.Empty(t, output)
+}
+
+func TestControlGroup_getControlGroup(t *testing.T) {
+	c, _, _ := TestCoreUnsealed(t)
+
+	ctx := namespace.RootContext(context.Background())
+	originalCG := logical.ControlGroup{
+		TTL: time.Duration(14440),
+		Factors: []logical.ControlGroupFactor{
+			{
+				Name: "test-cg",
+				Identity: logical.ControlGroupIdentity{
+					GroupNames: []string{"secops"},
+					Approvals:  2,
+				},
+			},
+		},
+	}
+
+	cg, err := jsonutil.EncodeJSON(originalCG)
+	require.Nil(t, err)
+
+	creationTime := time.Now()
+	te := logical.TokenEntry{
+		Path:           "token/create",
+		Policies:       []string{"response-wrapping"},
+		CreationTime:   creationTime.Unix(),
+		TTL:            time.Hour,
+		NumUses:        3,
+		ExplicitMaxTTL: time.Hour,
+		NamespaceID:    "root",
+		Meta: map[string]string{
+			"ttl":           "600s",
+			"control_group": string(cg),
+		},
+	}
+
+	testMakeTokenDirectly(t, ctx, c.tokenStore, &te)
+	require.NotEmpty(t, te.ID)
+
+	// Fetch control group via token
+	fetchedCG, err := c.getControlGroup(ctx, te.ID)
+	require.Nil(t, err)
+	require.Equal(t, &originalCG, fetchedCG)
+}
+
+func TestControlGroup_setControlGroup(t *testing.T) {
+	c, _, _ := TestCoreUnsealed(t)
+
+	creationTime := time.Now()
+	te := &logical.TokenEntry{
+		Path:           "token/create",
+		Policies:       []string{"response-wrapping"},
+		CreationTime:   creationTime.Unix(),
+		TTL:            time.Hour,
+		NumUses:        3,
+		ExplicitMaxTTL: time.Hour,
+		NamespaceID:    "root",
+		Meta: map[string]string{
+			"ttl": "600s",
+		},
+	}
+
+	ctx := namespace.RootContext(context.Background())
+	testMakeTokenDirectly(t, ctx, c.tokenStore, te)
+	require.NotEmpty(t, te.ID)                 // id has been created
+	require.Empty(t, te.Meta["control_group"]) // no control group
+
+	cg := logical.ControlGroup{
+		TTL: time.Duration(14440),
+		Factors: []logical.ControlGroupFactor{
+			{
+				Name: "test-cg",
+				Identity: logical.ControlGroupIdentity{
+					GroupNames: []string{"secops"},
+					Approvals:  2,
+				},
+			},
+		},
+	}
+
+	// Set control group via token
+	err := c.setControlGroup(ctx, te.ID, &cg)
+	require.Nil(t, err)
+
+	// Token entry should now have control group
+	te, err = c.tokenStore.Lookup(ctx, te.ID)
+	require.Nil(t, err)
+
+	require.NotEmpty(t, te)
+	require.NotEmpty(t, te.Meta["control_group"])
+}
+
+func TestControlGroup_addAuthorization(t *testing.T) {
+	c, _, _ := TestCoreUnsealed(t)
+
+	creationTime := time.Now()
+	te := &logical.TokenEntry{
+		Path:           "token/create",
+		Policies:       []string{"response-wrapping"},
+		CreationTime:   creationTime.Unix(),
+		TTL:            time.Hour,
+		NumUses:        3,
+		ExplicitMaxTTL: time.Hour,
+		NamespaceID:    "root",
+		Meta: map[string]string{
+			"ttl": "600s",
+		},
+	}
+
+	ctx := namespace.RootContext(context.Background())
+	testMakeTokenDirectly(t, ctx, c.tokenStore, te)
+	require.NotEmpty(t, te.ID)                 // id has been created
+	require.Empty(t, te.Meta["control_group"]) // no control group
+
+	cg := logical.ControlGroup{
+		TTL: time.Duration(14440),
+		Factors: []logical.ControlGroupFactor{
+			{
+				Name: "test-secops",
+				Identity: logical.ControlGroupIdentity{
+					GroupNames: []string{"secops"},
+					Approvals:  2,
+				},
+			},
+			{
+				Name: "test-admin",
+				Identity: logical.ControlGroupIdentity{
+					GroupNames: []string{"admin"},
+					Approvals:  2,
+				},
+			},
+			{
+				Name: "test-both",
+				Identity: logical.ControlGroupIdentity{
+					GroupNames: []string{"admin", "secops"},
+					Approvals:  2,
+				},
+			},
+		},
+	}
+
+	// Set control group via token
+	err := c.setControlGroup(ctx, te.ID, &cg)
+	require.Nil(t, err)
+
+	// addAuthorzation
+	var groups []*logical.Alias
+	groups = append(groups, &logical.Alias{
+		Name: "secops",
+	})
+	auth := logical.Auth{
+		DisplayName:  "user@example.com",
+		GroupAliases: groups,
+	}
+	err = c.addAuthorization(ctx, te.ID, &auth)
+	require.Nil(t, err)
+
+	// Token entry should now have the authorization
+	cgFetched, err := c.getControlGroup(ctx, te.ID)
+	require.Nil(t, err)
+
+	// expect all matching factors to receive an authorization
+	require.NotEmpty(t, cgFetched)
+	require.Len(t, cgFetched.Factors[0].Authorizations, 1)
+	require.Len(t, cgFetched.Factors[1].Authorizations, 0)
+	require.Len(t, cgFetched.Factors[2].Authorizations, 1)
+
+	// Second authorization by same user will result in error
+	err = c.addAuthorization(ctx, te.ID, &auth)
+	require.NotNil(t, err)
+
+	// Authorization by the token owner will result in error
+	auth.DisplayName = te.DisplayName
+	err = c.addAuthorization(ctx, te.ID, &auth)
+	require.NotNil(t, err)
+}
+
+func TestControlGroup_validateControlGroup(t *testing.T) {
+	c, _, _ := TestCoreUnsealed(t)
+
+	creationTime := time.Now()
+	te := &logical.TokenEntry{
+		Path:           "token/create",
+		Policies:       []string{"response-wrapping"},
+		CreationTime:   creationTime.Unix(),
+		TTL:            time.Hour,
+		NumUses:        3,
+		ExplicitMaxTTL: time.Hour,
+		NamespaceID:    "root",
+		Meta: map[string]string{
+			"ttl": "600s",
+		},
+	}
+
+	ctx := namespace.RootContext(context.Background())
+	testMakeTokenDirectly(t, ctx, c.tokenStore, te)
+	require.NotEmpty(t, te.ID)                 // id has been created
+	require.Empty(t, te.Meta["control_group"]) // no control group
+
+	cg := logical.ControlGroup{
+		TTL: time.Duration(1 * time.Second),
+		Factors: []logical.ControlGroupFactor{
+			{
+				Name: "test-secops",
+				ControlledCapabilities: []logical.Operation{logical.ReadOperation},
+				Identity: logical.ControlGroupIdentity{
+					GroupNames: []string{"secops"},
+					Approvals:  2,
+				},
+			},
+			{
+				Name: "test-admin",
+				ControlledCapabilities: []logical.Operation{logical.ReadOperation},
+				Identity: logical.ControlGroupIdentity{
+					GroupNames: []string{"admin"},
+					Approvals:  2,
+				},
+			},
+			{
+				Name: "test-both",
+				ControlledCapabilities: []logical.Operation{logical.ReadOperation},
+				Identity: logical.ControlGroupIdentity{
+					GroupNames: []string{"admin", "secops"},
+					Approvals:  2,
+				},
+			},
+		},
+	}
+
+	// Set control group via token
+	err := c.setControlGroup(ctx, te.ID, &cg)
+	require.Nil(t, err)
+
+	// addAuthorzation
+	var groups []*logical.Alias
+	groups = append(groups, &logical.Alias{
+		Name: "secops",
+	})
+	auth := logical.Auth{
+		DisplayName:  "user@example.com",
+		GroupAliases: groups,
+	}
+	err = c.addAuthorization(ctx, te.ID, &auth)
+	require.Nil(t, err)
+
+	// Should not yet validate for Read
+	validates, err := c.validateControlGroup(ctx, te.ID, logical.ReadOperation)
+	require.Nil(t, err)
+	require.False(t, validates)
+
+	// but should validate for unprotected operation 
+	validates, err = c.validateControlGroup(ctx, te.ID, logical.ListOperation)
+	require.Nil(t, err)
+	require.True(t, validates)
+
+	// Second auth by different user should authorize
+	auth.DisplayName = "different.user@example.com"
+	err = c.addAuthorization(ctx, te.ID, &auth)
+	require.Nil(t, err)
+	// now validates for Read
+	validates, err = c.validateControlGroup(ctx, te.ID, logical.ReadOperation)
+	require.Nil(t, err)
+	require.True(t, validates)
+
+	// After TTL, authorizations should expire
+	time.Sleep(1 * time.Second)
+	validates, err = c.validateControlGroup(ctx, te.ID, logical.ReadOperation)
+	require.Nil(t, err)
+	require.False(t, validates)
+}

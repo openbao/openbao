67c35c97ed286bac16cf12195e036091fc67e577
Alexander Scheel <ascheel@gitlab.com>
2026-03-27 08:06:32 -0500

    Add CEL source in profiles (#2671)
    * Add CEL source in profiles
    
    CEL is a useful tool in profiles as it gives more verbose control for
    operators than simple Go templating. This essentially gives us room to
    inject additional arbitrary functions into the CEL source engine. In the
    future, we can allow configuration of the evaluation config, though I
    don't think we'll immediately need it.
    
    Signed-off-by: Alexander Scheel <ascheel@gitlab.com>
    
    * Add documentation on CEL source
    
    Signed-off-by: Alexander Scheel <ascheel@gitlab.com>
    
    * Add changelog entry
    
    Signed-off-by: Alexander Scheel <ascheel@gitlab.com>
    
    ---------
    
    Signed-off-by: Alexander Scheel <ascheel@gitlab.com>

---
+++ helper/profiles/cel_source.go
@@ -0,0 +1,181 @@
+package profiles
+
+import (
+	"context"
+	"errors"
+	"fmt"
+
+	"github.com/google/cel-go/cel"
+	"github.com/google/cel-go/common/types"
+	celHelper "github.com/openbao/openbao/sdk/v2/helper/cel"
+)
+
+// CELSourceBuilder allows reading inputs from the CEL engine,
+// allowing for more advanced operations than the template engine.
+// This includes reading environment variables and files.
+//
+// Fields:
+//   - expression (string): expression to evaluate
+//   - variables([]map[string]string): additional variables to evaluate;
+//     each entry in the list is a map string->string which has two keys:
+//     -> name, the name of the variable to inject into the CEL context,
+//     -> expression, the expression for this variable to equal.
+//
+// When allowed as sources, the CEL context already includes:
+//   - requests
+//   - responses
+func CELSourceBuilder(ctx context.Context, engine *ProfileEngine, field map[string]interface{}) Source {
+	var options []cel.EnvOption
+	var allowRequests bool
+	var allowResponses bool
+	if _, ok := engine.sourceBuilders["request"]; ok {
+		options = append(options, cel.Variable("requests", types.NewMapType(types.StringType, types.DynType)))
+		allowRequests = true
+	}
+
+	if _, ok := engine.sourceBuilders["response"]; ok {
+		options = append(options, cel.Variable("responses", types.NewMapType(types.StringType, types.DynType)))
+		allowResponses = true
+	}
+
+	return &CELSource{
+		engine: engine,
+		field:  field,
+
+		options:        options,
+		allowRequests:  allowRequests,
+		allowResponses: allowResponses,
+	}
+}
+
+var _ SourceBuilder = CELSourceBuilder
+
+func WithCELSource() func(*ProfileEngine) {
+	return func(p *ProfileEngine) {
+		p.sourceBuilders["cel"] = CELSourceBuilder
+	}
+}
+
+type CELSource struct {
+	engine *ProfileEngine
+	field  map[string]interface{}
+
+	options        []cel.EnvOption
+	allowRequests  bool
+	allowResponses bool
+
+	program celHelper.Program
+}
+
+var _ Source = &CELSource{}
+
+func (s *CELSource) getConfig() *celHelper.EvalConfig {
+	return &celHelper.EvalConfig{
+		WithExtLib:    true,
+		WithJSON:      true,
+		CustomOptions: s.options,
+	}
+}
+
+func (s *CELSource) Validate(_ context.Context) ([]string, []string, error) {
+	rawExpr, present := s.field["expression"]
+	if !present {
+		return nil, nil, errors.New("cel source is missing required field 'expression'")
+	}
+
+	expr, ok := rawExpr.(string)
+	if !ok {
+		return nil, nil, fmt.Errorf("field 'expression' is of wrong type: expected 'string' got '%T'", rawExpr)
+	}
+
+	rawVariables, present := s.field["variables"]
+	if !present {
+		rawVariables = []interface{}{}
+	}
+
+	listVariables, ok := rawVariables.([]interface{})
+	if !ok {
+		return nil, nil, fmt.Errorf("field 'variables' is of wrong outer type: expected '[]interface{}' got '%T'", listVariables)
+	}
+
+	var variables []celHelper.Variable
+	for index, rawVariableMap := range listVariables {
+		variableMap, ok := rawVariableMap.(map[string]interface{})
+		if !ok {
+			return nil, nil, fmt.Errorf("field 'variables[%d]' is of wrong inner type: expected 'map[string]interface{}' got '%T'", index, listVariables)
+		}
+
+		rawName, present := variableMap["name"]
+		if !present {
+			return nil, nil, fmt.Errorf("field 'variables[%d].name' is missing", index)
+		}
+		delete(variableMap, "name")
+
+		name, ok := rawName.(string)
+		if !ok {
+			return nil, nil, fmt.Errorf("field 'variables[%d].name' is of wrong type: expected 'string' got '%T'", index, rawName)
+		}
+
+		rawExpression, present := variableMap["expression"]
+		if !present {
+			return nil, nil, fmt.Errorf("field 'variables[%d].expression' is missing", index)
+		}
+		delete(variableMap, "expression")
+
+		expression, ok := rawExpression.(string)
+		if !ok {
+			return nil, nil, fmt.Errorf("field 'variables[%d].expression' is of wrong type: expected 'string' got '%T'", index, rawExpression)
+		}
+
+		if len(variableMap) > 0 {
+			return nil, nil, fmt.Errorf("field 'variables[%d].name' has extraneous elements besides 'name' and 'expression'", index)
+		}
+
+		variables = append(variables, celHelper.Variable{
+			Name:       name,
+			Expression: expression,
+		})
+	}
+
+	program := celHelper.Program{
+		Variables:  variables,
+		Expression: expr,
+	}
+
+	if err := program.Validate(s.getConfig()); err != nil {
+		return nil, nil, fmt.Errorf("CEL source failed to validate program: %w", err)
+	}
+
+	s.program = program
+
+	return nil, nil, nil
+}
+
+func (s *CELSource) Evaluate(ctx context.Context, eh *EvaluationHistory) (interface{}, error) {
+	data := map[string]interface{}{}
+
+	if HasRequestSource(s.engine) {
+		data["requests"] = eh.Requests
+		if s.engine.outerBlockName == "" {
+			data["requests"] = eh.Requests[""]
+		}
+	}
+
+	if HasResponseSource(s.engine) {
+		data["responses"] = eh.Responses
+		if s.engine.outerBlockName == "" {
+			data["responses"] = eh.Responses[""]
+		}
+	}
+
+	result, err := s.program.Evaluate(ctx, s.getConfig(), data)
+	if err != nil {
+		return nil, fmt.Errorf("CEL source failed to evaluate: %w", err)
+	}
+
+	return result.Value(), nil
+}
+
+func (s *CELSource) Close(_ context.Context) error {
+	return nil
+}

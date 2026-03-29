9244ff1944af14a25e10611f3d6df60840f21451
Aditya <jaiswal.aditya.deelip@gmail.com>
2026-03-29 21:20:52 +0530

    refactor: Update database connection and role edit components to use tracked properties for connection and role IDs
    Signed-off-by: Aditya <jaiswal.aditya.deelip@gmail.com>

--- ui/app/components/pgp-list.js
+++ ui/app/components/pgp-list.js
@@ -3,51 +3,48 @@
  * SPDX-License-Identifier: MPL-2.0
  */
 
-import Component from '@ember/component';
+import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
 
-export default Component.extend({
-  onDataUpdate: () => {},
-  listLength: 0,
-  listData: null,
+export default class PgpListComponent extends Component {
+  @tracked listData = [];
+  onDataUpdate = () => {};
+  listLength = 0;
 
-  init() {
-    this._super(...arguments);
+  constructor() {
+    super(...arguments);
     let num = this.listLength;
     if (num) {
       num = parseInt(num, 10);
     }
-    const list = this.newList(num);
-    this.set('listData', list);
-  },
+    this.listData = this.newList(num);
+  }
 
   didReceiveAttrs() {
-    this._super(...arguments);
     let list;
     if (!this.listLength) {
-      this.set('listData', []);
+      this.listData = [];
       return;
     }
-    // no update needed
     if (this.listData.length === this.listLength) {
       return;
     }
     if (this.listLength < this.listData.length) {
-      // shorten the current list
       list = this.listData.slice(0, this.listLength);
     } else if (this.listLength > this.listData.length) {
-      // add to the current list by creating a new list and copying over existing list
       list = [...this.listData, ...this.newList(this.listLength - this.listData.length)];
     }
-    this.set('listData', list || this.listData);
-    this.onDataUpdate((list || this.listData).compact().map((k) => k.value));
-  },
+    this.listData = list || this.listData;
+    this.onDataUpdate((list || this.listData).filter(Boolean).map((k) => k.value));
+  }
 
   newList(length) {
     return Array(length || 0)
       .fill(null)
       .map(() => ({ value: '' }));
-  },
+  }
 
+// this should be in a .ts file probably
   actions: {
     setKey(index, key) {
       const { listData } = this;

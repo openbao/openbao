f31e9d657ad20e3baeb94e2be8f731dc826d6842
Aditya <jaiswal.aditya.deelip@gmail.com>
2026-03-29 20:54:18 +0530

    Refactor: Replace deprecated maybeQueryRecord with ember-concurrency tasks for loading capabilities
    Signed-off-by: Aditya <jaiswal.aditya.deelip@gmail.com>

--- ui/app/macros/maybe-query-record.js
+++ ui/app/macros/maybe-query-record.js
@@ -4,19 +4,13 @@
  */
 
 import { computed } from '@ember/object';
-import ObjectProxy from '@ember/object/proxy';
-import PromiseProxyMixin from '@ember/object/promise-proxy-mixin';
 import { resolve } from 'rsvp';
 
 export function maybeQueryRecord(modelName, options = {}, ...keys) {
   return computed(...keys, 'store', {
     get() {
       const query = typeof options === 'function' ? options(this) : options;
-      const PromiseObject = ObjectProxy.extend(PromiseProxyMixin);
-
-      return PromiseObject.create({
-        promise: query ? this.store.queryRecord(modelName, query) : resolve({}),
-      });
+      return query ? this.store.queryRecord(modelName, query) : resolve({});
     },
   });
 }

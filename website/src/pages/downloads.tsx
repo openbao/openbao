886a90fb3f0bd32415bbf4de4f8fd3141e4b01c7
Jonas Köhnen <jonas.koehnen@sap.com>
2026-04-08 01:34:46 +0200

    Add minimal, distroless container image variant (#2592)
    * Add distroless container image variant
    
    Signed-off-by: Jonas Köhnen <jonas.koehnen@sap.com>
    
    * Adjust downloads page
    
    Signed-off-by: Jonas Köhnen <jonas.koehnen@sap.com>
    
    * Fix docker tags on download page starting with a "v"
    
    Signed-off-by: Jonas Köhnen <jonas.koehnen@sap.com>
    
    * Try to appease Alex with the changelog before he gets to complaining about it
    
    Signed-off-by: Jonas Köhnen <jonas.koehnen@sap.com>
    
    * Fix comment
    
    Signed-off-by: Jonas Köhnen <jonas.koehnen@sap.com>
    
    * Simplify changelog
    
    Signed-off-by: Jonas Köhnen <jonas.koehnen@sap.com>
    
    ---------
    
    Signed-off-by: Jonas Köhnen <jonas.koehnen@sap.com>

--- website/src/pages/downloads.tsx
+++ website/src/pages/downloads.tsx
@@ -298,11 +298,20 @@ const PackageRepo = ({ type }) => {
 };
 const DockerList = ({ version, registry }) => {
   const dockerVersion = version.slice(1);
+
+  // For example: 2.6.0, 2.5.0-beta20251125.
+  // Consider replacing with a more complete semver parser once required.
+  const [major, minor, _patch] = dockerVersion
+    .match(/^(\d+)\.(\d+)\.(\d+)/).slice(1).map(v => parseInt(v));
+
   const dockerDistros = {
     "Alpine Image Distribution": "openbao/openbao",
     "Alpine Image Distribution with HSM Support": "openbao/openbao-hsm",
     "Red Hat Universal Base Image (UBI) Distribution": "openbao/openbao-ubi",
     "Red Hat Universal Base Image (UBI) Distribution with HSM support": "openbao/openbao-hsm-ubi",
+    ...(major >= 2 && minor >= 6 ? {
+      "Distroless Distribution": "openbao/openbao-distroless",
+    } : {}),
   }
   return (
       <>

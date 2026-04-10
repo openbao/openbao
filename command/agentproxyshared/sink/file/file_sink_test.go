ec3223c158fccb787b1dec3176eea59871cdb674
Jonah Zuercher <jonah.zuercher@secretz.io>
2026-04-10 10:21:34 +0200

    feat(agent): allow changing the uid and gid of a file sink
    Signed-off-by: Jonah Zuercher <jonah.zuercher@secretz.io>

--- command/agentproxyshared/sink/file/file_sink_test.go
+++ command/agentproxyshared/sink/file/file_sink_test.go
@@ -7,6 +7,7 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
+	"syscall"
 	"testing"
 
 	hclog "github.com/hashicorp/go-hclog"
@@ -133,6 +134,74 @@ func TestFileSinkMode(t *testing.T) {
 	}
 	if fi.Mode() != os.FileMode(0o644) {
 		t.Fatalf("wrong file mode was detected at %s", path)
+	}
+
+	fileBytes, err := os.ReadFile(path)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if string(fileBytes) != uuidStr {
+		t.Fatalf("expected %s, got %s", uuidStr, string(fileBytes))
+	}
+}
+
+func testFileSinkChown(t *testing.T, log hclog.Logger) (*sink.SinkConfig, string) {
+	tmpDir, err := os.MkdirTemp("", fmt.Sprintf("%s.", fileServerTestDir))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	path := filepath.Join(tmpDir, "token")
+
+	config := &sink.SinkConfig{
+		Logger: log.Named("sink.file"),
+		Config: map[string]interface{}{
+			"path": path,
+			"uid":  os.Getuid(),
+			"gid":  os.Getgid(),
+		},
+	}
+
+	s, err := NewFileSink(config)
+	if err != nil {
+		t.Fatal(err)
+	}
+	config.Sink = s
+
+	return config, tmpDir
+}
+
+func TestFileSinkChown(t *testing.T) {
+	log := logging.NewVaultLogger(hclog.Trace)
+
+	fs, tmpDir := testFileSinkChown(t, log)
+	defer os.RemoveAll(tmpDir)
+
+	path := filepath.Join(tmpDir, "token")
+
+	uuidStr, _ := uuid.GenerateUUID()
+	if err := fs.WriteToken(uuidStr); err != nil {
+		t.Fatal(err)
+	}
+
+	file, err := os.Open(path)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer file.Close()
+
+	fi, err := file.Stat()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	stat := fi.Sys().(*syscall.Stat_t)
+	if stat.Uid != uint32(os.Getuid()) {
+		t.Fatalf("expected uid %d, got %d", os.Getuid(), stat.Uid)
+	}
+	if stat.Gid != uint32(os.Getgid()) {
+		t.Fatalf("expected gid %d, got %d", os.Getgid(), stat.Gid)
 	}
 
 	fileBytes, err := os.ReadFile(path)

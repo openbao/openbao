78c5d05dda1a3a0dc5f1fa27e065be55e4ecebbb
esticansat <esticansat@machine.local>
2026-04-19 19:23:59 +0200

    test(certutil): Add unit tests for ParseHexFormatted
    Signed-off-by: esticansat <10881431+esticansat@users.noreply.github.com>

--- sdk/helper/certutil/helpers_test.go
+++ sdk/helper/certutil/helpers_test.go
@@ -1,6 +1,10 @@
 package certutil
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
 
 func TestGetHexFormatted(t *testing.T) {
 	t.Parallel()
@@ -46,6 +50,74 @@ func TestGetHexFormatted(t *testing.T) {
 	for _, tc := range testCases {
 		t.Run(tc.desc, func(t *testing.T) {
 			got := GetHexFormatted(tc.input, tc.sep)
+
+			assert.Equal(t, tc.wantOut, got)
+		})
+	}
+}
+
+func TestParseHexFormatted(t *testing.T) {
+	t.Parallel()
+
+	testCases := []struct {
+		desc    string
+		input   string
+		sep     string
+		wantOut []byte
+	}{
+		{
+			desc:    "Empty input",
+			input:   "",
+			sep:     ":",
+			wantOut: nil,
+		},
+		{
+			desc:    "Single hexadecimal byte",
+			input:   "0",
+			sep:     "",
+			wantOut: []byte{0x00},
+		},
+		{
+			desc:    "Maximum hexadecimal value",
+			input:   "f",
+			sep:     "",
+			wantOut: []byte{0xf},
+		},
+		{
+			desc:    "Two bytes without separator",
+			input:   "ff",
+			sep:     "",
+			wantOut: []byte{0xf, 0xf},
+		},
+		{
+			desc:    "Two bytes with separator",
+			input:   "0:1",
+			sep:     ":",
+			wantOut: []byte{0x00, 0x01},
+		},
+		{
+			desc:    "Case sensitive",
+			input:   "0:1:F",
+			sep:     ":",
+			wantOut: []byte{0x00, 0x01, 0x0f},
+		},
+		{
+			desc:    "Invalid hexadecimal",
+			input:   "0:z",
+			sep:     ":",
+			wantOut: nil,
+		},
+		{
+			desc:    "Empty segments",
+			input:   "0::1",
+			sep:     ":",
+			wantOut: nil,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.desc, func(t *testing.T) {
+			got := ParseHexFormatted(tc.input, tc.sep)
 
 			assert.Equal(t, tc.wantOut, got)
 		})

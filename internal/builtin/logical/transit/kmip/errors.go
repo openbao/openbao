53cc4eae6e77b87ad60f4ff2992ebf9f50d6aa5b
driif <andrii.fedorchuk@secretz.io>
2026-04-22 17:18:38 +0200

    add errors mapper and helpers for kmip
    Signed-off-by: driif <andrii.fedorchuk@secretz.io>

---
+++ internal/builtin/logical/transit/kmip/errors.go
@@ -0,0 +1,28 @@
+package kmip
+
+import (
+	"errors"
+
+	kmiplib "github.com/ovh/kmip-go"
+
+	"github.com/openbao/openbao/sdk/v2/logical"
+	"github.com/ovh/kmip-go/kmipserver"
+)
+
+func mapError(err error) error {
+	if err == nil {
+		return err
+	}
+
+	var kmipErr *kmipserver.Error
+	if errors.As(err, &kmipErr) {
+		return err
+	}
+
+	switch {
+	case errors.Is(err, logical.ErrPermissionDenied):
+		return kmipserver.Errorf(kmiplib.ResultReasonPermissionDenied, "%s", err.Error())
+	default:
+		return kmipserver.Errorf(kmiplib.ResultReasonGeneralFailure, "%s", err.Error())
+	}
+}

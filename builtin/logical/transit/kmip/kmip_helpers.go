1a7aaa0a024fa685b708ec8f857bef3c2faeb8c7
driif <andrii.fedorchuk@secretz.io>
2026-04-23 16:15:13 +0200

    add handlers for register and all crypto handlers. add helper for extract name from template attributes
    Signed-off-by: driif <andrii.fedorchuk@secretz.io>

--- builtin/logical/transit/kmip/kmip_helpers.go
+++ builtin/logical/transit/kmip/kmip_helpers.go
@@ -5,9 +5,9 @@ import (
 	kmiplib "github.com/ovh/kmip-go"
 )
 
-func AlgAndBitLenFromTemplateAttribute(ta kmiplib.TemplateAttribute) (kmiplib.CryptographicAlgorithm, int) {
+func AlgAndBitLenFromTemplateAttribute(ta kmiplib.TemplateAttribute) (kmiplib.CryptographicAlgorithm, int32) {
 	var alg kmiplib.CryptographicAlgorithm
-	var bitlen int
+	var bitlen int32
 
 	for _, attr := range ta.Attribute {
 		switch attr.AttributeName {
@@ -16,10 +16,21 @@ func AlgAndBitLenFromTemplateAttribute(ta kmiplib.TemplateAttribute) (kmiplib.Cr
 				alg = v
 			}
 		case kmip.AttributeNameCryptographicLength:
-			if v, ok := attr.AttributeValue.(int); ok {
+			if v, ok := attr.AttributeValue.(int32); ok {
 				bitlen = v
 			}
 		}
 	}
 	return alg, bitlen
+}
+
+func NameFromTemplateAttribute(ta kmiplib.TemplateAttribute) string {
+	for _, attr := range ta.Attribute {
+		if attr.AttributeName == kmiplib.AttributeNameName {
+			if v, ok := attr.AttributeValue.(kmiplib.Name); ok {
+				return v.NameValue
+			}
+		}
+	}
+	return ""
 }

2a8beadbea07cca3ccb3e05610ef3f83cb283dc6
Thayne McCombs <astrothayne@gmail.com>
2026-04-28 01:51:29 -0600

    Upgrade ember-mirage-cli
    This fixes some instances of ember-data:deprecate-early-static
    
    Signed-off-by: Thayne McCombs <astrothayne@gmail.com>

--- ui/mirage/factories/kubernetes-role.js
+++ ui/mirage/factories/kubernetes-role.js
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: MPL-2.0
  */
 
-import { Factory, trait } from 'ember-cli-mirage';
+import { Factory, trait } from 'miragejs';
 
 const generated_role_rules = `rules:
 - apiGroups: [""]

9cc5f12d585bd487b9157c029e27f2ba99f59e93
driif <andrii.fedorchuk@secretz.io>
2026-05-19 16:44:42 +0200

    add key mapping for kmip algs to transit
    Signed-off-by: driif <andrii.fedorchuk@secretz.io>

--- internal/builtin/logical/transit/kmip_adapter.go
+++ internal/builtin/logical/transit/kmip_adapter.go
@@ -2,9 +2,11 @@ package transit
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/hashicorp/go-hclog"
 	"github.com/openbao/openbao/builtin/logical/transit/kmip"
+	"github.com/openbao/openbao/sdk/v2/helper/keysutil"
 	"github.com/openbao/openbao/sdk/v2/logical"
 
 	kmiplib "github.com/ovh/kmip-go"
@@ -20,6 +22,7 @@ var (
 	_ kmip.CryptoAdapter = (*transitAdapter)(nil)
 )
 
+// POC: allow-all.
 func (a *transitAdapter) AuthenticateCert(ctx context.Context, subjectDN string) (allowedOps []string, err error) {
 	return nil, nil
 }
@@ -75,4 +78,60 @@ func (a *transitAdapter) Sign(ctx context.Context, id string, data []byte) ([]by
 }
 func (a *transitAdapter) Verify(ctx context.Context, id string, data, signature []byte) (bool, error) {
 	return false, nil
+}
+
+// === MAPPING ===
+
+// keyTypeFor maps a KMIP CryptographicAlgorithm + bit length to a transit KeyType.
+// The bool return indicates whether the key is asymmetric.
+func keyTypeFor(alg kmiplib.CryptographicAlgorithm, bitlen int32) (keysutil.KeyType, bool, error) {
+	switch alg {
+	case kmiplib.CryptographicAlgorithmAES:
+		switch bitlen {
+		case 128:
+			return keysutil.KeyType_AES128_GCM96, false, nil
+		case 256:
+			return keysutil.KeyType_AES256_GCM96, false, nil
+		}
+
+	case kmiplib.CryptographicAlgorithmChaCha20,
+		kmiplib.CryptographicAlgorithmChaCha20Poly1305:
+		if bitlen == 256 {
+			return keysutil.KeyType_ChaCha20_Poly1305, false, nil
+		}
+
+	case kmiplib.CryptographicAlgorithmRSA:
+		switch bitlen {
+		case 2048:
+			return keysutil.KeyType_RSA2048, true, nil
+		case 3072:
+			return keysutil.KeyType_RSA3072, true, nil
+		case 4096:
+			return keysutil.KeyType_RSA4096, true, nil
+		}
+
+	// KMIP uses ECDSA or generic EC; the curve is conveyed by the key size.
+	case kmiplib.CryptographicAlgorithmECDSA,
+		kmiplib.CryptographicAlgorithmEC:
+		switch bitlen {
+		case 256:
+			return keysutil.KeyType_ECDSA_P256, true, nil
+		case 384:
+			return keysutil.KeyType_ECDSA_P384, true, nil
+		case 521:
+			return keysutil.KeyType_ECDSA_P521, true, nil
+		}
+
+	case kmiplib.CryptographicAlgorithmHMACSHA1,
+		kmiplib.CryptographicAlgorithmHMACSHA224,
+		kmiplib.CryptographicAlgorithmHMACSHA256,
+		kmiplib.CryptographicAlgorithmHMACSHA384,
+		kmiplib.CryptographicAlgorithmHMACSHA512:
+		// transit HMAC accepts 256..4096 bits
+		// see keysutil.HmacMinKeySize and keysutil.HmacMaxKeySize
+		if bitlen >= 256 && bitlen <= 4096 && bitlen%8 == 0 {
+			return keysutil.KeyType_HMAC, false, nil
+		}
+	}
+	return 0, false, fmt.Errorf("unsupported alg=%v bitlen=%d", alg, bitlen)
 }

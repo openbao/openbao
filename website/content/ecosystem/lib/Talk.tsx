a51d67719efd4f950140b40d0ecfbc45e3c6eb45
Alexander Scheel <alex.scheel@control-plane.io>
2026-06-24 16:31:28 +0100

    Fix spacing, revisions after updates
    Signed-off-by: Alexander Scheel <alex.scheel@control-plane.io>

--- website/content/ecosystem/lib/Talk.tsx
+++ website/content/ecosystem/lib/Talk.tsx
@@ -5,28 +5,17 @@ const favicon = "/img/favicon.svg"
 
 import { getLogo } from './Member.tsx';
 
-function getOptionalLogo(member) {
-    if (member === undefined) {
-        return favicon;
-    }
-
-    const logo = getLogo(member);
-    if (logo === undefined) {
-        return favicon;
-    }
-
-    return logo;
-}
-
 export function Talk({ title, memberName, children }) {
-    const logo = getLogo(memberName) ?? favicon;
+    const logo = memberName
+        ? ( getLogo(memberName) ?? favicon)
+        : favicon;
 
     return (
         <div className="col col--6 padding-bottom--lg">
             <div className="card card--full-height">
                 <div className={clsx("card__header", styles.cardHeader)}>
                     <h2>{title}</h2>
-                    <img className={styles.logo} src={logo} alt={`${memberName} logo`} />
+                    <img className={styles.logo} src={logo} alt={`${memberName ?? "OpenBao"} logo`} />
                 </div>
                 <div className="card__body">
                     {children}
@@ -38,13 +27,13 @@ export function Talk({ title, memberName, children }) {
 
 export function YouTubeTalk({ title, memberName, vid, lowres, children }) {
     const preview = lowres
-        ? `https://i3.ytimg.com/vi/${vid}/mqdefault.jpg`;
+        ? `https://i3.ytimg.com/vi/${vid}/mqdefault.jpg`
         : `https://img.youtube.com/vi/${vid}/maxresdefault.jpg`;
 
     return (
-        <Talk title={ title } memberName={ memberName }>
-            <a href={`https://youtube.com/watch?v=${ vid }`}>
-                <img src={ preview } alt="Video Preview" style={{
+        <Talk title={title} memberName={memberName}>
+            <a href={`https://youtube.com/watch?v=${vid}`}>
+                <img src={preview} alt="Video Preview" style={{
                     width: "80%",
                     maxHeight: "200px",
                     margin: "0 auto",
@@ -55,7 +44,7 @@ export function YouTubeTalk({ title, memberName, vid, lowres, children }) {
 
             {children}
 
-            <a href={`https://youtube.com/watch?v=${ vid }`}>
+            <a href={`https://youtube.com/watch?v=${vid}`}>
                 <i>Watch on YouTube.</i>
             </a>
         </Talk>

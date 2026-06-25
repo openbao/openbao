31044ea8edbc339a669c510f399f1a592d2586eb
Wojciech Slabosz <wojciech.slabosz@sap.com>
2026-06-25 10:49:53 +0200

    Add node existence check before attempting promotion or demotion in raft cluster (#3350)
    * check for existence of node before attempting promotion or demotion
    
    Signed-off-by: Wojciech Slabosz <wojciech.slabosz@sap.com>
    
    * changelog added
    
    Signed-off-by: Wojciech Slabosz <wojciech.slabosz@sap.com>
    
    ---------
    
    Signed-off-by: Wojciech Slabosz <wojciech.slabosz@sap.com>

--- physical/raft/raft.go
+++ physical/raft/raft.go
@@ -13,6 +13,7 @@ import (
 	"math/rand"
 	"os"
 	"path/filepath"
+	"slices"
 	"strconv"
 	"strings"
 	"sync"
@@ -1244,28 +1245,25 @@ func (b *RaftBackend) PromotePeer(ctx context.Context, peerID string) error {
 		return err
 	}
 
+	peers, err := b.Peers(ctx)
+	if err != nil {
+		return fmt.Errorf("failed to get Raft peers: %s", err)
+	}
+
+	var addr string
+	found := slices.ContainsFunc(peers, func(p Peer) bool {
+		if p.ID == peerID {
+			addr = p.Address
+			return true
+		}
+		return false
+	})
+
+	if !found {
+		return fmt.Errorf("server %s not found in raft configuration", peerID)
+	}
+
 	if b.disableAutopilot {
-		if b.raft == nil {
-			return errors.New("raft storage is not initialized")
-		}
-		peers, err := b.Peers(ctx)
-		if err != nil {
-			return fmt.Errorf("failed to get Raft peers: %s", err)
-		}
-
-		found := false
-		addr := ""
-		for _, peer := range peers {
-			if peer.ID == peerID {
-				addr = peer.Address
-				found = true
-				break
-			}
-		}
-		if !found {
-			return fmt.Errorf("server %s not found in raft configuration", peerID)
-		}
-
 		future := b.raft.AddVoter(raft.ServerID(peerID), raft.ServerAddress(addr), 0, 0)
 		if err := future.Error(); err != nil {
 			return fmt.Errorf("failed to promote non-voter to voter: %s", err)
@@ -1294,34 +1292,22 @@ func (b *RaftBackend) DemotePeer(ctx context.Context, peerID string) error {
 		return err
 	}
 
-	if b.disableAutopilot {
-		if b.raft == nil {
-			return errors.New("raft storage is not initialized")
-		}
+	peers, err := b.Peers(ctx)
+	if err != nil {
+		return fmt.Errorf("failed to get Raft peers: %s", err)
+	}
 
+	if !slices.ContainsFunc(peers, func(p Peer) bool { return p.ID == peerID }) {
+		return fmt.Errorf("server %s not found in raft configuration", peerID)
+	}
+
+	if b.disableAutopilot {
 		// refuse to demote current leader to not trigger a leader election
 		// when the leader is demoted. This is not necessary if autopilot is enabled,
 		// as it will handle this case for us and only demote the leader after a
 		// leader election
 		if strings.EqualFold(peerID, b.localID) {
 			return errors.New("refusing to demote current leader")
-		}
-
-		peers, err := b.Peers(ctx)
-		if err != nil {
-			return fmt.Errorf("failed to get Raft peers: %s", err)
-		}
-
-		found := false
-		for _, peer := range peers {
-			if peer.ID == peerID {
-				found = true
-				break
-			}
-		}
-
-		if !found {
-			return fmt.Errorf("server %s not found in raft configuration", peerID)
 		}
 
 		future := b.raft.DemoteVoter(raft.ServerID(peerID), 0, 0)

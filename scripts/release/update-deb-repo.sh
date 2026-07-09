8a2202366f77bb5d2733edd7ceb1093b7f511753
Jonas Köhnen <jonas.koehnen@sap.com>
2026-07-09 02:20:49 +0200

    Publish betas into testing RPM/DEB repos  (#3385)
    * Publish betas into testing RPM/DEB repos
    
    Signed-off-by: Jonas Köhnen <jonas.koehnen@sap.com>
    
    * Update downloads page
    
    Signed-off-by: Jonas Köhnen <jonas.koehnen@sap.com>
    
    ---------
    
    Signed-off-by: Jonas Köhnen <jonas.koehnen@sap.com>

--- scripts/release/update-deb-repo.sh
+++ scripts/release/update-deb-repo.sh
@@ -5,6 +5,11 @@
 
 set -euo pipefail
 
+case "$VERSION" in
+  *-*) SUITE=testing ;;
+  *)   SUITE=stable  ;;
+esac
+
 mkdir -p build/deb/conf
 
 cat > build/deb/conf/distributions <<-EOF
@@ -14,9 +19,19 @@ cat > build/deb/conf/distributions <<-EOF
 	Codename: stable
 	Architectures: amd64 arm64 armhf ppc64el riscv64 s390x
 	Components: main
-	Description: Official APT repository for OpenBao
+	Description: Official APT repository for OpenBao releases
+	SignWith: ${GPG_FINGERPRINT}
+	Limit: 0
+
+	Origin: OpenBao - Official
+	Label: OpenBao
+	Suite: testing
+	Codename: testing
+	Architectures: amd64 arm64 armhf ppc64el riscv64 s390x
+	Components: main
+	Description: Official APT repository for OpenBao pre-releases
 	SignWith: ${GPG_FINGERPRINT}
 	Limit: 0
 EOF
 
-reprepro -b build/deb includedeb stable dist/*.deb
+reprepro -b build/deb includedeb "$SUITE" dist/*.deb

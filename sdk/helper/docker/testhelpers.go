ea202d810f99a9873867e25a266a3f8b1912edf9
Alexander Scheel <alex.scheel@control-plane.io>
2026-07-14 16:35:50 -0500

    Set user=root when invoking OpenBao for Alpine (#3499)
    Signed-off-by: Alexander Scheel <alex.scheel@control-plane.io>

--- sdk/helper/docker/testhelpers.go
+++ sdk/helper/docker/testhelpers.go
@@ -40,6 +40,7 @@ type RunOptions struct {
 	ImageRepo              string
 	ImageTag               string
 	ContainerName          string
+	User                   string
 	Cmd                    []string
 	Entrypoint             []string
 	Env                    []string
@@ -370,6 +371,7 @@ func (d *Runner) Start(ctx context.Context, addSuffix, forceLocalAddr bool) (*St
 	cfg := &container.Config{
 		Hostname: name,
 		Image:    fmt.Sprintf("%s:%s", d.RunOptions.ImageRepo, d.RunOptions.ImageTag),
+		User:     d.RunOptions.User,
 		Env:      d.RunOptions.Env,
 		Cmd:      d.RunOptions.Cmd,
 	}

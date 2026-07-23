c03e44ff984493ac6c824039f697472a5654288e
Jonas Köhnen <jonas.koehnen@sap.com>
2026-07-23 11:21:13 +0200

    Port from Go to TypeScript :P
    Signed-off-by: Jonas Köhnen <jonas.koehnen@sap.com>

--- website/content/ecosystem/lib/RandomizeMembers.tsx
+++ website/content/ecosystem/lib/RandomizeMembers.tsx
@@ -1,64 +1,38 @@
-import Randomize from "./Randomize.tsx";
+import Randomize from "./Randomize";
+import { getLogo } from "./Member";
 import clsx from "clsx";
 import styles from "./styles.module.css";
-import { useColorMode } from '@docusaurus/theme-common';
 
-const logos = require.context(
-    "openbao-ecosystem-logos", true, /\.(svg|png)$/
-);
-
-
-function transformLogos() {
-  console.log(logos);
-
-  const { colorMode } = useColorMode();
-
-  var vendors = {};
-  for (const logo_idx in logos.keys()) {
-    const logo = logos.keys()[logo_idx];
-
-    const dark_logo = logo.includes("/dark.png") || logo.includes("/dark.svg");
-    const light_logo = logo.includes("/light.png") || logo.includes("/light.svg");
-    const dark_mode = colorMode == "dark";
-
-    const vendor = logo.substring(2).split(".")[0].split("/")[0];
-    console.log("logo", logo, "vendor", vendor);
-
-    const use_dark = dark_logo && dark_mode;
-    const use_light = light_logo && !dark_mode;
-    const new_vendor = !(vendor in vendors);
-
-    if (use_dark || use_light || new_vendor) {
-      vendors[vendor] = logo;
-    }
-  }
-
-  var rendered = [];
-
-  for (const vendor_name in vendors) {
-    const logo = vendors[vendor_name];
-    const ref = logos(logo).default ?? logos(logo);
-
-    rendered.push(
-      <div className="col col--2 padding-bottom--lg">
-        <div className="card card--full-height">
-          <div className={clsx("card__header", styles.centered )}>
-            <img id={vendor_name} className={styles.logoOnly} src={ref} alt={`${vendor_name} logo`} />
-          </div>
-        </div>
-      </div>
-    );
-  }
-
-  return rendered;
-}
+const logos = require.context("openbao-ecosystem-logos", true, /\.(svg|png)$/);
 
 export default function RandomizeMembers() {
-  const rendered = transformLogos();
+  // Collect the unique set of logos, deduplicating dark/light mode ones.
+  const names = [
+    ...new Set(
+      logos.keys().map((path) => path.substring(2).split(".")[0].split("/")[0]),
+    ).values(),
+  ];
+
   return (
     <div className="row">
       <Randomize>
-        { ...rendered }
+        {names.map((name) => {
+          const logo = getLogo(name, name);
+          return (
+            <div className="col col--2 padding-bottom--lg">
+              <div className="card card--full-height">
+                <div className={clsx("card__header", styles.centered)}>
+                  <img
+                    id={name}
+                    className={styles.logoOnly}
+                    src={logo}
+                    alt={`${name} logo`}
+                  />
+                </div>
+              </div>
+            </div>
+          );
+        })}
       </Randomize>
     </div>
   );

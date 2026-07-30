0053962cc404eb91b40c3c46e2df6ec5f74d6b5a
Aron Rohr <github@luiggi33.de>
2026-07-30 18:58:46 +0200

    revert error message masking
    there seem to be two types of messages, the main error uses vault still so this should be kept
    
    Signed-off-by: Aron Rohr <github@luiggi33.de>

--- internal/command/format.go
+++ internal/command/format.go
@@ -633,10 +633,10 @@ func (t TableFormatter) OutputMap(ui cli.Ui, data map[string]any) error {
 func OutputSealStatus(ui cli.Ui, client *api.Client, status *api.SealStatusResponse) int {
 	sealStatusOutput := SealStatusOutput{SealStatusResponse: *status}
 
-	// Mask the 'OpenBao is sealed' error, since this means HA is enabled, but that
+	// Mask the 'Vault is sealed' error, since this means HA is enabled, but that
 	// we cannot query for the leader since we are sealed.
 	leaderStatus, err := client.Sys().Leader()
-	if err != nil && strings.Contains(err.Error(), "OpenBao is sealed") {
+	if err != nil && strings.Contains(err.Error(), "Vault is sealed") {
 		leaderStatus = &api.LeaderResponse{HAEnabled: true}
 		err = nil
 	}

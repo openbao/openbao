ab50dc8a176366f75d707374b9359dc840697ff8
Cloonson <74468324+Cloonson@users.noreply.github.com>
2026-08-03 16:52:02 +0200

    Refactor warning handling for secret data
    Signed-off-by: Cloonson <74468324+Cloonson@users.noreply.github.com>

--- internal/command/util.go
+++ internal/command/util.go
@@ -105,13 +105,8 @@ func PrintRawField(ui cli.Ui, data any, field string) int {
 		ui.Error(fmt.Sprintf("Field %q not present in secret", field))
 		return 1
 	}
-	// Really similar to the warning used in openbao/internal/command/format.go. The PrintRawField function was simply missing the warnings check.
-	if secret, ok := data.(*api.Secret); ok && secret != nil && len(secret.Warnings) > 0 {
-		ui.Warn("WARNING! The following warnings were returned from OpenBao\n")
-		for _, warning := range secret.Warnings {
-			ui.Warn(wrapAtLengthWithPadding(fmt.Sprintf("* %s", warning), 2))
-			ui.Warn("")
-		}
+	if secret, ok := data.(*api.Secret); ok {
+		printWarnings(ui, secret)
 	}
 
 	format := Format(ui)

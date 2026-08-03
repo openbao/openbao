f874e3ffc78997ae6f7751394bb708fc00d76fbc
Cloonson <74468324+Cloonson@users.noreply.github.com>
2026-08-03 16:56:33 +0200

    Simplify warning output logic in kv_get.go
    Refactor warning printing to use a single function call. 
    
    `if len(secret.Warnings) > 0` dropped too because printWarnings already checks that
    
    Signed-off-by: Cloonson <74468324+Cloonson@users.noreply.github.com>

--- internal/command/kv_get.go
+++ internal/command/kv_get.go
@@ -209,12 +209,9 @@ func (c *KVGetCommand) Run(args []string) int {
 	if secret.WrapInfo != nil || c.flagFormat != "table" {
 		return OutputSecret(c.UI, secret)
 	}
-
-	if len(secret.Warnings) > 0 {
-		tf := TableFormatter{}
-		tf.printWarnings(c.UI, secret)
-	}
-
+	
+	printWarnings(c.UI, secret)
+	
 	if v2 {
 		outputPath(c.UI, fullPath, "Secret Path")
 	}

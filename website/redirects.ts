43d9d98124dd84b454f74bf61997aaa81656933a
Thore Goebel <thore.goebel@securosys.ch>
2026-08-06 14:46:40 +0200

    docs: Remove vendor-specific PKCS#11 guides
    The information content of these guides are either:
    
    - Vendor-specific how-tos => these should be maintained by the vendors
    - OpenBao how-tos => covered by the generic PKCS#11 guide, no need to duplicate it
    
    Signed-off-by: Thore Goebel <thore.goebel@securosys.ch>

--- website/redirects.ts
+++ website/redirects.ts
@@ -4,4 +4,24 @@ export const redirects = [
         from: "/api-docs/system/rotate-config",
         to: "/api-docs/system/rotate/keyring-config",
     },
+    {
+        from: "/docs/guides/unseal/pkcs11/generic",
+        to: "/docs/guides/unseal_pkcs11",
+    },
+    {
+        from: "/docs/guides/unseal/pkcs11/securosys",
+        to: "/docs/guides/unseal_pkcs11",
+    },
+    {
+        from: "/docs/guides/unseal/pkcs11/utimaco",
+        to: "/docs/guides/unseal_pkcs11",
+    },
+    {
+        from: "/docs/guides/unseal/pkcs11/duokey",
+        to: "/docs/guides/unseal_pkcs11",
+    },
+    {
+        from: "/docs/guides/unseal/pkcs11/nitrokey",
+        to: "/docs/guides/unseal_pkcs11",
+    },
 ];

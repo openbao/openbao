300675944df87608c3ea12304706769e22ad2498
Thore Goebel <thore.goebel@securosys.ch>
2026-08-06 14:34:48 +0200

    docs: Add generic PKCS#11 auto-unseal guide
    This guide complements the existing docs that already exist at:
    plugin system, pkcs stanza, vendor-specific guides.
    
    This guide is intended to be more descriptive and step-by-step than the
    other "reference-style" docs.
    
    Signed-off-by: Thore Goebel <thore.goebel@securosys.ch>

--- website/sidebars.ts
+++ website/sidebars.ts
@@ -99,6 +99,7 @@ const sidebars: SidebarsConfig = {
                     Unsealing: [
                         {
                             "PKCS#11": [
+                                "guides/unseal/pkcs11/generic",
                                 "guides/unseal/pkcs11/securosys",
                                 "guides/unseal/pkcs11/utimaco",
                                 "guides/unseal/pkcs11/duokey",

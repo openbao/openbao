947d193eb53c120a5a0b5a16e3470e87495a1e46
Enrico Fusi <enrico.fusi@sap.com>
2026-08-07 20:30:15 +0200

    Add RFC on Per-Namespace Auto Seals
    Signed-off-by: Enrico Fusi <enrico.fusi@sap.com>

--- website/sidebarsCommunity.ts
+++ website/sidebarsCommunity.ts
@@ -80,6 +80,7 @@ const sidebars: SidebarsConfig = {
                 "rfcs/authenticated-rekey",
                 "rfcs/self-init",
                 "rfcs/namespace-sealing",
+                "rfcs/namespace-auto-seals",
                 "rfcs/external-keys",
                 "rfcs/config-audit-devices",
                 "rfcs/opentelemetry",

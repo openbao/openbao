899e716a0186408b8167782c95ead7fe0d03f567
Alexander Scheel <alex.scheel@control-plane.io>
2026-08-14 13:22:05 -0500

    Fix missing OIDC default keys for namespaces (#3662) (#3802)
    Signed-off-by: Wojciech Slabosz <wojciech.slabosz@sap.com>
    Signed-off-by: Jonas Köhnen <jonas.koehnen@sap.com>
    Co-authored-by: Wojciech Slabosz <wojciech.slabosz@sap.com>
    Co-authored-by: Jonas Köhnen <jonas.koehnen@sap.com>

--- vault/auth.go
+++ vault/auth.go
@@ -198,9 +198,10 @@ func (c *Core) enableCredentialInternalWithLock(ctx context.Context, entry *rout
 	// restore the original readOnlyErr, so we can write to the view in
 	// Initialize() if necessary
 	view.SetReadOnlyErr(origViewReadOnlyErr)
-	// initialize, using the core's active context.
-	err = backend.Initialize(c.activeContext.Load(), &logical.InitializationRequest{Storage: view})
-	if err != nil {
+
+	// Initialize, using the core's active context.
+	activeCtx := namespace.ContextWithNamespace(c.activeContext.Load(), ns)
+	if err := backend.Initialize(activeCtx, &logical.InitializationRequest{Storage: view}); err != nil {
 		return err
 	}
 
@@ -1200,8 +1201,7 @@ func (c *Core) setupCredential(ctx context.Context, entry *routing.MountEntry) (
 			view.SetReadOnlyErr(origViewReadOnlyErr)
 		}
 
-		err := backend.Initialize(ctx, &logical.InitializationRequest{Storage: view})
-		if err != nil {
+		if err := backend.Initialize(namespace.ContextWithNamespace(ctx, localEntry.Namespace), &logical.InitializationRequest{Storage: view}); err != nil {
 			postUnsealLogger.Error("failed to initialize auth backend", "error", err)
 		}
 	}

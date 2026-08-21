2facb4f90c2f47ea6e45d5ad8f5e4b50bc962305
Aron Rohr <github@luiggi33.de>
2026-08-22 00:25:01 +0200

    update workflow group help text
    Signed-off-by: Aron Rohr <github@luiggi33.de>

--- internal/command/workflow.go
+++ internal/command/workflow.go
@@ -24,11 +24,27 @@ func (c *WorkflowCommand) Help() string {
 Usage: bao workflow <subcommand> [options] [args]
 
   This command groups subcommands are for interacting with workflows.
-  Users can list, read, edit, delete and call workflows.
+  Users can list, read, write, delete and call workflows.
 
   List all workflows:
 
       $ bao workflow list
+
+  Read the workflow named "my-workflow":
+
+      $ bao workflow read my-workflow
+
+  Create or update a workflow named "my-workflow" from local file:
+
+      $ bao workflow write my-workflow ./my-workflow.hcl
+
+  Delete the workflow named "my-workflow":
+
+      $ bao workflow delete my-workflow
+
+  Call the workflow named "my-workflow":
+
+      $ bao workflow call my-workflow
 
   Please see the individual subcommand help for detailed usage information.
 `

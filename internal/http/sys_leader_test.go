openbao/openbao

commit 37399d45b7e7ae755c54201902fb2ebcb37bbab2

cleanup of health, leader and ha-status responses (#2383)
- removed PerfStandby, PerfStandbyLastRemoteWAL and LastWAL from SealStatusOutput
- adjusted /leader and/seal-status openapi schema in vault/logical_system_paths.go
- ActiveTime, IsSelf, LeaderAddress and LeaderClusterAddress properties will only appear when HA is enabled
- adjusted related `/leader`, `/health`, `ha-status` documentation
Signed-off-by: Wojciech Slabosz <wojciech.slabosz@sap.com>

http/sys_leader_test.go
@@ -7,7 +7,6 @@ import (
 	"net/http"
 	"reflect"
 	"testing"
-	"time"
 
 	"github.com/openbao/openbao/vault"
 )
@@ -24,11 +23,7 @@ func TestSysLeader_get(t *testing.T) {
 
 	var actual map[string]interface{}
 	expected := map[string]interface{}{
-		"ha_enabled":             false,
-		"is_self":                false,
-		"leader_address":         "",
-		"leader_cluster_address": "",
-		"active_time":            time.Time{}.UTC().Format(time.RFC3339),
+		"ha_enabled": false,
 	}
 	testResponseStatus(t, resp, 200)
 	testResponseBody(t, resp, &actual)

vault/logical_system.go
@@ -4304,22 +4304,21 @@ func (b *SystemBackend) pathInternalOpenAPI(ctx context.Context, req *logical.Re
 }
 
 type SealStatusResponse struct {
-	Type             string   `json:"type"`
-	Initialized      bool     `json:"initialized"`
-	Sealed           bool     `json:"sealed"`
-	T                int      `json:"t"`
-	N                int      `json:"n"`
-	Progress         int      `json:"progress"`
-	Nonce            string   `json:"nonce"`
-	Version          string   `json:"version"`
-	BuildDate        string   `json:"build_date"`
-	Migration        bool     `json:"migration"`
-	ClusterName      string   `json:"cluster_name,omitempty"`
-	ClusterID        string   `json:"cluster_id,omitempty"`
-	RecoverySeal     bool     `json:"recovery_seal"`
-	RecoverySealType string   `json:"recovery_seal_type,omitempty"`
-	StorageType      string   `json:"storage_type,omitempty"`
-	Warnings         []string `json:"warnings,omitempty"`
+	Type             string `json:"type"`
+	Initialized      bool   `json:"initialized"`
+	Sealed           bool   `json:"sealed"`
+	T                int    `json:"t"`
+	N                int    `json:"n"`
+	Progress         int    `json:"progress"`
+	Nonce            string `json:"nonce"`
+	Version          string `json:"version"`
+	BuildDate        string `json:"build_date"`
+	Migration        bool   `json:"migration"`
+	ClusterName      string `json:"cluster_name,omitempty"`
+	ClusterID        string `json:"cluster_id,omitempty"`
+	RecoverySeal     bool   `json:"recovery_seal"`
+	RecoverySealType string `json:"recovery_seal_type,omitempty"`
+	StorageType      string `json:"storage_type,omitempty"`
 }
 
 func (core *Core) GetSealStatus(ctx context.Context, lock bool) (*SealStatusResponse, error) {
@@ -4342,64 +4341,49 @@ func (core *Core) GetSealStatus(ctx context.Context, lock bool) (*SealStatusResp
 		return nil, err
 	}
 
-	if sealConfig == nil {
-		s := &SealStatusResponse{
-			Type:             core.SealAccess().BarrierType().String(),
-			Initialized:      initialized,
-			Sealed:           true,
-			RecoverySeal:     core.SealAccess().RecoveryKeySupported(),
-			RecoverySealType: recoveryType,
-			StorageType:      core.StorageType(),
-			Version:          version.GetVersion().VersionNumber(),
-			BuildDate:        version.BuildDate,
-		}
+	ssr := &SealStatusResponse{
+		Type:             core.SealAccess().BarrierType().String(),
+		Initialized:      initialized,
+		Sealed:           sealed,
+		RecoverySeal:     core.SealAccess().RecoveryKeySupported(),
+		RecoverySealType: recoveryType,
+		StorageType:      core.StorageType(),
+		Version:          version.GetVersion().VersionNumber(),
+		BuildDate:        version.BuildDate,
+	}
 
-		return s, nil
+	if sealConfig == nil {
+		return ssr, nil
 	}
 
 	// Fetch the local cluster name and identifier
-	var clusterName, clusterID string
 	if !sealed {
 		cluster, err := core.Cluster(ctx)
 		if err != nil {
 			return nil, err
 		}
-		if cluster == nil {
-			return nil, errors.New("failed to fetch cluster details")
-		}
-		clusterName = cluster.Name
-		clusterID = cluster.ID
+
+		ssr.ClusterName = cluster.Name
+		ssr.ClusterID = cluster.ID
 	}
 
 	progress, nonce := core.SecretProgress(lock)
 
-	s := &SealStatusResponse{
-		Type:             core.SealAccess().BarrierType().String(),
-		Initialized:      initialized,
-		Sealed:           sealed,
-		T:                sealConfig.SecretThreshold,
-		N:                sealConfig.SecretShares,
-		Progress:         progress,
-		Nonce:            nonce,
-		Version:          version.GetVersion().VersionNumber(),
-		BuildDate:        version.BuildDate,
-		Migration:        core.IsInSealMigrationMode(lock) && !core.IsSealMigrated(lock),
-		ClusterName:      clusterName,
-		ClusterID:        clusterID,
-		RecoverySeal:     core.SealAccess().RecoveryKeySupported(),
-		RecoverySealType: recoveryType,
-		StorageType:      core.StorageType(),
-	}
+	ssr.T = sealConfig.SecretThreshold
+	ssr.N = sealConfig.SecretShares
+	ssr.Progress = progress
+	ssr.Nonce = nonce
+	ssr.Migration = core.IsInSealMigrationMode(lock) && !core.IsSealMigrated(lock)
 
-	return s, nil
+	return ssr, nil
 }
 
 type LeaderResponse struct {
 	HAEnabled            bool      `json:"ha_enabled"`
-	IsSelf               bool      `json:"is_self"`
-	ActiveTime           time.Time `json:"active_time,omitempty"`
-	LeaderAddress        string    `json:"leader_address"`
-	LeaderClusterAddress string    `json:"leader_cluster_address"`
+	IsSelf               bool      `json:"is_self,omitempty"`
+	ActiveTime           time.Time `json:"active_time,omitzero"`
+	LeaderAddress        string    `json:"leader_address,omitempty"`
+	LeaderClusterAddress string    `json:"leader_cluster_address,omitempty"`
 
 	// Raft Indexes for this node
 	RaftCommittedIndex uint64 `json:"raft_committed_index,omitempty"`
@@ -4409,29 +4393,27 @@ type LeaderResponse struct {
 func (core *Core) GetLeaderStatus() (*LeaderResponse, error) {
 	core.stateLock.RLock()
 	defer core.stateLock.RUnlock()
-
 	return core.GetLeaderStatusLocked()
 }
 
 func (core *Core) GetLeaderStatusLocked() (*LeaderResponse, error) {
-	haEnabled := true
 	isLeader, address, clusterAddr, err := core.LeaderLocked()
 	if errwrap.Contains(err, ErrHANotEnabled.Error()) {
-		haEnabled = false
-		err = nil
+		return &LeaderResponse{
+			HAEnabled: false,
+		}, nil
 	}
+
 	if err != nil {
 		return nil, err
 	}
 
 	resp := &LeaderResponse{
-		HAEnabled:            haEnabled,
+		HAEnabled:            true,
 		IsSelf:               isLeader,
 		LeaderAddress:        address,
 		LeaderClusterAddress: clusterAddr,
-	}
-	if isLeader {
-		resp.ActiveTime = core.activeTime
+		ActiveTime:           core.activeTime,
 	}
 
 	resp.RaftCommittedIndex, resp.RaftAppliedIndex = core.GetRaftIndexesLocked()

api/sys_health.go
@@ -43,12 +43,13 @@ type HealthResponse struct {
 	Initialized                bool   `json:"initialized"`
 	Sealed                     bool   `json:"sealed"`
 	Standby                    bool   `json:"standby"`
-	PerformanceStandby         bool   `json:"performance_standby"`
 	ReplicationPerformanceMode string `json:"replication_performance_mode"`
 	ReplicationDRMode          string `json:"replication_dr_mode"`
 	ServerTimeUTC              int64  `json:"server_time_utc"`
 	Version                    string `json:"version"`
 	ClusterName                string `json:"cluster_name,omitempty"`
 	ClusterID                  string `json:"cluster_id,omitempty"`
-	LastWAL                    uint64 `json:"last_wal,omitempty"`
+	// not present in OpenBao, but left here for compatibility.
+	PerformanceStandby bool   `json:"performance_standby"`
+	LastWAL            uint64 `json:"last_wal,omitempty"`
 }

api/sys_leader.go
@@ -32,10 +32,10 @@ func (c *Sys) LeaderWithContext(ctx context.Context) (*LeaderResponse, error) {
 
 type LeaderResponse struct {
 	HAEnabled            bool      `json:"ha_enabled"`
-	IsSelf               bool      `json:"is_self"`
-	ActiveTime           time.Time `json:"active_time"`
-	LeaderAddress        string    `json:"leader_address"`
-	LeaderClusterAddress string    `json:"leader_cluster_address"`
+	IsSelf               bool      `json:"is_self,omitempty"`
+	ActiveTime           time.Time `json:"active_time,omitzero"`
+	LeaderAddress        string    `json:"leader_address,omitempty"`
+	LeaderClusterAddress string    `json:"leader_cluster_address,omitempty"`
 	RaftCommittedIndex   uint64    `json:"raft_committed_index,omitempty"`
 	RaftAppliedIndex     uint64    `json:"raft_applied_index,omitempty"`
 }

api/sys_seal.go
@@ -96,22 +96,23 @@ func sealStatusRequestWithContext(ctx context.Context, c *Sys, r *Request) (*Sea
 }
 
 type SealStatusResponse struct {
-	Type             string   `json:"type"`
-	Initialized      bool     `json:"initialized"`
-	Sealed           bool     `json:"sealed"`
-	T                int      `json:"t"`
-	N                int      `json:"n"`
-	Progress         int      `json:"progress"`
-	Nonce            string   `json:"nonce"`
-	Version          string   `json:"version"`
-	BuildDate        string   `json:"build_date"`
-	Migration        bool     `json:"migration"`
-	ClusterName      string   `json:"cluster_name,omitempty"`
-	ClusterID        string   `json:"cluster_id,omitempty"`
-	RecoverySeal     bool     `json:"recovery_seal"`
-	RecoverySealType string   `json:"recovery_seal_type,omitempty"`
-	StorageType      string   `json:"storage_type,omitempty"`
-	Warnings         []string `json:"warnings,omitempty"`
+	Type             string `json:"type"`
+	Initialized      bool   `json:"initialized"`
+	Sealed           bool   `json:"sealed"`
+	T                int    `json:"t"`
+	N                int    `json:"n"`
+	Progress         int    `json:"progress"`
+	Nonce            string `json:"nonce"`
+	Version          string `json:"version"`
+	BuildDate        string `json:"build_date"`
+	Migration        bool   `json:"migration"`
+	ClusterName      string `json:"cluster_name,omitempty"`
+	ClusterID        string `json:"cluster_id,omitempty"`
+	RecoverySeal     bool   `json:"recovery_seal"`
+	RecoverySealType string `json:"recovery_seal_type,omitempty"`
+	StorageType      string `json:"storage_type,omitempty"`
+	// unused in OpenBao, but present for compatibility
+	Warnings []string `json:"warnings,omitempty"`
 }
 
 type UnsealOpts struct {

command/format.go
@@ -281,13 +281,13 @@ func (t TableFormatter) Output(ui cli.Ui, secret *api.Secret, data interface{}) 
 	case map[string]interface{}:
 		return t.OutputMap(ui, data)
 	case SealStatusOutput:
-		return t.OutputSealStatusStruct(ui, nil, data)
+		return t.OutputSealStatusStruct(ui, data)
 	default:
 		return errors.New("cannot use the table formatter for this type")
 	}
 }
 
-func (t TableFormatter) OutputSealStatusStruct(ui cli.Ui, secret *api.Secret, data interface{}) error {
+func (t TableFormatter) OutputSealStatusStruct(ui cli.Ui, data interface{}) error {
 	status := data.(SealStatusOutput)
 	var sealPrefix string
 
@@ -314,9 +314,17 @@ func (t TableFormatter) OutputSealStatusStruct(ui cli.Ui, secret *api.Secret, da
 		out = append(out, fmt.Sprintf("Seal Migration in Progress | %t", status.Migration))
 	}
 
-	out = append(out, fmt.Sprintf("Version | %s", status.Version))
-	out = append(out, fmt.Sprintf("Build Date | %s", status.BuildDate))
-	out = append(out, fmt.Sprintf("Storage Type | %s", status.StorageType))
+	if status.Version != "" {
+		out = append(out, fmt.Sprintf("Version | %s", status.Version))
+	}
+
+	if status.BuildDate != "" {
+		out = append(out, fmt.Sprintf("Build Date | %s", status.BuildDate))
+	}
+
+	if status.StorageType != "" {
+		out = append(out, fmt.Sprintf("Storage Type | %s", status.StorageType))
+	}
 
 	if status.ClusterName != "" && status.ClusterID != "" {
 		out = append(out, fmt.Sprintf("Cluster Name | %s", status.ClusterName))
@@ -330,14 +338,8 @@ func (t TableFormatter) OutputSealStatusStruct(ui cli.Ui, secret *api.Secret, da
 		if !status.Sealed {
 			out = append(out, fmt.Sprintf("HA Cluster | %s", status.LeaderClusterAddress))
 			mode := "standby"
-			showLeaderAddr := false
 			if status.IsSelf {
 				mode = "active"
-			} else {
-				if status.LeaderAddress == "" {
-					status.LeaderAddress = "<none>"
-				}
-				showLeaderAddr = true
 			}
 			out = append(out, fmt.Sprintf("HA Mode | %s", mode))
 
@@ -345,13 +347,8 @@ func (t TableFormatter) OutputSealStatusStruct(ui cli.Ui, secret *api.Secret, da
 				out = append(out, fmt.Sprintf("Active Since | %s", status.ActiveTime.Format(time.RFC3339Nano)))
 			}
 			// This is down here just to keep ordering consistent
-			if showLeaderAddr {
+			if !status.IsSelf && status.LeaderAddress != "" {
 				out = append(out, fmt.Sprintf("Active Node Address | %s", status.LeaderAddress))
-			}
-
-			if status.PerfStandby {
-				out = append(out, fmt.Sprintf("Performance Standby Node | %t", status.PerfStandby))
-				out = append(out, fmt.Sprintf("Performance Standby Last Remote WAL | %d", status.PerfStandbyLastRemoteWAL))
 			}
 		}
 	}
@@ -361,12 +358,6 @@ func (t TableFormatter) OutputSealStatusStruct(ui cli.Ui, secret *api.Secret, da
 	}
 	if status.RaftAppliedIndex > 0 {
 		out = append(out, fmt.Sprintf("Raft Applied Index | %d", status.RaftAppliedIndex))
-	}
-	if status.LastWAL != 0 {
-		out = append(out, fmt.Sprintf("Last WAL | %d", status.LastWAL))
-	}
-	if len(status.Warnings) > 0 {
-		out = append(out, fmt.Sprintf("Warnings | %v", status.Warnings))
 	}
 
 	ui.Output(tableOutput(out, &columnize.Config{
@@ -669,14 +660,11 @@ func looksLikeDuration(k string) bool {
 // Currently we are adding the fields from api.LeaderResponse
 type SealStatusOutput struct {
 	api.SealStatusResponse
-	HAEnabled                bool      `json:"ha_enabled"`
-	IsSelf                   bool      `json:"is_self,omitempty"`
-	ActiveTime               time.Time `json:"active_time,omitempty"`
-	LeaderAddress            string    `json:"leader_address,omitempty"`
-	LeaderClusterAddress     string    `json:"leader_cluster_address,omitempty"`
-	PerfStandby              bool      `json:"performance_standby,omitempty"`
-	PerfStandbyLastRemoteWAL uint64    `json:"performance_standby_last_remote_wal,omitempty"`
-	LastWAL                  uint64    `json:"last_wal,omitempty"`
-	RaftCommittedIndex       uint64    `json:"raft_committed_index,omitempty"`
-	RaftAppliedIndex         uint64    `json:"raft_applied_index,omitempty"`
+	HAEnabled            bool      `json:"ha_enabled"`
+	IsSelf               bool      `json:"is_self,omitempty"`
+	ActiveTime           time.Time `json:"active_time,omitzero"`
+	LeaderAddress        string    `json:"leader_address,omitempty"`
+	LeaderClusterAddress string    `json:"leader_cluster_address,omitempty"`
+	RaftCommittedIndex   uint64    `json:"raft_committed_index,omitempty"`
+	RaftAppliedIndex     uint64    `json:"raft_applied_index,omitempty"`
 }

command/format_test.go
@@ -124,9 +124,7 @@ Cluster Name                  cluster name
 Cluster ID                    cluster id
 HA Enabled                    true
 Raft Committed Index          3
-Raft Applied Index            4
-Last WAL                      2
-Warnings                      [warning]`
+Raft Applied Index            4`
 
 	if expectedOutputString != output {
 		fmt.Printf("%s\n%+v\n %s\n%+v\n", "output found was: ", output, "versus", expectedOutputString)
@@ -152,7 +150,7 @@ Unseal Nonce                  nonce
 Seal Migration in Progress    true
 Version                       version
 Build Date                    build date
-Storage Type                  n/a
+Storage Type                  type
 HA Enabled                    false`
 
 	if expectedOutputString != output {
@@ -184,7 +182,6 @@ func getMockStatusData(emptyFields bool) SealStatusOutput {
 			ClusterID:        "cluster id",
 			RecoverySeal:     true,
 			StorageType:      "storage type",
-			Warnings:         []string{"warning"},
 		}
 
 		// must initialize this struct without explicit field names due to embedding
@@ -195,9 +192,6 @@ func getMockStatusData(emptyFields bool) SealStatusOutput {
 			time.Time{}.UTC(),        // ActiveTime
 			"leader address",         // LeaderAddress
 			"leader cluster address", // LeaderClusterAddress
-			true,                     // PerfStandby
-			1,                        // PerfStandbyLastRemoteWAL
-			2,                        // LastWAL
 			3,                        // RaftCommittedIndex
 			4,                        // RaftAppliedIndex
 		}
@@ -217,7 +211,7 @@ func getMockStatusData(emptyFields bool) SealStatusOutput {
 			ClusterName:      "",
 			ClusterID:        "",
 			RecoverySeal:     true,
-			StorageType:      "",
+			StorageType:      "type",
 		}
 
 		// must initialize this struct without explicit field names due to embedding
@@ -228,9 +222,6 @@ func getMockStatusData(emptyFields bool) SealStatusOutput {
 			time.Time{}.UTC(), // ActiveTime
 			"",                // LeaderAddress
 			"",                // LeaderClusterAddress
-			false,             // PerfStandby
-			0,                 // PerfStandbyLastRemoteWAL
-			0,                 // LastWAL
 			0,                 // RaftCommittedIndex
 			0,                 // RaftAppliedIndex
 		}

command/read_test.go
@@ -148,7 +148,7 @@ func TestReadCommand_Run(t *testing.T) {
 		}
 		combined := ui.OutputWriter.String() + ui.ErrorWriter.String()
 		expected := []string{
-			"cluster_id", "cluster_name", "initialized", "performance_standby", "replication_dr_mode", "replication_performance_mode", "sealed",
+			"cluster_id", "cluster_name", "initialized", "replication_dr_mode", "replication_performance_mode", "sealed",
 			"server_time_utc", "standby", "version",
 		}
 		for _, expectedField := range expected {

commit 991495ea347fc7d506ba6f2b343f3086d0ba7e3b

fix: `sys/leader` does not contain `is_self` on standbys
This regression was introduced in https://github.com/openbao/openbao/pull/2383

Signed-off-by: Philipp Stehle <philipp.stehle@secretz.io>

internal/vault/logical_system.go
@@ -4523,7 +4523,7 @@ func (c *Core) GetSealStatus(ctx context.Context, lock bool) (*CoreSealStatusRes
 
 type LeaderResponse struct {
 	HAEnabled            bool      `json:"ha_enabled"`
-	IsSelf               bool      `json:"is_self,omitempty"`
+	IsSelf               *bool     `json:"is_self,omitempty"`
 	ActiveTime           time.Time `json:"active_time,omitzero"`
 	LeaderAddress        string    `json:"leader_address,omitempty"`
 	LeaderClusterAddress string    `json:"leader_cluster_address,omitempty"`
@@ -4553,7 +4553,7 @@ func (core *Core) GetLeaderStatusLocked() (*LeaderResponse, error) {
 
 	resp := &LeaderResponse{
 		HAEnabled:            true,
-		IsSelf:               isLeader,
+		IsSelf:               &isLeader,
 		LeaderAddress:        address,
 		LeaderClusterAddress: clusterAddr,
 		ActiveTime:           core.activeTime,

internal/http/sys_leader_test.go
@@ -5,29 +5,84 @@ package http
 
 import (
 	"net/http"
-	"reflect"
 	"testing"
+	"time"
 
+	"github.com/openbao/openbao/v2/internal/helper/testhelpers"
 	"github.com/openbao/openbao/v2/internal/vault"
+	"github.com/stretchr/testify/require"
 )
 
 func TestSysLeader_get(t *testing.T) {
-	core, _, _ := vault.TestCoreUnsealed(t)
-	ln, addr := TestServer(t, core)
-	defer ln.Close()
+	requestSysLeader := func(t *testing.T, client *http.Client, addr string) map[string]any {
+		t.Helper()
 
-	resp, err := http.Get(addr + "/v1/sys/leader")
-	if err != nil {
-		t.Fatalf("err: %s", err)
+		resp, err := client.Get(addr + "/v1/sys/leader")
+		require.NoError(t, err)
+
+		var actual map[string]any
+
+		testResponseStatus(t, resp, 200)
+		testResponseBody(t, resp, &actual)
+
+		return actual
 	}
 
-	var actual map[string]any
-	expected := map[string]any{
-		"ha_enabled": false,
-	}
-	testResponseStatus(t, resp, 200)
-	testResponseBody(t, resp, &actual)
-	if !reflect.DeepEqual(actual, expected) {
-		t.Fatalf("bad: %#v \n%#v", actual, expected)
-	}
+	t.Run("non-ha", func(t *testing.T) {
+		core, _, _ := vault.TestCoreUnsealed(t)
+		ln, addr := TestServer(t, core)
+		defer func() {
+			require.NoError(t, ln.Close())
+		}()
+
+		actual := requestSysLeader(t, http.DefaultClient, addr)
+		require.Equal(t, map[string]any{
+			"ha_enabled": false,
+		}, actual)
+	})
+
+	t.Run("ha", func(t *testing.T) {
+		testStartTime := time.Now()
+		cluster := vault.NewTestCluster(t, nil, &vault.TestClusterOptions{
+			NumCores:    2,
+			HandlerFunc: Handler,
+		})
+		testhelpers.WaitForActiveNodeAndStandbys(t, cluster)
+		t.Cleanup(cluster.Cleanup)
+
+		// primary
+		actual := requestSysLeader(t, &http.Client{
+			Transport: &http.Transport{
+				TLSClientConfig:   cluster.Nodes()[0].TLSConfig(),
+				ForceAttemptHTTP2: true,
+			},
+		}, cluster.Cores[0].Client.Address())
+
+		require.Contains(t, actual, "active_time")
+		activeTime, err := time.Parse(time.RFC3339, actual["active_time"].(string))
+		require.NoError(t, err)
+		require.WithinRange(t, activeTime, testStartTime, time.Now())
+		delete(actual, "active_time")
+
+		expected := map[string]any{
+			"ha_enabled":             true,
+			"is_self":                true,
+			"leader_address":         cluster.Cores[0].Client.Address(),
+			"leader_cluster_address": cluster.Cores[0].ClusterAddr(),
+			// "active_time" is tested above and removed from the map
+		}
+		require.Equal(t, expected, actual)
+
+		// standby
+		expected["is_self"] = false
+
+		actual = requestSysLeader(t, &http.Client{
+			Transport: &http.Transport{
+				TLSClientConfig:   cluster.Nodes()[1].TLSConfig(),
+				ForceAttemptHTTP2: true,
+			},
+		}, cluster.Cores[1].Client.Address())
+
+		require.Equal(t, expected, actual)
+	})
 }
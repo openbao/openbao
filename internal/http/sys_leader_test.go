3bf4462bde2523f7232822a768359b32e5f58564
Philipp Stehle <philipp.stehle@secretz.io>
2026-09-04 20:03:01 +0200

    fix: `sys/leader` does not contain `is_self` on standbys (#3932)
    This regression was introduced in https://github.com/openbao/openbao/pull/2383
    
    Signed-off-by: Philipp Stehle <philipp.stehle@secretz.io>

--- internal/http/sys_leader_test.go
+++ internal/http/sys_leader_test.go
@@ -5,29 +5,84 @@ package http
 
 import (
 	"net/http"
-	"reflect"
 	"testing"
+	"time"
 
+	"github.com/openbao/openbao/v2/internal/helper/testhelpers"
 	"github.com/openbao/openbao/v2/internal/vault"
+	"github.com/stretchr/testify/require"
 )
 
 func TestSysLeader_get(t *testing.T) {
-	core, _, _ := vault.TestCoreUnsealed(t)
-	ln, addr := TestServer(t, core)
-	defer ln.Close()
+	requestSysLeader := func(t *testing.T, client *http.Client, addr string) map[string]any {
+		t.Helper()
 
-	resp, err := http.Get(addr + "/v1/sys/leader")
-	if err != nil {
-		t.Fatalf("err: %s", err)
+		resp, err := client.Get(addr + "/v1/sys/leader")
+		require.NoError(t, err)
+
+		var actual map[string]any
+
+		testResponseStatus(t, resp, 200)
+		testResponseBody(t, resp, &actual)
+
+		return actual
 	}
 
-	var actual map[string]any
-	expected := map[string]any{
-		"ha_enabled": false,
-	}
-	testResponseStatus(t, resp, 200)
-	testResponseBody(t, resp, &actual)
-	if !reflect.DeepEqual(actual, expected) {
-		t.Fatalf("bad: %#v \n%#v", actual, expected)
-	}
+	t.Run("non-ha", func(t *testing.T) {
+		core, _, _ := vault.TestCoreUnsealed(t)
+		ln, addr := TestServer(t, core)
+		defer func() {
+			require.NoError(t, ln.Close())
+		}()
+
+		actual := requestSysLeader(t, http.DefaultClient, addr)
+		require.Equal(t, map[string]any{
+			"ha_enabled": false,
+		}, actual)
+	})
+
+	t.Run("ha", func(t *testing.T) {
+		testStartTime := time.Now()
+		cluster := vault.NewTestCluster(t, nil, &vault.TestClusterOptions{
+			NumCores:    2,
+			HandlerFunc: Handler,
+		})
+		testhelpers.WaitForActiveNodeAndStandbys(t, cluster)
+		t.Cleanup(cluster.Cleanup)
+
+		// primary
+		actual := requestSysLeader(t, &http.Client{
+			Transport: &http.Transport{
+				TLSClientConfig:   cluster.Nodes()[0].TLSConfig(),
+				ForceAttemptHTTP2: true,
+			},
+		}, cluster.Cores[0].Client.Address())
+
+		require.Contains(t, actual, "active_time")
+		activeTime, err := time.Parse(time.RFC3339, actual["active_time"].(string))
+		require.NoError(t, err)
+		require.WithinRange(t, activeTime, testStartTime, time.Now())
+		delete(actual, "active_time")
+
+		expected := map[string]any{
+			"ha_enabled":             true,
+			"is_self":                true,
+			"leader_address":         cluster.Cores[0].Client.Address(),
+			"leader_cluster_address": cluster.Cores[0].ClusterAddr(),
+			// "active_time" is tested above and removed from the map
+		}
+		require.Equal(t, expected, actual)
+
+		// standby
+		expected["is_self"] = false
+
+		actual = requestSysLeader(t, &http.Client{
+			Transport: &http.Transport{
+				TLSClientConfig:   cluster.Nodes()[1].TLSConfig(),
+				ForceAttemptHTTP2: true,
+			},
+		}, cluster.Cores[1].Client.Address())
+
+		require.Equal(t, expected, actual)
+	})
 }

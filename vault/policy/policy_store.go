da3ed385d81a08f022a73a1d7acdae8f2a406d40
Jonas Köhnen <jonas.koehnen@sap.com>
2026-09-11 16:16:15 +0200

    Fix policy cache traversal (openbao-security#114)
    Prevent cross-namespace policy resolution traversal in the LRU policy
    cache, allowing unintentional cross-namespace access.
    
    Signed-off-by: Jonas Köhnen <jonas.koehnen@sap.com>

--- vault/policy/policy_store.go
+++ vault/policy/policy_store.go
@@ -7,7 +7,6 @@ import (
 	"context"
 	"errors"
 	"fmt"
-	"path"
 	"slices"
 	"strings"
 	"time"
@@ -153,7 +152,7 @@ var (
 type Store struct {
 	core core
 
-	tokenPoliciesLRU *lru.TwoQueueCache[string, *Policy]
+	tokenPoliciesLRU *lru.TwoQueueCache[cacheKey, *Policy]
 
 	// This is used to ensure that writes to the store (acl) or to the egp
 	// path tree don't happen concurrently. We are okay reading stale data so
@@ -180,7 +179,7 @@ func NewStore(ctx context.Context, core core, baseView barrier.View, system logi
 	}
 
 	if !system.CachingDisabled() {
-		cache, _ := lru.New2Q[string, *Policy](policyCacheSize)
+		cache, _ := lru.New2Q[cacheKey, *Policy](policyCacheSize)
 		ps.tokenPoliciesLRU = cache
 	}
 
@@ -222,7 +221,7 @@ func (ps *Store) InvalidateNamespace(ctx context.Context, uuid string) {
 	defer ps.lockWithUnlock(ctx)()
 
 	for _, key := range ps.tokenPoliciesLRU.Keys() {
-		if strings.HasPrefix(key, uuid) {
+		if key.namespace == uuid {
 			ps.tokenPoliciesLRU.Remove(key)
 		}
 	}
@@ -378,7 +377,7 @@ func (ps *Store) switchedGetPolicy(ctx context.Context, name string, policyType 
 	name = ps.sanitizeName(name)
 	index := ps.cacheKey(ns, name)
 
-	var cache *lru.TwoQueueCache[string, *Policy]
+	var cache *lru.TwoQueueCache[cacheKey, *Policy]
 	var view barrier.View
 
 	switch policyType {
@@ -699,8 +698,15 @@ func (ps *Store) sanitizeName(name string) string {
 	return strings.ToLower(strings.TrimSpace(name))
 }
 
-func (ps *Store) cacheKey(ns *namespace.Namespace, name string) string {
-	return path.Join(ns.UUID, name)
+type cacheKey struct {
+	name, namespace string
+}
+
+func (ps *Store) cacheKey(ns *namespace.Namespace, name string) cacheKey {
+	return cacheKey{
+		name:      name,
+		namespace: ns.UUID,
+	}
 }
 
 // LoadDefaultPolicies loads default policies for the namespace in the provided context

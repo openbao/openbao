0646d5920ae43f4c595de89d7c511207126292e0
Jonas Köhnen <jonas.koehnen@sap.com>
2026-09-16 11:52:10 +0200

    Remove coreNumber
    Signed-off-by: Jonas Köhnen <jonas.koehnen@sap.com>

--- internal/vault/mount.go
+++ internal/vault/mount.go
@@ -1651,7 +1651,6 @@ func (c *Core) newLogicalBackend(ctx context.Context, entry *routing.MountEntry,
 	}
 
 	ctx = namespace.ContextWithNamespace(ctx, entry.Namespace)
-	ctx = context.WithValue(ctx, "core_number", c.coreNumber)
 	b, err := f(ctx, config)
 	if err != nil {
 		return nil, err

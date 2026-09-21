b20dbd727f2d1d91c227bb100a3fedfcb17f5974
Dmitry Chepurovskiy <me@dm3ch.net>
2026-09-21 17:01:51 +0300

    Fix review findings
    Signed-off-by: Dmitry Chepurovskiy <me@dm3ch.net>

--- internal/command/base_test.go
+++ internal/command/base_test.go
@@ -7,9 +7,6 @@ import (
 	"net/http"
 	"reflect"
 	"testing"
-
-	"github.com/openbao/openbao/api/v2"
-	"github.com/stretchr/testify/require"
 )
 
 func getDefaultCliHeaders(t *testing.T) http.Header {
@@ -21,14 +18,7 @@ func getDefaultCliHeaders(t *testing.T) http.Header {
 	return cli.Headers()
 }
 
-func isolateHeaderEnvironment(t *testing.T) {
-	t.Helper()
-	t.Setenv(api.EnvHeaders, "")
-	t.Setenv(api.UpstreamVariableName(api.EnvHeaders), "")
-}
-
 func TestClient_FlagHeader(t *testing.T) {
-	isolateHeaderEnvironment(t)
 	defaultHeaders := getDefaultCliHeaders(t)
 
 	cases := []struct {
@@ -79,33 +69,4 @@ func TestClient_FlagHeader(t *testing.T) {
 			t.Errorf("expected [%#v] but got [%#v]", expectedHeaders, actualHeaders)
 		}
 	}
-}
-
-func TestClient_EnvironmentHeader(t *testing.T) {
-	isolateHeaderEnvironment(t)
-	t.Setenv(api.EnvHeaders, `{"X-IAP-Token":"token"}`)
-
-	client, err := (&BaseCommand{}).Client()
-	require.NoError(t, err)
-	require.Equal(t, []string{"token"}, client.Headers().Values("X-IAP-Token"))
-}
-
-func TestClient_EnvironmentAndFlagHeader(t *testing.T) {
-	isolateHeaderEnvironment(t)
-	t.Setenv(api.EnvHeaders, `{"X-IAP-Token":"environment"}`)
-
-	client, err := (&BaseCommand{
-		flagHeader: map[string]string{"X-IAP-Token": "flag"},
-	}).Client()
-	require.NoError(t, err)
-	require.Equal(t, []string{"environment", "flag"}, client.Headers().Values("X-IAP-Token"))
-}
-
-func TestClient_EnvironmentReservedHeader(t *testing.T) {
-	isolateHeaderEnvironment(t)
-	t.Setenv(api.EnvHeaders, `{"X-Vault-Test":"value"}`)
-
-	client, err := (&BaseCommand{}).Client()
-	require.Nil(t, client)
-	require.ErrorContains(t, err, "BAO_HEADERS contains a header name with reserved prefix")
 }

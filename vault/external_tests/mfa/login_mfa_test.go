25d36a88423257481e2f57189fd180b927acafb1
Wojciech Slabosz <wojciech.slabosz@sap.com>
2026-09-22 17:24:23 +0200

    Login enforcement delete actually deletes storage entry (#4033)
    * fixed mfa enforcement config deletionnot updating storage
    
    Signed-off-by: Wojciech Slabosz <wojciech.slabosz@sap.com>
    
    * prune memdb entries when storage entry doesn't exist
    
    Signed-off-by: Wojciech Slabosz <wojciech.slabosz@sap.com>
    
    * changelog added
    
    Signed-off-by: Wojciech Slabosz <wojciech.slabosz@sap.com>
    
    ---------
    
    Signed-off-by: Wojciech Slabosz <wojciech.slabosz@sap.com>

--- vault/external_tests/mfa/login_mfa_test.go
+++ vault/external_tests/mfa/login_mfa_test.go
@@ -11,6 +11,7 @@ import (
 	"github.com/hashicorp/go-secure-stdlib/strutil"
 	"github.com/openbao/openbao/api/v2"
 	"github.com/openbao/openbao/builtin/credential/userpass"
+	"github.com/openbao/openbao/helper/testhelpers"
 	vaulthttp "github.com/openbao/openbao/http"
 	"github.com/openbao/openbao/sdk/v2/logical"
 	"github.com/openbao/openbao/vault"
@@ -598,6 +599,18 @@ func TestLoginMFA_LoginEnforcement_CRUD(t *testing.T) {
 	// when both the response and the error are nil on a read request, that gets translated into a 404
 	if resp != nil || err != nil {
 		t.Fatal("expected the read to 404 but it didn't")
+	}
+
+	// After sealing of the cluster deleted entry must not exist.
+	for _, core := range cluster.Cores {
+		testhelpers.EnsureCoreSealed(t, core)
+	}
+	cluster.UnsealCores(t)
+	activeCore := testhelpers.DeriveStableActiveCore(t, cluster)
+
+	resp, err = activeCore.Client.Logical().Read(myPath)
+	if resp != nil || err != nil {
+		t.Fatal("expected the read to 404 after restart but it didn't")
 	}
 }
 

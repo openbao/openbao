8c6f8b2ec8ad4d94adda67ab3b8fcccb5c6bc649
Alexander Scheel <alex.scheel@control-plane.io>
2026-09-23 09:54:56 -0500

    Check plugin path before executing (openbao-security#112)
    Various mechanisms allow bypassing directory controls, most notably,
    `sys/raw` and snapshot restoration. Double check the path before
    executing the plugin to ensure we're sandboxed.
    
    Signed-off-by: Alexander Scheel <alex.scheel@control-plane.io>

--- internal/vault/plugin_catalog.go
+++ internal/vault/plugin_catalog.go
@@ -1047,8 +1047,10 @@ func (c *PluginCatalog) getExternal(ctx context.Context, name string, pluginType
 		return nil, nil
 	}
 
-	// Prepend the plugin directory to the command.
-	entry.Command = filepath.Join(c.directory, entry.Command)
+	entry.Command, err = c.checkCommandDirectory(entry.Name, pluginType, entry.Command, entry.Sha256, entry.Oci)
+	if err != nil {
+		return nil, err
+	}
 
 	return entry, nil
 }
@@ -1192,17 +1194,17 @@ func (c *PluginCatalog) Set(ctx context.Context, name string, pluginType consts.
 	return err
 }
 
-func (c *PluginCatalog) setInternal(ctx context.Context, name string, pluginType consts.PluginType, version string, command string, args []string, env []string, sha256 []byte, isOci bool, declarative bool) (*pluginutil.PluginRunner, error) {
+func (c *PluginCatalog) checkCommandDirectory(name string, pluginType consts.PluginType, command string, sha256 []byte, isOci bool) (string, error) {
 	// Best effort check to make sure the command isn't breaking out of the
 	// configured plugin directory.
 	commandFull := filepath.Join(c.directory, command)
 	sym, err := filepath.EvalSymlinks(commandFull)
 	if err != nil {
-		return nil, fmt.Errorf("error while validating the command path: %w", err)
+		return "", fmt.Errorf("error while validating the command path: %w", err)
 	}
 	symAbs, err := filepath.Abs(filepath.Dir(sym))
 	if err != nil {
-		return nil, fmt.Errorf("error while validating the command path: %w", err)
+		return "", fmt.Errorf("error while validating the command path: %w", err)
 	}
 
 	ok := symAbs == c.directory
@@ -1216,7 +1218,16 @@ func (c *PluginCatalog) setInternal(ctx context.Context, name string, pluginType
 	}
 
 	if !ok {
-		return nil, errors.New("cannot execute files outside of configured plugin directory")
+		return "", errors.New("cannot execute files outside of configured plugin directory")
+	}
+
+	return commandFull, nil
+}
+
+func (c *PluginCatalog) setInternal(ctx context.Context, name string, pluginType consts.PluginType, version string, command string, args []string, env []string, sha256 []byte, isOci bool, declarative bool) (*pluginutil.PluginRunner, error) {
+	commandFull, err := c.checkCommandDirectory(name, pluginType, command, sha256, isOci)
+	if err != nil {
+		return nil, err
 	}
 
 	// entryTmp should only be used for the below type and version checks, it uses the
